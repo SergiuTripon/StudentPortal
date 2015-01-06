430e912a84ccc34f3c260705b13fba2b8955b7b5
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-06 18:24:06 +0000

    Corrected success message

--- forgotten-password.php
+++ forgotten-password.php
@@ -104,7 +104,7 @@ include 'includes/signin.php';
     <p id="hide" class="feedback-custom text-justify">Please enter the email you used to register to the <b>Student Portal</b> and we will email you a link to reset your password.</p>
 	
 	<p id="error" class="feedback-sad text-center"></p>
-	<p id="success" class="feedback-sad text-center"></p>
+	<p id="success" class="feedback-happy text-center"></p>
 	
 	<label>Email address</label>
     <input class="form-control" type="email" name="email" id="email" placeholder="Email address">
