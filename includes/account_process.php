a3071eba1edad7b60836ade5db97645a929e992a
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-06 18:48:45 +0000

    Changed variables Merged functions together

--- includes/account_process.php
+++ includes/account_process.php
@@ -192,24 +192,24 @@ elseif (isset($_POST['deleteaccount_button'])) {
 
 elseif (isset($_POST['account_type1'], $_POST['gender1'], $_POST['firstname1'], $_POST['surname1'], $_POST['studentno1'], $_POST['email1'], $_POST['password1'], $_POST['confirmpwd1'], $_POST['dateofbirth1'], $_POST['phonenumber1'], $_POST['degree1'], $_POST['address11'], $_POST['address21'], $_POST['town1'], $_POST['city1'], $_POST['country1'], $_POST['postcode1'])) {
 
-	$account_type = filter_input(INPUT_POST, 'account_type', FILTER_SANITIZE_STRING);
-	$gender = filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_STRING);
-	$firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
-	$surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_STRING);
-	$studentno = filter_input(INPUT_POST, 'studentno', FILTER_SANITIZE_STRING);
+	$account_type = filter_input(INPUT_POST, 'account_type1', FILTER_SANITIZE_STRING);
+	$gender = filter_input(INPUT_POST, 'gender1', FILTER_SANITIZE_STRING);
+	$firstname = filter_input(INPUT_POST, 'firstname1', FILTER_SANITIZE_STRING);
+	$surname = filter_input(INPUT_POST, 'surname1', FILTER_SANITIZE_STRING);
+	$studentno = filter_input(INPUT_POST, 'studentno1', FILTER_SANITIZE_STRING);
 	$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 	$email = filter_var($email, FILTER_VALIDATE_EMAIL);
-	$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-	$confirmpwd = filter_input(INPUT_POST, 'confirmpwd', FILTER_SANITIZE_STRING);
-	$dateofbirth = filter_input(INPUT_POST, 'dateofbirth', FILTER_SANITIZE_STRING);
-	$phonenumber = filter_input(INPUT_POST, 'phonenumber', FILTER_SANITIZE_STRING);
-	$degree = filter_input(INPUT_POST, 'degree', FILTER_SANITIZE_STRING);
-	$address1 = filter_input(INPUT_POST, 'address1', FILTER_SANITIZE_STRING);
-	$address2 = filter_input(INPUT_POST, 'address2', FILTER_SANITIZE_STRING);
-	$town = filter_input(INPUT_POST, 'town', FILTER_SANITIZE_STRING);
-	$city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
-	$country = filter_input(INPUT_POST, 'country', FILTER_SANITIZE_STRING);
-	$postcode = filter_input(INPUT_POST, 'postcode', FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_STRING);
+	$confirmpwd = filter_input(INPUT_POST, 'confirmpwd1', FILTER_SANITIZE_STRING);
+	$dateofbirth = filter_input(INPUT_POST, 'dateofbirth1', FILTER_SANITIZE_STRING);
+	$phonenumber = filter_input(INPUT_POST, 'phonenumber1', FILTER_SANITIZE_STRING);
+	$degree = filter_input(INPUT_POST, 'degree1', FILTER_SANITIZE_STRING);
+	$address1 = filter_input(INPUT_POST, 'address11', FILTER_SANITIZE_STRING);
+	$address2 = filter_input(INPUT_POST, 'address21', FILTER_SANITIZE_STRING);
+	$town = filter_input(INPUT_POST, 'town1', FILTER_SANITIZE_STRING);
+	$city = filter_input(INPUT_POST, 'city1', FILTER_SANITIZE_STRING);
+	$country = filter_input(INPUT_POST, 'country1', FILTER_SANITIZE_STRING);
+	$postcode = filter_input(INPUT_POST, 'postcode1', FILTER_SANITIZE_STRING);
 
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		header('HTTP/1.0 550 The email address you entered is invalid.');
