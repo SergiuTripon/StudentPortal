4170a8d54de027119105da9e3e575a700d64080a
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-06 19:00:15 +0000

    Added column name within query Changed column name within query

--- includes/account_process.php
+++ includes/account_process.php
@@ -248,7 +248,7 @@ elseif (isset($_POST['account_type1'], $_POST['gender1'], $_POST['firstname1'], 
 	$password_hash = password_hash($password, PASSWORD_BCRYPT);
 
 	$stmt3 = $mysqli->prepare("INSERT INTO user_signin (account_type, email, password, created_on) VALUES (?, ?, ?, ?)");
-	$stmt3->bind_param('sss', $account_type, $email, $password_hash, $created_on);
+	$stmt3->bind_param('ssss', $account_type, $email, $password_hash, $created_on);
 	$stmt3->execute();
 	$stmt3->close();
 
