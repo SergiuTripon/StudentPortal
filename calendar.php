a31aab5016752a2fe6d4af9df14134d49cd88c9e
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-08 22:29:08 +0000

    Changing tables style

--- calendar.php
+++ calendar.php
@@ -283,7 +283,7 @@ else $userid = '';
 		"ordering": true,
 		"info": false,
 		"language": {
-			"emptyTable": "There are no task at the moment."
+			"emptyTable": "There are no tasks at the moment."
 		}
 	});
     });
