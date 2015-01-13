5ab457eb3c754961b003bbf84eda36301f8c7b95
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-13 17:47:45 +0000

    Testing a new idea

--- includes/showcase/showcase.php
+++ includes/showcase/showcase.php
@@ -266,7 +266,7 @@
     <div class="modal-content">
 
 	<div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal"><i id="feature-icon" class="fa"></i></button>
+    <divclass="close"><i id="feature-icon" class="fa"></i></div>
     <h4 class="modal-title" id="modal-custom-label"></h4>
     </div>
 
