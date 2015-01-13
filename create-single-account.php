44872adee108fcecb5a7f7d89c37a4df417f49f5
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-13 21:01:42 +0000

    Fixing assets paths

--- create-single-account.php
+++ create-single-account.php
@@ -309,7 +309,7 @@ include 'includes/signin.php';
 	$(function () {
 	$('#dateofbirth').datepicker({
 		dateFormat: "yy-mm-dd",
-		defaultDate: "01/01/1993"
+		defaultDate: "1993/01/01"
 	});
 	});
 	</script>
