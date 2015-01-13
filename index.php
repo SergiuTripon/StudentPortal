7359c097f5ab436f96693480a532046a4f0ab86e
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-13 14:36:02 +0000

    Testing a new idea

--- index.php
+++ index.php
@@ -247,22 +247,23 @@ include 'includes/signin.php';
         $(document).on('click', '.timetable-tile', function (event) {
             $('.tiles').hide();
             $('.feature').show();
-            $('.feature').focus();
             $('#feature-icon').addClass("fa-table");
             $('#feature-title').empty().append("Timetable");
             $('#feature-detail').empty().append("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pulvinar purus, vitae gravida arcu blandit eu. Suspendisse laoreet metus eget sollicitudin varius.");
+            $('.feature').focus();
         });
         $(document).on('click', '.exams-tile', function (event) {
             $('.tiles').hide();
             $('.feature').show();
-            $('.feature').focus();
             $('#feature-icon').addClass("fa-pencil");
             $('#feature-title').empty().append("Exams");
             $('#feature-detail').empty().append("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pulvinar purus, vitae gravida arcu blandit eu. Suspendisse laoreet metus eget sollicitudin varius.");
+            $('.feature').focus();
         });
         $(document).on('click', '#back-button', function (event) {
             $('.feature').hide();
             $('.tiles').show();
+            $('.tiles').focus();
         });
     </script>
 
