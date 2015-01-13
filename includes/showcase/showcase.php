46e97ae6b6f24319d806b8e76758262bbf77c2ae
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-13 15:51:40 +0000

    Testing a new idea

--- includes/showcase/showcase.php
+++ includes/showcase/showcase.php
@@ -106,7 +106,7 @@
     <div class="container">
     <div class="row">
     <div class="col-lg-12 text-center">
-    <h2 class="about-title"><b>Student Portal</b></h2>
+    <h2><b>Student Portal</b></h2>
 	<h2>All your university needs, in one single place.</h2>
 	</div>
 	</div><!-- /.row -->
