7ec97937e40ae42a829ed6242f066410b0af84a6
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-13 13:54:47 +0000

    Testing a new idea

--- includes/showcase/showcase.php
+++ includes/showcase/showcase.php
@@ -113,7 +113,7 @@
 	</div>
 
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div class="tile exam-tile">
+    <div class="tile exams-tile">
 	<i class="fa fa-pencil"></i>
 	<p class="tile-text">Exams</p>
     </div>
