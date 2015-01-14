0d16b9e52858a60bb5791f5a2fccc0f4ce0d35cd
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 01:20:29 +0000

    Fixing assets paths

--- calendar.php
+++ calendar.php
@@ -12,9 +12,6 @@ else $userid = '';
 
 <head>
 
-	<!-- Pace JS -->
-    <script src="../assets/js/pace-js/pace.js"></script>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,7 +25,6 @@ else $userid = '';
 </head>
 
 <body>
-<div class="preloader"></div>
 
 	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 	
