30bd9e0ef1767cb9fc5de08a9e112f6b807bf3ff
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 12:17:56 +0000

    Fixing assets paths

--- calendar.php
+++ calendar.php
@@ -25,10 +25,10 @@ else $userid = '';
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel='stylesheet' type="text/css">
 
 	<!-- bootstrap -->
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
 
 	<!-- font-awesome -->
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../assets/css/font-awesome/font-awesome.min.css" rel="stylesheet">
 
 	<!-- ladda -->
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
