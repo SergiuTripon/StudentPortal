a2e8204ffba0e907a7ed6da87d5ebfbcea618aa1
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 00:12:04 +0000

    Fixing assets paths

--- includes/signin.php
+++ includes/signin.php
@@ -6,8 +6,7 @@ if (session_status() == PHP_SESSION_NONE) {
 
 include 'db_connection.php';
 
+?>
 
 <!-- Bootstrap CSS -->
 <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-
-?>
