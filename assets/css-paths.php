f9d23c9f80ae03d3c23b2b5ca9ed20d5ba40cab7
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 12:20:40 +0000

    Fixing assets paths

--- assets/css-paths.php
+++ assets/css-paths.php
@@ -5,13 +5,13 @@
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel='stylesheet' type="text/css">
 
 <!-- bootstrap -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="../assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
 
 <!-- font-awesome -->
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="../assets/css/font-awesome/font-awesome.min.css" rel="stylesheet">
 
 <!-- ladda -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
+<link href="../assets/css/ladda/ladda-themeless.min.css" rel="stylesheet">
 
 <!-- Date Picker CSS -->
 <link rel="stylesheet" href="../assets/css/date-time-picker/jquery-ui-1.10.0.custom.css">
@@ -21,12 +21,12 @@
 <link href="../assets/css/calendar/calendar.css" rel="stylesheet">
 
 <!-- data-tables -->
-<link href="https://cdn.datatables.net/1.10.4/css/jquery.dataTables.min.css" rel="stylesheet">
-<link href="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/font-awesome/dataTables.fontAwesome.css" rel="stylesheet">
+<link href="../assets/css/data-tables/jquery.dataTables.min.css" rel="stylesheet">
+<link href="../assets/css/data-tables/dataTables.fontAwesome.css" rel="stylesheet">
 
 <!-- common -->
 <link href="../assets/css/common/custom.css" rel="stylesheet">
 <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<script src="../assets/css/common/html5shiv.min.js"></script>
+<script src="../assets/css/common/respond.min.js"></script>
 <![endif]-->
