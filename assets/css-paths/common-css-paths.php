07051991327c4974fd8c90983cc2fcaa2d211087
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 13:33:54 +0000

    Fixing assets paths

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -14,7 +14,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
 
 <!-- common -->
-<link href="../../css/common/custom.css" rel="stylesheet">
+<link href="../../../css/common/custom.css" rel="stylesheet">
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
