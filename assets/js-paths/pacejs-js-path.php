0904496247b8d2d16f7fd8758363918c355f3608
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 13:45:50 +0000

    Fixing assets paths

--- assets/js-paths/pacejs-js-path.php
+++ assets/js-paths/pacejs-js-path.php
@@ -1,3 +1,3 @@
 <!-- pace-js -->
-<script src="../js/pace-js/spin.min.js"></script>
-<script src="../js/pace-js/pace.js"></script>
+<script src="../assets/js/pace-js/spin.min.js"></script>
+<script src="../assets/js/pace-js/pace.js"></script>
