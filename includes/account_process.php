3c870859a1da4c7a3b70e42c46121b21d283e760
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 18:33:13 +0000

    Changing security access

--- includes/account_process.php
+++ includes/account_process.php
@@ -34,7 +34,7 @@ if (isset($_POST['gender'], $_POST['firstname'], $_POST['surname'], $_POST['date
 		$dateofbirth = NULL;
 	}
 
-	if ($degree == '') {
+	if ($degree == 'undefined') {
 		$degree = NULL;
 	}
 
