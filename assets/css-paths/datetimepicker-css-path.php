d147b6d16b2de69086fe20b28a0b067f76744829
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 17:00:04 +0000

    Remove css rule on p tag

--- assets/css-paths/datetimepicker-css-path.php
+++ assets/css-paths/datetimepicker-css-path.php
@@ -1,3 +1,3 @@
 <!-- Date Picker CSS -->
-<link rel="stylesheet" href="../css/date-time-picker/jquery-ui-1.10.0.custom.css">
-<link rel="stylesheet" href="../css/date-time-picker/jquery-ui-timepicker-addon.css">
+<link href="../assets/css/date-time-picker/jquery-ui-1.10.0.custom.css" rel="stylesheet">
+<link href="../assets/css/date-time-picker/jquery-ui-timepicker-addon.css" rel="stylesheet">
