6289d77d0c738f38c99e2d2876b50ab9d6212eda
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 01:26:56 +0000

    Fixing assets paths

--- calendar.php
+++ calendar.php
@@ -18,40 +18,14 @@ else $userid = '';
     <meta name="description" content="">
     <meta name="author" content="">
 
-	<!-- favicon -->
-	<link rel="icon" href="../assets/img/favicon/favicon.ico">
-
-	<!-- open-sans-font -->
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel='stylesheet' type="text/css">
-
-	<!-- bootstrap -->
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-
-	<!-- font-awesome -->
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-	<!-- ladda -->
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
-
-	<!-- bootstrap calendar -->
-	<link href="../assets/css/calendar/calendar.css" rel="stylesheet">
-
-	<!-- data-tables -->
-	<link href="https://cdn.datatables.net/1.10.4/css/jquery.dataTables.min.css" rel="stylesheet">
-	<link href="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/font-awesome/dataTables.fontAwesome.css" rel="stylesheet">
-
-	<!-- common -->
-	<link href="../assets/css/common/custom.css" rel="stylesheet">
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<?php include 'assets/css-paths.php'; ?>
 
     <title>Student Portal | Calendar</title>
 
 </head>
 
 <body>
+<div class="preloader"></div>
 
 	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 	
