69b69fc941cc89212d23c6e6467dcccc250ebe9b
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 13:55:37 +0000

    Fixing assets paths

--- calendar.php
+++ calendar.php
@@ -20,7 +20,7 @@ else $userid = '';
     <meta name="description" content="">
     <meta name="author" content="">
 
-	<?php include 'assets/css-paths/common-paths.php'; ?>
+	<?php include 'assets/css-paths/common-css-paths.php'; ?>
 	<?php include 'assets/css-paths/calendar-css-path.php'; ?>
 
     <title>Student Portal | Calendar</title>
