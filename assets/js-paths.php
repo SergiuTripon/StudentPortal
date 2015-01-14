acd6cd944391a8e403e75a583edc8532cdb0bdf2
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 01:18:26 +0000

    Fixing assets paths

--- assets/js-paths.php
+++ assets/js-paths.php
@@ -3,7 +3,7 @@
 
 <!-- pace-js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
+<script src="../assets/js/pace-js/pace.min.js"></script>
 
 <!-- bootstrap -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
