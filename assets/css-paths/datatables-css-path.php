33be79fda7d9e68d43c92ad26604e323eb778076
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 17:34:05 +0000

    Changing success message behaviour

--- assets/css-paths/datatables-css-path.php
+++ assets/css-paths/datatables-css-path.php
@@ -1,3 +1,3 @@
 <!-- data-tables -->
-<link href="../assets/css/data-tables/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.4/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/font-awesome/dataTables.fontAwesome.css" rel="stylesheet">
