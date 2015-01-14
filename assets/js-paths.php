95623476e310eee3f822de948ec57abe1653f883
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 01:06:00 +0000

    Fixing assets paths

--- assets/js-paths.php
+++ assets/js-paths.php
@@ -2,17 +2,17 @@
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
 <!-- pace-js -->
-<script src="../assets/js/pace-js/spin.min.js"></script>
-<script src="../assets/js/pace-js/pace.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
 
 <!-- bootstrap -->
-<script src="../assets/js/bootstrap/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
 <!-- ladda -->
-<script src="../assets/js/ladda/ladda.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
 
 <!-- easing -->
-<script src="../assets/js/easing/jquery.easing.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 <!-- tile-js -->
 <script src="../assets/js/tile-js/tileJs.min.js"></script>
@@ -23,11 +23,11 @@
 <script src="../assets/js/date-time-picker/jquery-ui-timepicker-addon.js"></script>
 
 <!-- dataTables JS -->
-<script src="../assets/js/data-tables/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
 
 <!-- Bootstrap Calendar JS -->
-<script src="../assets/js/calendar/jstz.min.js"></script>
-<script src="../assets/js/calendar/underscore.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
 <script src="../assets/js/calendar/calendar.js"></script>
 <script src="../assets/js/calendar/calendar-app.js"></script>
 
