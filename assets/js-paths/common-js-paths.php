3c63df0e25b459bd16ac051588e2766b9717e4b9
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-14 13:43:34 +0000

    Fixing assets paths

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -8,5 +8,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
 
 <!-- common-->
-<script src="../../js/common/custom.js"></script>
-<script src="../../js/common/ie10-viewport-bug-workaround.js"></script>
+<script src="..//js/common/custom.js"></script>
+<script src="../js/common/ie10-viewport-bug-workaround.js"></script>
