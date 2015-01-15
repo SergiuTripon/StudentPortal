6ed57705c95c56429eda8ee838cb4a5612ee708f
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-15 18:16:04 +0000

    Various changes

--- includes/paypal_process.php
+++ includes/paypal_process.php
@@ -1,250 +1,161 @@
 <?php
-include_once("paypal_config.php");
-include_once("paypal_class.php");
+include_once 'signin.php';
 
-$paypalmode = ($PayPalMode=='sandbox') ? '.sandbox' : '';
+if (isset($_SESSION['userid']))
+$userid = $_SESSION['userid'];
+else $userid = '';
 
-if($_POST) //Post Data received from product list page.
-{
-	//Mainly we need 4 variables from product page Item Name, Item Price, Item Number and Item Quantity.
-	
-	//Please Note : People can manipulate hidden field amounts in form,
-	//In practical world you must fetch actual price from database using item id. Eg: 
-	//$ItemPrice = $mysqli->query("SELECT item_price FROM products WHERE id = Product_Number");
+define('EMAIL_ADD', 'contact@sergiu-tripon.com');
+define('PAYPAL_EMAIL_ADD', 'triponsergiu-facilitator@hotmail.co.uk');
+require_once("paypal_class.php");
+$p = new paypal_class();
+$p->admin_mail = EMAIL_ADD;
+$payment = $_REQUEST["payment"];
 
-	$ItemName 		= $_POST["itemname"]; //Item Name
-	$ItemPrice 		= $_POST["itemprice"]; //Item Price
-	$ItemNumber 	= $_POST["itemnumber"]; //Item Number
-	$ItemDesc 		= $_POST["itemdesc"]; //Item Number
-	$ItemQty 		= $_POST["itemQty"]; // Item Quantity
-	$ItemTotalPrice = ($ItemPrice*$ItemQty); //(Item Price x Quantity = Total) Get total amount of product; 
-	
-	//Other important variables like tax, shipping cost
-	$TotalTaxAmount 	= 2.58;  //Sum of tax for all items in this order. 
-	$HandalingCost 		= 2.00;  //Handling cost for this order.
-	$InsuranceCost 		= 1.00;  //shipping insurance cost for this order.
-	$ShippinDiscount 	= -3.00; //Shipping discount for this order. Specify this as negative number.
-	$ShippinCost 		= 3.00; //Although you may change the value later, try to pass in a shipping amount that is reasonably accurate.
-	
-	//Grand total including all tax, insurance, shipping cost and discount
-	$GrandTotal = ($ItemTotalPrice + $TotalTaxAmount + $HandalingCost + $InsuranceCost + $ShippinCost + $ShippinDiscount);
-	
-	//Parameters for SetExpressCheckout, which will be sent to PayPal
-	$padata = 	'&METHOD=SetExpressCheckout'.
-				'&RETURNURL='.urlencode($PayPalReturnURL ).
-				'&CANCELURL='.urlencode($PayPalCancelURL).
-				'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
-				
-				'&L_PAYMENTREQUEST_0_NAME0='.urlencode($ItemName).
-				'&L_PAYMENTREQUEST_0_NUMBER0='.urlencode($ItemNumber).
-				'&L_PAYMENTREQUEST_0_DESC0='.urlencode($ItemDesc).
-				'&L_PAYMENTREQUEST_0_AMT0='.urlencode($ItemPrice).
-				'&L_PAYMENTREQUEST_0_QTY0='. urlencode($ItemQty).
-				
-				/* 
-				//Additional products (L_PAYMENTREQUEST_0_NAME0 becomes L_PAYMENTREQUEST_0_NAME1 and so on)
-				'&L_PAYMENTREQUEST_0_NAME1='.urlencode($ItemName2).
-				'&L_PAYMENTREQUEST_0_NUMBER1='.urlencode($ItemNumber2).
-				'&L_PAYMENTREQUEST_0_DESC1='.urlencode($ItemDesc2).
-				'&L_PAYMENTREQUEST_0_AMT1='.urlencode($ItemPrice2).
-				'&L_PAYMENTREQUEST_0_QTY1='. urlencode($ItemQty2).
-				*/
-				
-				/* 
-				//Override the buyer's shipping address stored on PayPal, The buyer cannot edit the overridden address.
-				'&ADDROVERRIDE=1'.
-				'&PAYMENTREQUEST_0_SHIPTONAME=J Smith'.
-				'&PAYMENTREQUEST_0_SHIPTOSTREET=1 Main St'.
-				'&PAYMENTREQUEST_0_SHIPTOCITY=San Jose'.
-				'&PAYMENTREQUEST_0_SHIPTOSTATE=CA'.
-				'&PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE=US'.
-				'&PAYMENTREQUEST_0_SHIPTOZIP=95131'.
-				'&PAYMENTREQUEST_0_SHIPTOPHONENUM=408-967-4444'.
-				*/
-				
-				'&NOSHIPPING=0'. //set 1 to hide buyer's shipping address, in-case products that does not require shipping
-				
-				'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($ItemTotalPrice).
-				'&PAYMENTREQUEST_0_TAXAMT='.urlencode($TotalTaxAmount).
-				'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($ShippinCost).
-				'&PAYMENTREQUEST_0_HANDLINGAMT='.urlencode($HandalingCost).
-				'&PAYMENTREQUEST_0_SHIPDISCAMT='.urlencode($ShippinDiscount).
-				'&PAYMENTREQUEST_0_INSURANCEAMT='.urlencode($InsuranceCost).
-				'&PAYMENTREQUEST_0_AMT='.urlencode($GrandTotal).
-				'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($PayPalCurrencyCode).
-				'&LOCALECODE=GB'. //PayPal pages to match the language on your website.
-				'&LOGOIMG=http://www.sanwebe.com/wp-content/themes/sanwebe/img/logo.png'. //site logo
-				'&CARTBORDERCOLOR=FFFFFF'. //border color of cart
-				'&ALLOWNOTE=1';
-				
-				############# set session variable we need later for "DoExpressCheckoutPayment" #######
-				$_SESSION['ItemName'] 			=  $ItemName; //Item Name
-				$_SESSION['ItemPrice'] 			=  $ItemPrice; //Item Price
-				$_SESSION['ItemNumber'] 		=  $ItemNumber; //Item Number
-				$_SESSION['ItemDesc'] 			=  $ItemDesc; //Item Number
-				$_SESSION['ItemQty'] 			=  $ItemQty; // Item Quantity
-				$_SESSION['ItemTotalPrice'] 	=  $ItemTotalPrice; //(Item Price x Quantity = Total) Get total amount of product; 
-				$_SESSION['TotalTaxAmount'] 	=  $TotalTaxAmount;  //Sum of tax for all items in this order. 
-				$_SESSION['HandalingCost'] 		=  $HandalingCost;  //Handling cost for this order.
-				$_SESSION['InsuranceCost'] 		=  $InsuranceCost;  //shipping insurance cost for this order.
-				$_SESSION['ShippinDiscount'] 	=  $ShippinDiscount; //Shipping discount for this order. Specify this as negative number.
-				$_SESSION['ShippinCost'] 		=   $ShippinCost; //Although you may change the value later, try to pass in a shipping amount that is reasonably accurate.
-				$_SESSION['GrandTotal'] 		=  $GrandTotal;
+date_default_timezone_set('Europe/London');
+$posted_date = date("Y-m-d G:i:s");
 
+$half = '0';
+$invoice_id = filter_input(INPUT_POST, 'invoice_id', FILTER_SANITIZE_STRING);
+$product_id = filter_input(INPUT_POST, 'product_id', FILTER_SANITIZE_STRING);
+$product_name = filter_input(INPUT_POST, 'product_name', FILTER_SANITIZE_STRING);
+$product_quantity = filter_input(INPUT_POST, 'product_quantity', FILTER_SANITIZE_STRING);
+$product_amount = filter_input(INPUT_POST, 'product_amount', FILTER_SANITIZE_STRING);
+$pending = 'pending';
+$payer_firstname = filter_input(INPUT_POST, 'payer_firstname', FILTER_SANITIZE_STRING);
+$payer_surname = filter_input(INPUT_POST, 'payer_surname', FILTER_SANITIZE_STRING);
+$payer_email = filter_input(INPUT_POST, 'payer_email', FILTER_SANITIZE_STRING);
+$payer_phonenumber = filter_input(INPUT_POST, 'payer_phonenumber', FILTER_SANITIZE_STRING);
+$payer_address1 = filter_input(INPUT_POST, 'payer_address1', FILTER_SANITIZE_STRING);
+$payer_address2 = filter_input(INPUT_POST, 'payer_address2', FILTER_SANITIZE_STRING);
+$payer_town = filter_input(INPUT_POST, 'payer_town', FILTER_SANITIZE_STRING);
+$payer_city = filter_input(INPUT_POST, 'payer_city', FILTER_SANITIZE_STRING);
+$payer_postcode = filter_input(INPUT_POST, 'payer_postcode', FILTER_SANITIZE_STRING);
 
-		//We need to execute the "SetExpressCheckOut" method to obtain paypal token
-		$paypal= new MyPayPal();
-		$httpParsedResponseAr = $paypal->PPHttpPost('SetExpressCheckout', $padata, $PayPalApiUsername, $PayPalApiPassword, $PayPalApiSignature, $PayPalMode);
+switch($payment){
+	case "process": // case process insert the form data in DB and process to the paypal
+
+		$stmt = $mysqli->prepare("UPDATE user_details set address1=?, city=?, postcode=? WHERE userid = ? LIMIT 1");
+		$stmt->bind_param('sssi', $payer_address1, $payer_city, $payer_postcode, $userid);
+		$stmt->execute();
+		$stmt->close();
+
+		$stmt = $mysqli->prepare("INSERT INTO paypal_log (userid, half, invoice_id, product_id, product_name, product_quantity, product_amount, posted_date, payment_status, payer_firstname, payer_surname, payer_email, payer_phonenumber, payer_address1, payer_address2, payer_town, payer_city, payer_postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param('iiiisiisssssssssss', $userid, $half, $invoice_id, $product_id, $product_name, $product_quantity, $product_amount, $posted_date, $pending, $payer_firstname, $payer_surname, $payer_email, $payer_phonenumber, $payer_address1, $payer_address2, $payer_town, $payer_city, $payer_postcode);
+		$stmt->execute();
+		$stmt->close();
 		
-		//Respond according to message we receive from Paypal
-		if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"]))
-		{
-
-				//Redirect user to PayPal store with Token received.
-			 	$paypalurl ='https://www'.$paypalmode.'.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token='.$httpParsedResponseAr["TOKEN"].'';
-				header('Location: '.$paypalurl);
-			 
-		}else{
-			//Show error message
-			echo '<div style="color:red"><b>Error : </b>'.urldecode($httpParsedResponseAr["L_LONGMESSAGE0"]).'</div>';
-			echo '<pre>';
-			print_r($httpParsedResponseAr);
-			echo '</pre>';
-		}
-
-}
-
-//Paypal redirects back to this page using ReturnURL, We should receive TOKEN and Payer ID
-if(isset($_GET["token"]) && isset($_GET["PayerID"]))
-{
-	//we will be using these two variables to execute the "DoExpressCheckoutPayment"
-	//Note: we haven't received any payment yet.
+		$this_script = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+		
+		$p->add_field('business', PAYPAL_EMAIL_ADD); // Call the facilitator eaccount
+		$p->add_field('cmd', $_POST["cmd"]); // cmd should be _cart for cart checkout
+		$p->add_field('upload', '1');
+		$p->add_field('return', $this_script.'?payment=success'); // return URL after the transaction got over
+		$p->add_field('cancel_return', $this_script.'?payment=cancel'); // cancel URL if the trasaction was cancelled during half of the transaction
+		$p->add_field('notify_url', $this_script.'?payment=ipn'); // Notify URL which received IPN (Instant Payment Notification)
+		$p->add_field('currency_code', $_POST["currency_code"]);
+		$p->add_field('invoice', $_POST["invoice_id"]);
+		$p->add_field('item_name_1', $_POST["product_name"]);
+		$p->add_field('item_number_1', $_POST["product_id"]);
+		$p->add_field('quantity_1', $_POST["product_quantity"]);
+		$p->add_field('amount_1', $_POST["product_amount"]);
+		$p->add_field('first_name', $_POST["payer_firstname"]);
+		$p->add_field('last_name', $_POST["payer_surname"]);
+		$p->add_field('email', $_POST["payer_email"]);
+		$p->add_field('night_phone_b', $_POST["payer_phonenumber"]);
+		$p->add_field('address1', $_POST["payer_address1"]);
+		$p->add_field('city', $_POST["payer_city"]);
+		$p->add_field('country', $_POST["payer_country"]);
+		$p->add_field('zip', $_POST["payer_postcode"]);
+		$p->submit_paypal_post(); // POST it to paypal
+		$p->dump_fields(); // Show the posted values for a reference, comment this line before app goes live
+	break;
 	
-	$token = $_GET["token"];
-	$payer_id = $_GET["PayerID"];
+	case "success": // success case to show the user payment got success
 	
-	//get session variables
-	$ItemName 			= $_SESSION['ItemName']; //Item Name
-	$ItemPrice 			= $_SESSION['ItemPrice'] ; //Item Price
-	$ItemNumber 		= $_SESSION['ItemNumber']; //Item Number
-	$ItemDesc 			= $_SESSION['ItemDesc']; //Item Number
-	$ItemQty 			= $_SESSION['ItemQty']; // Item Quantity
-	$ItemTotalPrice 	= $_SESSION['ItemTotalPrice']; //(Item Price x Quantity = Total) Get total amount of product; 
-	$TotalTaxAmount 	= $_SESSION['TotalTaxAmount'] ;  //Sum of tax for all items in this order. 
-	$HandalingCost 		= $_SESSION['HandalingCost'];  //Handling cost for this order.
-	$InsuranceCost 		= $_SESSION['InsuranceCost'];  //shipping insurance cost for this order.
-	$ShippinDiscount 	= $_SESSION['ShippinDiscount']; //Shipping discount for this order. Specify this as negative number.
-	$ShippinCost 		= $_SESSION['ShippinCost']; //Although you may change the value later, try to pass in a shipping amount that is reasonably accurate.
-	$GrandTotal 		= $_SESSION['GrandTotal'];
-
-	$padata = 	'&TOKEN='.urlencode($token).
-				'&PAYERID='.urlencode($payer_id).
-				'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
-				
-				//set item info here, otherwise we won't see product details later	
-				'&L_PAYMENTREQUEST_0_NAME0='.urlencode($ItemName).
-				'&L_PAYMENTREQUEST_0_NUMBER0='.urlencode($ItemNumber).
-				'&L_PAYMENTREQUEST_0_DESC0='.urlencode($ItemDesc).
-				'&L_PAYMENTREQUEST_0_AMT0='.urlencode($ItemPrice).
-				'&L_PAYMENTREQUEST_0_QTY0='. urlencode($ItemQty).
-
-				/* 
-				//Additional products (L_PAYMENTREQUEST_0_NAME0 becomes L_PAYMENTREQUEST_0_NAME1 and so on)
-				'&L_PAYMENTREQUEST_0_NAME1='.urlencode($ItemName2).
-				'&L_PAYMENTREQUEST_0_NUMBER1='.urlencode($ItemNumber2).
-				'&L_PAYMENTREQUEST_0_DESC1=Description text'.
-				'&L_PAYMENTREQUEST_0_AMT1='.urlencode($ItemPrice2).
-				'&L_PAYMENTREQUEST_0_QTY1='. urlencode($ItemQty2).
-				*/
-
-				'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($ItemTotalPrice).
-				'&PAYMENTREQUEST_0_TAXAMT='.urlencode($TotalTaxAmount).
-				'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($ShippinCost).
-				'&PAYMENTREQUEST_0_HANDLINGAMT='.urlencode($HandalingCost).
-				'&PAYMENTREQUEST_0_SHIPDISCAMT='.urlencode($ShippinDiscount).
-				'&PAYMENTREQUEST_0_INSURANCEAMT='.urlencode($InsuranceCost).
-				'&PAYMENTREQUEST_0_AMT='.urlencode($GrandTotal).
-				'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($PayPalCurrencyCode);
+	$stmt1 = $mysqli->prepare("SELECT half, product_amount FROM paypal_log WHERE userid = ? LIMIT 1");
+	$stmt1->bind_param('i', $userid);
+	$stmt1->execute();
+	$stmt1->store_result();	
+	$stmt1->bind_result($half, $product_amount);
+	$stmt1->fetch();
+	$stmt1->close();
 	
-	//We need to execute the "DoExpressCheckoutPayment" at this point to Receive payment from user.
-	$paypal= new MyPayPal();
-	$httpParsedResponseAr = $paypal->PPHttpPost('DoExpressCheckoutPayment', $padata, $PayPalApiUsername, $PayPalApiPassword, $PayPalApiSignature, $PayPalMode);
+	if ($product_amount == '9000.00' ) {
 	
-	//Check if everything went ok..
-	if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])) 
-	{
-
-			echo '<h2>Success</h2>';
-			echo 'Your Transaction ID : '.urldecode($httpParsedResponseAr["PAYMENTINFO_0_TRANSACTIONID"]);
-			
-				/*
-				//Sometimes Payment are kept pending even when transaction is complete. 
-				//hence we need to notify user about it and ask him manually approve the transiction
-				*/
-				
-				if('Completed' == $httpParsedResponseAr["PAYMENTINFO_0_PAYMENTSTATUS"])
-				{
-					echo '<div style="color:green">Payment Received! Your product will be sent to you very soon!</div>';
-				}
-				elseif('Pending' == $httpParsedResponseAr["PAYMENTINFO_0_PAYMENTSTATUS"])
-				{
-					echo '<div style="color:red">Transaction Complete, but payment is still pending! '.
-					'You need to manually authorize this payment in your <a target="_new" href="http://www.paypal.com">Paypal Account</a></div>';
-				}
-
-				// we can retrive transection details using either GetTransactionDetails or GetExpressCheckoutDetails
-				// GetTransactionDetails requires a Transaction ID, and GetExpressCheckoutDetails requires Token returned by SetExpressCheckOut
-				$padata = 	'&TOKEN='.urlencode($token);
-				$paypal= new MyPayPal();
-				$httpParsedResponseAr = $paypal->PPHttpPost('GetExpressCheckoutDetails', $padata, $PayPalApiUsername, $PayPalApiPassword, $PayPalApiSignature, $PayPalMode);
-
-				if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])) 
-				{
-					
-					echo '<br /><b>Stuff to store in database :</b><br /><pre>';
-					/*
-					#### SAVE BUYER INFORMATION IN DATABASE ###
-					//see (http://www.sanwebe.com/2013/03/basic-php-mysqli-usage) for mysqli usage
-					
-					$buyerName = $httpParsedResponseAr["FIRSTNAME"].' '.$httpParsedResponseAr["LASTNAME"];
-					$buyerEmail = $httpParsedResponseAr["EMAIL"];
-					
-					//Open a new connection to the MySQL server
-					$mysqli = new mysqli('host','username','password','database_name');
-					
-					//Output any connection error
-					if ($mysqli->connect_error) {
-						die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-					}		
-					
-					$insert_row = $mysqli->query("INSERT INTO BuyerTable 
-					(BuyerName,BuyerEmail,TransactionID,ItemName,ItemNumber, ItemAmount,ItemQTY)
-					VALUES ('$buyerName','$buyerEmail','$transactionID','$ItemName',$ItemNumber, $ItemTotalPrice,$ItemQTY)");
-					
-					if($insert_row){
-						print 'Success! ID of last inserted record is : ' .$mysqli->insert_id .'<br />'; 
-					}else{
-						die('Error : ('. $mysqli->errno .') '. $mysqli->error);
-					}
-					
-					*/
-					
-					echo '<pre>';
-					print_r($httpParsedResponseAr);
-					echo '</pre>';
-				} else  {
-					echo '<div style="color:red"><b>GetTransactionDetails failed:</b>'.urldecode($httpParsedResponseAr["L_LONGMESSAGE0"]).'</div>';
-					echo '<pre>';
-					print_r($httpParsedResponseAr);
-					echo '</pre>';
-
-				}
+	$full_fees = 0.00;
 	
-	}else{
-			echo '<div style="color:red"><b>Error : </b>'.urldecode($httpParsedResponseAr["L_LONGMESSAGE0"]).'</div>';
-			echo '<pre>';
-			print_r($httpParsedResponseAr);
-			echo '</pre>';
+	$stmt2 = $mysqli->prepare("UPDATE user_fees SET fee_amount = ? WHERE userid = ? LIMIT 1");
+	$stmt2->bind_param('ii', $full_fees, $userid);
+	$stmt2->execute();
+	$stmt2->close();
+	
+	include_once '../includes/paypal/paypal_success.php';
+	
+	} else {
+	
+	if ($product_amount == '4500.00' AND $half == '0' ) {
+	
+	$half_fees = 4500.00;
+	$half_fees1 = 1;
+	
+	$stmt3 = $mysqli->prepare("UPDATE user_fees SET fee_amount = ? WHERE userid = ? LIMIT 1");
+	$stmt3->bind_param('ii', $half_fees, $userid);
+	$stmt3->execute();
+	$stmt3->close();
+	
+	$stmt4 = $mysqli->prepare("UPDATE paypal_log SET half = ? WHERE userid = ? LIMIT 1");
+	$stmt4->bind_param('ii', $half_fees1, $userid);
+	$stmt4->execute();
+	$stmt4->close();
+	
+	include_once '../includes/paypal/paypal_success.php';
+
+	} else {
+	
+	$stmt5 = $mysqli->prepare("UPDATE user_fees SET fee_amount = ? WHERE userid = ? LIMIT 1");
+	$stmt5->bind_param('ii', $full_fees, $userid);
+	$stmt5->execute();
+	$stmt5->close();
+	
+	include_once '../includes/paypal/paypal_success.php';
 	}
+	}
+	break;
+	
+	case "cancel": // case cancel to show user the transaction was cancelled
+	
+	$paymentstatus1 = 'cancelled';
+	$cancelled_date = date("Y-m-d G:i:s");
+	
+	$stmt5 = $mysqli->prepare("UPDATE paypal_log SET payment_status = ?, cancelled_date=? WHERE userid = ? ORDER BY posted_date DESC LIMIT 1");
+	$stmt5->bind_param('ssi', $paymentstatus1, $cancelled_date, $userid);
+	$stmt5->execute();
+	$stmt5->close();
+	
+	include_once '../includes/paypal/paypal_cancel.php';
+	
+	break;
+	
+	case "ipn": // IPN case to receive payment information. this case will not displayed in browser. This is server to server communication. PayPal will send the transactions each and every details to this case in secured POST menthod by server to server. 
+		$transaction_id  = $_POST["txn_id"];
+		$payment_status = strtolower($_POST["payment_status"]);
+		$invoice_id = $_POST["invoice"];
+		
+		$completed_date = date("Y-m-d G:i:s");
+		
+	if ($p->validate_ipn()){ // validate the IPN, do the others stuffs here as per your app logic
+			
+	$stmt6 = $mysqli->prepare("UPDATE paypal_log SET transaction_id='$transaction_id', payment_status ='$payment_status', completed_date='$completed_date' WHERE invoice_id ='$invoice_id'");
+	$stmt6->execute();
+	$stmt6->close();
+			
+	$subject = 'Instant Payment Notification - Recieved Payment';
+	$p->send_report($subject); // Send the notification about the transaction
+	} else {
+	$subject = 'Instant Payment Notification - Payment Fail';
+	$p->send_report($subject); // failed notification
+	}
+	break;
 }
 ?>
