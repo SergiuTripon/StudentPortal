3344065ba3bfb39dc3687610d537ca89440b6624
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 19:32:23 +0000

    Various changes

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -1,5 +1,5 @@
 <!-- favicon -->
-<link rel="shortcut icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
 
 <!-- open-sans-font -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel="stylesheet">
