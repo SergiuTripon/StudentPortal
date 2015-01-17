cad045602adebdc02d15f1f2672f9811f432e9eb
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 19:39:06 +0000

    Various changes

--- assets/css-paths/calendar-css-path.php
+++ assets/css-paths/calendar-css-path.php
@@ -1,2 +1,2 @@
 <!-- bootstrap calendar -->
-<link href="https://sergiu-tripon.co.uk/assets/css/calendar/calendar.css" rel="stylesheet">
+<link href="https://student-portal.co.uk/assets/css/calendar/calendar.css" rel="stylesheet">
--- assets/js-paths/tilejs-js-path.php
+++ assets/js-paths/tilejs-js-path.php
@@ -1,2 +1,2 @@
 <!-- tile-js -->
-<script src="https://sergiu-tripon.co.uk/assets/js/tile-js/tileJs.min.js"></script>
+<script src="https://student-portal.co.uk/assets/js/tile-js/tileJs.min.js"></script>
