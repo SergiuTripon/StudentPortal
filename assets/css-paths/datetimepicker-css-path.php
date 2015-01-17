7248a778ae8554cbb994e613d0f49e46606dba3a
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 19:34:08 +0000

    Various changes

--- assets/css-paths/datetimepicker-css-path.php
+++ assets/css-paths/datetimepicker-css-path.php
@@ -1,3 +1,3 @@
 <!-- Date Picker CSS -->
-<link href="../assets/css/date-time-picker/jquery-ui-1.10.0.custom.css" rel="stylesheet">
-<link href="../assets/css/date-time-picker/jquery-ui-timepicker-addon.css" rel="stylesheet">
+<link href="https://sergiu-tripon.co.uk/assets/css/date-time-picker/jquery-ui-1.10.0.custom.css" rel="stylesheet">
+<link href="https://sergiu-tripon.co.uk/assets/css/date-time-picker/jquery-ui-timepicker-addon.css" rel="stylesheet">
