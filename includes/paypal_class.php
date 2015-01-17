a1ef2f805e7dc13681406e83a4a002117efb58b1
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 01:53:01 +0000

    Various changes

--- includes/paypal_class.php
+++ includes/paypal_class.php
@@ -70,11 +70,11 @@ class paypal_class {
 
 		echo "<script src=\"../assets/js/pace-js/pace.js\"></script>";
 
-		echo <meta charset=\"utf-8\">\n";
-    	echo <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
-    	echo <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
-    	echo <meta name=\"description\" content=\"\">\n";
-    	echo <meta name=\"author\" content=\"\">\n";
+		echo "<meta charset=\"utf-8\">\n";
+    	echo "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
+    	echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+    	echo "<meta name=\"description\" content=\"\">\n";
+    	echo "<meta name=\"author\" content=\"\">\n";
 
 		echo "<title>Processing Payment...</title>\n";
 
