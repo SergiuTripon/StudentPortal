3ce80254bb8da4eb7e51031fe5417530c81db250
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 13:56:05 +0000

    Various changes

--- includes/footers/portal_footer.php
+++ includes/footers/portal_footer.php
@@ -26,7 +26,7 @@
 	
 	<footer>
     <div class="container">
-	<div id="seal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=csOvqppO0S9h469AZ4vAXvtrvg75WztsHqitRvaKQvjaRfs62ebr39CvkC86"></script></div>
     <p>&copy; 2014 Student Portal - All Rights Reserved.</p>
-    </div>
+	<div id="seal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=csOvqppO0S9h469AZ4vAXvtrvg75WztsHqitRvaKQvjaRfs62ebr39CvkC86"></script></div>
+	</div>
     </footer>
