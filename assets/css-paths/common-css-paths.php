ed028a54ad147c1babd98444ce87bcb23907ad95
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 19:32:59 +0000

    Various changes

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -1,22 +1,22 @@
-<!-- favicon -->
-<link rel="shortcut icon" type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
 
-<!-- open-sans-font -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel="stylesheet">
+    <!-- open-sans-font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel="stylesheet">
 
-<!-- bootstrap -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- font-awesome -->
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- font-awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- ladda -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
+    <!-- ladda -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
 
-<!-- common -->
-<link href="https://student-portal.co.uk/assets/css/custom/custom.css" rel="stylesheet">
+    <!-- common -->
+    <link href="https://student-portal.co.uk/assets/css/custom/custom.css" rel="stylesheet">
 
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
