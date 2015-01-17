6d9984b4dd860e73ccd1ad278c6377bedf73ef9d
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-17 19:34:36 +0000

    Various changes

--- assets/js-paths/calendar-js-path.php
+++ assets/js-paths/calendar-js-path.php
@@ -1,5 +1,5 @@
 <!-- Bootstrap Calendar JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
-<script src="../assets/js/calendar/calendar.js"></script>
-<script src="../assets/js/calendar/calendar-app.js"></script>
+<script src="https://sergiu-tripon.co.uk/assets/js/calendar/calendar.js"></script>
+<script src="https://sergiu-tripon.co.uk/assets/js/calendar/calendar-app.js"></script>
