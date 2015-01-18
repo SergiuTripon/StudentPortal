dfcd1b2917a220af0951b67b1d24d480d4b25c3f
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-18 21:02:08 +0000

    Various changes

--- includes/signin.php
+++ includes/signin.php
@@ -1,4 +1,12 @@
 <?php
+
+if ($_SERVER['HTTPS'] != "on") {
+    echo "This is not HTTPS";
+} else {
+    echo "This is HTTPS";
+}
+
+
 if (session_status() == PHP_SESSION_NONE) {
 	session_cache_limiter('none');
     session_start();
