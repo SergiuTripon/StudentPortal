502d78cefdd833eeb43690746c64d2d4671d7fa1
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-18 22:03:18 +0000

    Changed the margin-bottom rule

--- pay-course-fees.php
+++ pay-course-fees.php
@@ -332,7 +332,7 @@ if ($fee_amount == "0.00") {
 
 	jQuery.ajax({
 	type: "POST",
-	url: "https://student-portal.co.uk/includes/paypal_process1.php",
+	url: "https://student-portal.co.uk/includes/paypal_process.php",
     data:'payment=' + payment + '&product_id=' + product_id + '&product_quantity=' + product_quantity + '&payer_email=' + payer_email + '&payer_phonenumber=' + payer_phonenumber + '&payer_address2=' + payer_address2 + '&payer_town=' + payer_town + payer_address2 + '&payer_firstname=' + payer_firstname + '&payer_surname=' + payer_surname + '&product_name=' + product_name + '&product_amount=' + product_amount + '&payer_address1=' + payer_address1 + '&payer_city=' + payer_city + '&payer_postcode=' + payer_postcode + '&payer_country=' + payer_country,
     success:function(response){
 		$("#error").hide();
