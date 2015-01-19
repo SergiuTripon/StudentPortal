3495db19c95790cfdfa5b7cbd860040dc23c446f
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-19 21:05:36 +0000

    Changed date format

--- calendar.php
+++ calendar.php
@@ -128,7 +128,7 @@ function fix_url($url) {
 
 	$stmt1 = $mysqli->query("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate, task_category FROM user_tasks where userid = '$userid' AND task_status = 'active'");
 
-	$url = fix_url($row["task_url"]);
+	$url1 = fix_url($row["task_url"]);
 
 	while($row = $stmt1->fetch_assoc()) {			
 	  echo '<tr id="task-'.$row["taskid"].'">
@@ -187,7 +187,7 @@ function fix_url($url) {
 
 	$stmt2 = $mysqli->query("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate, task_category FROM user_tasks where userid = '$userid' AND task_status = 'completed'");
 
-	$url = fix_url($row["task_url"]);
+	$url1 = fix_url($row["task_url"]);
 
 	while($row = $stmt2->fetch_assoc()) {
 	echo '<tr id="task-'.$row["taskid"].'">
