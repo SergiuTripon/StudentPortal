5231417641bd4981cc8955d745b8411db18b9438
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-23 16:30:28 +0000

    Changed structure

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -8,7 +8,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- font-awesome -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- ladda -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
