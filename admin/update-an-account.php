780c8519f0a0b713e766bee84909e6148f461580
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-23 17:41:51 +0000

    Changed structure

--- admin/update-an-account.php
+++ admin/update-an-account.php
@@ -5,7 +5,7 @@ if (isset($_POST["recordToUpdate"])) {
 
 $idToUpdate = filter_input(INPUT_POST, 'recordToUpdate', FILTER_SANITIZE_NUMBER_INT);
 
-$stmt1 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname FROM user_siginin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_sigin.userid = ? LIMIT 1");
+$stmt1 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname FROM user_siginin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
 $stmt1->bind_param('i', $idToUpdate);
 $stmt1->execute();
 $stmt1->store_result();
