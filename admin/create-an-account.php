b72092964d6d6e18a296a55641895fbd033bcdc6
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-23 17:25:08 +0000

    Changed structure

--- admin/create-an-account.php
+++ admin/create-an-account.php
@@ -192,7 +192,7 @@ include '../includes/signin.php';
 	<?php include '../includes/footers/portal_footer.php'; ?>
 
     <!-- Sign Out (Inactive) JS -->
-    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+    <script src="../../assets/js/custom/sign-out-inactive.js"></script>
 
     <?php else : ?>
 
@@ -220,7 +220,7 @@ include '../includes/signin.php';
     </header>
 
     <!-- Sign Out (Inactive) JS -->
-    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+    <script src="../../assets/js/custom/sign-out-inactive.js"></script>
 
     <?php endif; ?>
 	<?php else : ?>
