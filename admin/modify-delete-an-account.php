068bd42aaaa588bca8508462e08bbe73037c28a4
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-25 18:37:55 +0000

    Changed structure

--- admin/modify-delete-an-account.php
+++ admin/modify-delete-an-account.php
@@ -161,10 +161,6 @@ else $userid = '';
 
     <input type="hidden" name="degree" id="degree">
 
-    <div class="text-right">
-    <button id="FormSubmit" class="btn btn-custom btn-lg ladda-button mt10 mr5" data-style="slide-up" data-spinner-color="#FFA500"><span class="ladda-label">Update</span></button>
-    </div>
-
 	</div>
 
     </form>
