045a278562c4255e782387b0eb870b7b36a0c4e7
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-25 16:47:47 +0000

    Changed structure

--- admin/update-an-account.php
+++ admin/update-an-account.php
@@ -524,7 +524,7 @@ if ($dateofbirth == "0000-00-00") {
     jQuery.ajax({
 	type: "POST",
 	url: "https://student-portal.co.uk/includes/test_process.php",
-    data:'userid=' + userid + '&firstname=1' + firstname1 + '&surname1=' + surname1 + '&gender1=' + gender1 + '&dateofbirth1=' + dateofbirth1 + '&studentno1=' + studentno1 + '&degree1=' + degree1 + '&email1=' + email1 + '&phonenumber1=' + phonenumber1 + '&address11=' + address11 + '&address21=' + address21 + '&town1=' + town1 + '&city1=' + city1 + '&country1=' + country1 + '&postcode1=' + postcode1,
+    data:'userid=' + userid + '&firstname1=' + firstname1 + '&surname1=' + surname1 + '&gender1=' + gender1 + '&dateofbirth1=' + dateofbirth1 + '&studentno1=' + studentno1 + '&degree1=' + degree1 + '&email1=' + email1 + '&phonenumber1=' + phonenumber1 + '&address11=' + address11 + '&address21=' + address21 + '&town1=' + town1 + '&city1=' + city1 + '&country1=' + country1 + '&postcode1=' + postcode1,
     success:function(response){
 		$("#error").hide();
 		$("#hide").hide();
