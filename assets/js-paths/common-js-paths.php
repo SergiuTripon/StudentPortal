55b29332064ecc2aded44b672c43300f934c8c48
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-26 18:40:22 +0000

    Changing icons

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -8,6 +8,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
 
-<!-- common-->
+<!-- custom -->
 <script src="https://student-portal.co.uk/assets/js/custom/custom.js"></script>
+
+<!-- common-->
 <script src="https://student-portal.co.uk/assets/js/common/ie10-viewport-bug-workaround.js"></script>
