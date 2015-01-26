3ce65d0db67b25203dfdc0f7dc3abd097fcea41d
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-26 19:01:25 +0000

    Changing icons

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -1,4 +1,5 @@
-    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<!-- js library -->
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 
     <!-- bootstrap -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
