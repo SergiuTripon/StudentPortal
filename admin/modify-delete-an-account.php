b8f227f8f7630e8134e35771707793be065b9f2b
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-26 17:44:37 +0000

    Changed structure

--- admin/modify-delete-an-account.php
+++ admin/modify-delete-an-account.php
@@ -253,6 +253,7 @@ else $userid = '';
 			<div id="hide">
 			<a id="delete-'.$row["userid"].'" class="btn btn-custom btn-lg delete-button ladda-button" data-style="slide-up" data-spinner-color="#FFA500">Yes</a>
 			<button type="button" class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500" data-dismiss="modal">Back</button>
+			<a id="success-button" class="btn btn-custom btn-lg ladda-button" style="display: none;" data-style="slide-up" data-spinner-color="#FFA500">Continue</a>
 			</div>
 			</div>
 
@@ -411,6 +412,13 @@ else $userid = '';
 		$('.feedback-custom').removeClass('.feedback-custom');
 		$('.feedback-custom').addClass('.feedback-happy');
 		$('.feedback-custom').empty().append('The account has been deleted successfully.');
+		$('#success-button').show();
+		$("#success-button").click(function (e) {
+			e.preventDefault();
+			setTimeout(function(){
+				location.reload();
+			}, 1000);
+		}
 	},
 
 	error:function (xhr, ajaxOptions, thrownError){
