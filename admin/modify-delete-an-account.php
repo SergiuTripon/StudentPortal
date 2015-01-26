53af193e1555db9820e84425e70d546f07a228f0
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-26 16:13:28 +0000

    Changed structure

--- admin/modify-delete-an-account.php
+++ admin/modify-delete-an-account.php
@@ -272,11 +272,11 @@ else $userid = '';
 	<?php
 	$stmt5 = $mysqli->query("SELECT user_signin.userid FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE NOT user_signin.userid = '$userid'");
 	while($row = $stmt5->fetch_assoc()) {
-		echo '<a id="delete-'.$row["userid"].'" class="delete-button"><i class="fa fa-close"></i></a>';
+		echo '<a id="delete-'.$row["userid"].'" class="btn btn-custom btn-lg delete-button">Yes</a>';
 	}
 	$stmt5->close();
 	?>
-    <button type="button" class="btn btn-custom btn-lg" data-dismiss="modal">Back</button>
+    <button type="button" class="btn btn-custom btn-lg" data-dismiss="modal">No</button>
     </div>
 
 	</div><!-- /modal -->
