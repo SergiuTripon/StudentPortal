8238262ecc945688c2ab826216bb5b4519b13f33
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-26 18:50:42 +0000

    Changing icons

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -1,15 +1,15 @@
 <!-- js library -->
-<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
 
-<!-- bootstrap -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
-<!-- ladda -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
+    <!-- ladda -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
 
-<!-- custom -->
-<script src="https://student-portal.co.uk/assets/js/custom/custom.js"></script>
+    <!-- custom -->
+    <script src="https://student-portal.co.uk/assets/js/custom/custom.js"></script>
 
-<!-- common-->
-<script src="https://student-portal.co.uk/assets/js/common/ie10-viewport-bug-workaround.js"></script>
+    <!-- common-->
+    <script src="https://student-portal.co.uk/assets/js/common/ie10-viewport-bug-workaround.js"></script>
--- assets/js-paths/tilejs-js-path.php
+++ assets/js-paths/tilejs-js-path.php
@@ -1,2 +1,2 @@
-<!-- tile-js -->
-<script src="https://student-portal.co.uk/assets/js/tile-js/tileJs.min.js"></script>
+    <!-- tile-js -->
+    <script src="https://student-portal.co.uk/assets/js/tile-js/tileJs.min.js"></script>
