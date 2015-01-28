f79e0b519f050c7ed5af2e7b5d83c3f474fa56ff
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-28 20:26:14 +0000

    Changing structure

--- assets/js-paths/datatables-js-path.php
+++ assets/js-paths/datatables-js-path.php
@@ -1,2 +1,2 @@
     <!-- dataTables JS -->
-    <script src="https://student-portal.co.uk/js/data-tables/jquery.dataTables.min.js"></script>
+    <script src="https://student-portal.co.uk/assets/js/data-tables/jquery.dataTables.min.js"></script>
