181f050dafbe683e99f8233aa440e3b59ac341a4
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-28 19:57:30 +0000

    Changing structure

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -5,18 +5,18 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300" rel="stylesheet">
 
     <!-- bootstrap -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
 
     <!-- font-awesome -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/css/font-awesome/font-awesome.min.css" rel="stylesheet">
 
     <!-- ladda -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/ladda/ladda-themeless.min.css" rel="stylesheet">
 
     <!-- custom -->
     <link href="https://student-portal.co.uk/assets/css/custom/custom.css" rel="stylesheet">
 
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://student-portal.co.uk/js/html5shiv/html5shiv.min.js"></script>
+    <script src="https://student-portal.co.uk/js/respond/respond.min.js"></script>
     <![endif]-->
