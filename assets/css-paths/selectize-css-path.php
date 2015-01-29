a67c672663d096b33896a886186d5a709dcf2ce0
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-29 13:25:09 +0000

    Changing structure

--- assets/css-paths/selectize-css-path.php
+++
@@ -1,3 +0,0 @@
-    <!-- selectize -->
-    <link href="https://student-portal.co.uk/assets/css/selectize/selectize.default.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/selectize/selectize.bootstrap3.css" rel="stylesheet">
--- assets/js-paths/selectize-js-path.php
+++
@@ -1,2 +0,0 @@
-    <!-- selectize -->
-    <script src="https://student-portal.co.uk/assets/js/selectize/selectize.min.js"></script>
