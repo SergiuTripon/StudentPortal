68f0dfc114aecee0524bd0a0ce1716e0477415ca
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-01-29 00:09:38 +0000

    Changing structure

--- assets/css-paths/select2-css-path.php
+++ assets/css-paths/select2-css-path.php
@@ -1,3 +1,3 @@
     <!-- select2 -->
-    <link href="https://student-portal.co.uk/assets/css/select2/select2.css" rel="stylesheet">
     <link href="https://student-portal.co.uk/assets/css/select2/select2-bootstrap.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/select2/select2.css" rel="stylesheet">
