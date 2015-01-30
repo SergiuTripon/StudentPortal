e36f7b4d9f8c54c052fe25f63e5323b9805a2c7a
Sergiu Tripon <contact@sergiu-tripon.com>
2015-01-30 11:24:31 +0000

    Changed structure

--- calendar/create-task.php
+++ calendar/create-task.php
@@ -190,7 +190,7 @@ include '../includes/signin.php';
 
 	$(window).resize(function(){
 		var width = $(window).width();
-		if(width <= 340){
+		if(width <= 380){
 			$('.btn-group').removeClass('btn-group-justified');
 		} else {
 			$('.btn-group').addClass('btn-group-justified');
