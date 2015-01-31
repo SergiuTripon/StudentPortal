10e64d4b02976be8945b98547ea3fc0333c6db2a
Sergiu Tripon <contact@sergiu-tripon.com>
2015-01-31 21:22:29 +0000

    Changed structure

--- includes/processes.php
+++ includes/processes.php
@@ -11,7 +11,7 @@ if (isset($_POST['email'], $_POST['password'])) {
 
 //Call Register function
 //Call RegisterUser function
-if(isset($_POST["gender1"], $_POST["firstname1"], $_POST["surname1"], $_POST["studentno1"], $_POST["email1"], $_POST["password1"], $_POST["confirmpwd1"])) {
+if(isset($_POST["firstname"], $_POST["surname"], $_POST["gender"], $_POST["email1"], $_POST["studentno"], $_POST["password1"], $_POST["confirmpwd"])) {
 	RegisterUser();
 }
 
@@ -19,12 +19,12 @@ if(isset($_POST["gender1"], $_POST["firstname1"], $_POST["surname1"], $_POST["st
 
 //Call Forgotten Password/Password Reset functions
 //Call SendPasswordToken function
-if (isset($_POST["email3"])) {
+if (isset($_POST["email2"])) {
 	SendPasswordToken();
 }
 
 //Call ResetPassword function
-elseif (isset($_POST["toke4"], $_POST["email4"], $_POST["password4"], $_POST["confirmpwd4"])) {
+elseif (isset($_POST["token"], $_POST["email3"], $_POST["password2"], $_POST["confirmpwd1"])) {
 	ResetPassword();
 }
 
@@ -32,17 +32,17 @@ elseif (isset($_POST["toke4"], $_POST["email4"], $_POST["password4"], $_POST["co
 
 //Call Account functions
 //Call UpdateAccount function
-elseif (isset($_POST['gender5'], $_POST['firstname5'], $_POST['surname5'], $_POST['dateofbirth5'], $_POST['email5'], $_POST['phonenumber5'], $_POST['address5'], $_POST['address5'], $_POST['town5'], $_POST['city5'], $_POST['country5'], $_POST['postcode5'], $_POST['degree5'])) {
+elseif (isset($_POST['firstname1'], $_POST['surname1'], $_POST['gender1'], $_POST['dateofbirth'], $_POST['degree'], $_POST['email4'], $_POST['phonenumber'], $_POST['address1'], $_POST['address2'], $_POST['town'], $_POST['city'], $_POST['country'], $_POST['postcode'])) {
 	UpdateAccount();
 }
 
 //Call ChangePassword function
-elseif (isset($_POST["password6"], $_POST["confirmpwd6"])) {
+elseif (isset($_POST["password3"], $_POST["confirmpwd2"])) {
 	ChangePassword();
 }
 
 //Call DeleteAccount function
-elseif (isset($_POST['deleteaccount_button7'])) {
+elseif (isset($_POST['deleteaccount_button'])) {
 	DeleteAccount();
 }
 
@@ -50,22 +50,22 @@ elseif (isset($_POST['deleteaccount_button7'])) {
 
 //Call Admin Account functions
 //Call CreateAnAccount function
-elseif (isset($_POST['account_type8'], $_POST['gender8'], $_POST['firstname8'], $_POST['surname8'], $_POST['studentno8'], $_POST['email'], $_POST['password8'], $_POST['confirmpwd8'], $_POST['dateofbirth8'], $_POST['phonenumber8'], $_POST['degree8'], $_POST['address18'], $_POST['address28'], $_POST['town8'], $_POST['city8'], $_POST['country8'], $_POST['postcode8'])) {
+elseif (isset($_POST['account_type1'], $_POST['firstname2'], $_POST['surname2'], $_POST['gender2'], $_POST['dateofbirth1'], $_POST['studentno2'], $_POST['degree1'], $_POST['email5'], $_POST['phonenumber1'], $_POST['password4'], $_POST['confirmpwd3'], $_POST['address11'], $_POST['address21'], $_POST['town1'], $_POST['city1'], $_POST['country1'], $_POST['postcode1'])) {
 	CreateAnAccount();
 }
 
 //Call UpdateAnAccount function
-elseif (isset($_POST['userid9'], $_POST['firstname9'], $_POST['surname9'], $_POST['gender9'], $_POST['dateofbirth9'], $_POST['studentno9'], $_POST['degree9'], $_POST['email9'], $_POST['phonenumber9'], $_POST['address19'], $_POST['address29'], $_POST['town9'], $_POST['city9'], $_POST['country9'], $_POST['postcode9'])) {
+elseif (isset($_POST['userid'], $_POST['firstname3'], $_POST['surname3'], $_POST['gender3'], $_POST['dateofbirth2'], $_POST['studentno3'], $_POST['degree2'], $_POST['email6'], $_POST['phonenumber2'], $_POST['address12'], $_POST['address22'], $_POST['town2'], $_POST['city2'], $_POST['country2'], $_POST['postcode2'])) {
 	UpdateAnAccount();
 }
 
 //Call ChangeAccountPassword function
-elseif (isset($_POST["userid10"], $_POST["password10"], $_POST["confirmpwd10"])) {
+elseif (isset($_POST["userid1"], $_POST["password5"], $_POST["confirmpwd4"])) {
 	ChangeAccountPassword();
 }
 
 //Call DeleteAnAccount function
-elseif (isset($_POST["recordToDelete11"])) {
+elseif (isset($_POST["recordToDelete"])) {
 	DeleteAnAccount();
 }
 
@@ -73,16 +73,16 @@ elseif (isset($_POST["recordToDelete11"])) {
 
 //Call Calendar functions
 //Call CreateTask function
-elseif (isset($_POST['taskid12'], $_POST['task_name12'], $_POST['task_notes12'], $_POST['task_url12'], $_POST['task_startdate12'], $_POST['task_duedate13'], $_POST['task_category14'])) {
+elseif (isset($_POST['taskid'], $_POST['task_name'], $_POST['task_notes'], $_POST['task_url'], $_POST['task_startdate'], $_POST['task_duedate'], $_POST['task_category'])) {
 	CreateTask();
 }
 
 //Call UpdateTask function
-elseif (isset($_POST['taskid15'], $_POST['task_name15'], $_POST['task_notes15'], $_POST['task_url15'], $_POST['task_startdate15'], $_POST['task_duedate15'], $_POST['task_category15'])) {
+elseif (isset($_POST['taskid1'], $_POST['task_name1'], $_POST['task_notes1'], $_POST['task_url1'], $_POST['task_startdate1'], $_POST['task_duedate1'], $_POST['task_category1'])) {
 	UpdateTask();
 }
 
 //Call CompleteTask function
-elseif (isset($_POST["recordToComplete16"])) {
+elseif (isset($_POST["recordToComplete"])) {
 	CompleteTask();
 }
