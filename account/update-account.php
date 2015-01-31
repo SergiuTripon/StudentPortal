074ffded6f19cf049f0a1ea46450f442bb09ce65
Sergiu Tripon <contact@sergiu-tripon.com>
2015-01-31 23:02:50 +0000

    Changed structure

--- account/update-account.php
+++ account/update-account.php
@@ -259,8 +259,6 @@ if ($dateofbirth == "0000-00-00") {
 	</div>
     </div>
 
-    <input type="hidden" name="degree" id="degree">
-
     <div class="text-right">
     <button id="FormSubmit" class="btn btn-custom btn-lg ladda-button mt10 mr5" data-style="slide-up" data-spinner-color="#FFA500"><span class="ladda-label">Update</span></button>
     </div>
@@ -363,8 +361,6 @@ if ($dateofbirth == "0000-00-00") {
 	<input class="form-control" type="text" name="country" id="country" value="United Kingdom" placeholder="Enter your country" readonly="readonly">
 	<label>Postcode</label>
     <input class="form-control" type="text" name="postcode" id="postcode" value="<?php echo $postcode; ?>" placeholder="Enter your postcode">
-
-    <input type="hidden" name="degree" id="degree">
 
     <hr class="hr-custom">
 
