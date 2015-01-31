15c9f9bee281be07f4c7af70eea545efac4a1f59
Sergiu Tripon <contact@sergiu-tripon.com>
2015-01-31 14:19:54 +0000

    Changed structure

--- account/update-account.php
+++ account/update-account.php
@@ -371,8 +371,10 @@ if ($dateofbirth == "0000-00-00") {
 
     <input type="hidden" name="degree" id="degree">
 
-    <div class="text-right">
-    <button id="FormSubmit" class="btn btn-custom btn-lg ladda-button mt10 mr5" data-style="slide-up" data-spinner-color="#FFA500"><span class="ladda-label">Update</span></button>
+    <hr class="hr-custom">
+
+    <div class="text-center">
+    <button id="FormSubmit" class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500"><span class="ladda-label">Update account</span></button>
     </div>
 
 	</div>
