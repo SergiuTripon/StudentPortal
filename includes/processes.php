5a21c338ed89b3502972ddf1c1fb31bc6ba8707b
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-01 14:28:25 +0000

    Changed structure

--- includes/processes.php
+++ includes/processes.php
@@ -55,7 +55,7 @@ elseif (isset($_POST['account_type'], $_POST['firstname2'], $_POST['surname2'], 
 }
 
 //Call UpdateAnAccount function
-elseif (isset($_POST['userid'], $_POST['account_type1'], $_POST['firstname3'], $_POST['surname3'], $_POST['gender3'], $_POST['email6'], $_POST['studentno1'], $_POST['degree1'], $_POST['nationality2'], $_POST['dateofbirth2'], $_POST['phonenumber2'], $_POST['address12'], $_POST['address22'], $_POST['town2'], $_POST['city2'], $_POST['country2'], $_POST['postcode2'])) {
+elseif (isset($_POST['userid'], $_POST['account_type1'], $_POST['firstname3'], $_POST['surname3'], $_POST['gender3'], $_POST['studentno1'], $_POST['degree1'], $_POST['email6'], $_POST['nationality2'], $_POST['dateofbirth2'], $_POST['phonenumber2'], $_POST['address12'], $_POST['address22'], $_POST['town2'], $_POST['city2'], $_POST['country2'], $_POST['postcode2'])) {
 	UpdateAnAccount();
 }
 
