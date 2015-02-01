e5f66bf8ac9a1fdf3f9c99d107ea5a49bef74ffc
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-01 11:14:41 +0000

    Changed structure

--- admin/update-an-account.php
+++ admin/update-an-account.php
@@ -133,7 +133,12 @@ if ($dateofbirth == "0000-00-00") {
 	</div>
 	</div>
 
+	<br>
+
+	<h4 class="feedback-custom">Extras</h4>
 	<hr class="hr-thin">
+
+	<br>
 
 	<div class="form-group">
 	<div class="col-xs-6 col-sm-6 full-width pl0">
