2885d86ddccb13d263432a540d98149f6261c16c
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-02 21:57:06 +0000

    Changed structure

--- assets/js-paths/calendar-js-path.php
+++ assets/js-paths/calendar-js-path.php
@@ -2,3 +2,4 @@
     <script src="https://student-portal.co.uk/assets/js/calendar/jstz.min.js"></script>
     <script src="https://student-portal.co.uk/assets/js/calendar/underscore-min.js"></script>
     <script src="https://student-portal.co.uk/assets/js/calendar/calendar.js"></script>
+    <script src="https://student-portal.co.uk/assets/js/calendar/calendar-app.js"></script>
