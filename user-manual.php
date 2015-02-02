a070b9c08c953e58c947647b4ecc03084a2924dc
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-02 17:29:55 +0000

    Changed structure

--- user-manual.php
+++ user-manual.php
@@ -70,71 +70,14 @@ include 'includes/session.php';
 
 
 
-    <?php include 'includes/showcase/showcase.php'; ?>
-
 	<?php endif; ?>
 
     <?php include 'assets/js-paths/common-js-paths.php'; ?>
     <?php include 'assets/js-paths/easing-js-path.php'; ?>
-    <?php include 'assets/js-paths/tilejs-js-path.php'; ?>
 
 	<script>
     $(document).ready(function() {
 
-    //Ladda
-    Ladda.bind('.ladda-button', {timeout: 2000});
-
-    //Ajax call
-    $("#FormSubmit").click(function (e) {
-    e.preventDefault();
-
-	var hasError = false;
-
-	var email = $('#email').val();
-	if (email === '') {
-        $("#error").empty().append("Please enter an email address.");
-		$("#email").css("border-color", "#FF5454");
-		hasError  = true;
-		return false;
-	} else {
-		$("#error").hide();
-		$("#email").css("border-color", "#4DC742");
-	}
-
-	var password = $("#password").val();
-	if(password === '') {
-		$("#error").show();
-        $("#error").empty().append("Please enter a password.");
-		$("#password").css("border-color", "#FF5454");
-		hasError  = true;
-		return false;
-    } else {
-		$("#error").hide();
-		$("#password").css("border-color", "#4DC742");
-	}
-
-	if(hasError == false){
-    jQuery.ajax({
-	type: "POST",
-	url: "https://student-portal.co.uk/includes/processes.php",
-    data:'email=' + email + '&password=' + password,
-    success:function(){
-		window.location = '../overview/';
-    },
-    error:function (xhr, ajaxOptions, thrownError){
-		$("#error").show();
-        $("#error").empty().append(thrownError);
-    }
-	});
-    }
-
-	return true;
-
-	});
-	});
-	</script>
-
-	<script>
     // jQuery to collapse the navbar on scroll
     $(window).scroll(function () {
         if ($(".navbar").offset().top > 50) {
@@ -158,6 +101,8 @@ include 'includes/session.php';
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
+    });
+
     });
 	</script>
 
