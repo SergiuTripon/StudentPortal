9062c9795db7b4eef9d78ec79bed358174e698fa
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-02 16:37:11 +0000

    Changed structure

--- user-manual.php
+++ user-manual.php
@@ -94,7 +94,7 @@
     <i class="fa fa-fw fa-refresh"></i> How to update your account
     </li>
     </ol>
-    <h1 class="page-header">Account <small>How to update an account</small></h1>
+    <h2 class="page-header">Account <small>How to update an account</small></h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas bibendum libero sed nisl aliquet, bibendum dictum enim luctus. Suspendisse consequat fermentum tortor non egestas. Cras sed purus turpis.
     Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam non massa sit amet elit pretium maximus. Maecenas eu metus aliquam, gravida massa a, posuere odio. Maecenas dictum non odio eu commodo.
     Nunc scelerisque mauris ac ultricies laoreet. Nunc tincidunt consequat commodo. Etiam viverra ullamcorper metus ut porttitor. Vivamus a tortor at elit blandit congue.</p>
