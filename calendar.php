c3c5506f5e8c344149bf3b924a52968d4e692707
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-02 22:15:14 +0000

    Changed structure

--- calendar.php
+++ calendar.php
@@ -18,10 +18,10 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
 	<meta http-equiv="Pragma" content="no-cache">
 	<meta http-equiv="Expires" content="-1">
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+	<meta charset="utf-8">
+	<meta name="description" content="">
     <meta name="author" content="">
 
 	<?php include 'assets/css-paths/datatables-css-path.php'; ?>
