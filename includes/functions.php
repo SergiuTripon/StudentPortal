4bc6172b6cc8ee2008f66c75fdd29fc8d1b881e2
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-03 17:17:56 +0000

    Created events page

--- includes/functions.php
+++ includes/functions.php
@@ -1032,7 +1032,7 @@ function EventsPaypalPaymentSuccess() {
 	$payment_status1 = ($_POST["payment_status"]);
 	$payment_date = date('H:i d/m/Y', strtotime($_POST["payment_date"]));
 
-	$stmt1 = $mysqli->prepare("INSERT INTO booked_events (userid, eventid, event_name, event_amount, tickets_quantity, booked_on) VALUES ('2', '$item_number1', '$product_name', '$product_amount', '$quantity1', '$created_on')");
+	$stmt1 = $mysqli->prepare("INSERT INTO booked_events (userid, eventid, event_name, event_amount, tickets_quantity, booked_on) VALUES ('$userid', '$item_number1', '$product_name', '$product_amount', '$quantity1', '$created_on')");
 	$stmt1->execute();
 	$stmt1->close();
 
