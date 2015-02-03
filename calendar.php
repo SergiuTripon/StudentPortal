cf3e44aa40df1450d44d80102873c4cd91a83e9e
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-03 14:17:35 +0000

    Created events page

--- calendar.php
+++ calendar.php
@@ -291,9 +291,49 @@ include 'includes/session.php';
 	<?php endif; ?>
 
 	<?php include 'assets/js-paths/common-js-paths.php'; ?>
-	<?php include 'assets/js-paths/calendar-js-path.php'; ?>
 	<?php include 'assets/js-paths/tilejs-js-path.php'; ?>
 	<?php include 'assets/js-paths/datatables-js-path.php'; ?>
+	<?php include 'assets/js-paths/calendar-js-path.php'; ?>
+
+	<script>
+	(function($) {
+
+	"use strict";
+
+	var options = {
+		events_source: '../../includes/tasks_json.php',
+		view: 'month',
+		tmpl_path: '../assets/tmpls/',
+		tmpl_cache: false,
+		onAfterViewLoad: function(view) {
+			$('.page-header h3').text(this.getTitle());
+			$('.btn-group button').removeClass('active');
+			$('button[data-calendar-view="' + view + '"]').addClass('active');
+		},
+		classes: {
+			months: {
+				general: 'label'
+			}
+		}
+	};
+
+	var calendar = $('#calendar').calendar(options);
+
+	$('.btn-group button[data-calendar-nav]').each(function() {
+		var $this = $(this);
+		$this.click(function() {
+			calendar.navigate($this.data('calendar-nav'));
+		});
+	});
+
+	$('.btn-group button[data-calendar-view]').each(function() {
+		var $this = $(this);
+		$this.click(function() {
+			calendar.view($this.data('calendar-view'));
+		});
+	});
+	}(jQuery));
+	</script>
 
 	<script type="text/javascript" class="init">
     $(document).ready(function () {
