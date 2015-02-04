adc40c8191bf67b8a6cd9e6a0affef217ae94756
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-04 22:59:53 +0000

    Removed showcase from the files that were using it

--- contact.php
+++ contact.php
@@ -157,26 +157,26 @@ include 'includes/session.php';
     <div id="hide">
 
 	<div class="form-group">
-	<div class="col-sm-4 full-width pl0">
+	<div class="col-xs-4 col-sm-4 full-width pl0">
 	<input class="form-control" type="text" name="firstname" id="firstname" placeholder="First name">
     </div>
 	</div>
 
 	<div class="form-group">
-	<div class="col-sm-4 full-width">
+	<div class="col-xs-4 col-sm-4 full-width">
     <input class="form-control" type="text" name="surname" id="surname" placeholder="Surname">
 	</div>
 	</div>
 
 	<div class="form-group">
-	<div class="col-sm-4 full-widthpr0">
+	<div class="col-xs-4 col-sm-4 full-widthpr0">
     <input class="form-control" type="email" name="email" id="email" placeholder="E-mail address">
 	</div>
 	</div>
     <p id="error1" class="feedback-sad"></p>
 
 	<div class="form-group">
-	<div class="col-sm-12 full-width">
+	<div class="col-xs-12 col-sm-12 full-width">
 	<textarea class="form-control" rows="8" name="message" id="message" placeholder="Message"></textarea>
 	</div>
 	</div>
