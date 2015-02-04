fec10086a093254531ac8e44019253fb6b32edbf
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-04 21:00:50 +0000

    Trying a new style

--- contact.php
+++ contact.php
@@ -129,7 +129,7 @@ include 'includes/session.php';
 
     <div class="text-center">
     <div class="logo-custom">
-    <i class="fa fa-graduation-cap" style="font-size: 150px;"></i>
+    <i class="fa fa-envelope" style="font-size: 150px;"></i>
     </div>
     <a href="#contact1" class="btn btn-circle page-scroll">
     <i class="fa fa-angle-double-down animated"></i>
