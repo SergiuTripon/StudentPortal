3c9e3619394a2c04b11ac90c8b4f00b2d3987896
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-06 21:32:59 +0000

    Changed structure

--- includes/menus/menu.php
+++ includes/menus/menu.php
@@ -17,6 +17,15 @@
     <ul class="nav navbar-nav navbar-right">
         <li><a href="/">Sign In</a></li>
         <li><a href="/register/">Register</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Fidn out more <span class="fa fa-caret-down"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            <li><a href="/about/">About</a></li>
+            <li><a href="/features/">Features</a></li>
+            <li><a href="/user-manual/">User manual</a></li>
+            <li><a href="/contact/">Contact</a></li>
+          </ul>
+        </li>
     </ul>
     </div><!--/.nav-collapse -->
     </div><!--/.container -->
