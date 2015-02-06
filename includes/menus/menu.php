402599cab92a95726534ceee3f9cc515c91c0ece
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-06 00:01:58 +0000

    Changed structure

--- includes/menus/menu.php
+++ includes/menus/menu.php
@@ -115,6 +115,20 @@
 
 	<li><a id="contact" href="/contact/">Contact</a></li>
 
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		<ul class="dropdown-menu" role="menu">
+			<li><a href="#">Action</a></li>
+			<li><a href="#">Another action</a></li>
+			<li><a href="#">Something else here</a></li>
+			<li class="divider"></li>
+			<li class="dropdown-header">Nav header</li>
+			<li><a href="#">Separated link</a></li>
+			<li><a href="#">One more separated link</a></li>
+		</ul>
+	</li>
+
+
 	</ul>
     </div>
     <!-- /.navbar-collapse -->
