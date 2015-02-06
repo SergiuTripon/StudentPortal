2f9290e5ef9e13dc446b4dca6856dcdecda27409
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-06 20:41:01 +0000

    Changed structure

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,7 +1,26 @@
     <!-- Footer -->
     <footer>
-    <div class="container">
-    <p>Place sticky footer content here.</p>
-    </div>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Toggle -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- End of Toggle -->
+                    <a class="navbar-brand" href="#"><i class="fa fa-graduation-cap"></i> Student Portal</a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/">Sign In</a></li>
+                        <li><a href="/register/">Register</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container -->
+        </nav>
     </footer>
     <!-- End of Footer -->
