8c60d1f359f841e2e90a0688ce533639888da09a
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-06 20:44:20 +0000

    Changed structure

--- includes/footers/footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- Footer -->
-    <footer>
-    <div class="container">
-    <p class="pull-right">Place sticky footer content here.</p>
-    <p class="text-right">Place sticky footer content here.</p>
-    </div>
-    </footer>
-    <!-- End of Footer -->
