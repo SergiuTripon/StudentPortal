337145f467dd264347809fe2ac02cce55d6dd959
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-06 23:36:51 +0000

    Changed structure

--- password-reset.php
+++ password-reset.php
@@ -15,51 +15,45 @@ include 'includes/session.php';
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <?php include 'assets/css-paths/common-css-paths.php';?>
+    <?php include 'assets/css-paths/common-css-paths.php'; ?>
 
-    <title>Student Portal | Password Reset</title>
-	
-	<style>
-	html, body {
-		height: 100% !important;
-	}
-	</style>
-	
+    <title>Student Portal</title>
+
 </head>
 
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-	
+<body>
+
 	<div class="preloader"></div>
 
 	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 
-    <header class="intro">
-    <div class="intro-body">
+    <header><!-- Header -->
+    <div class="wrapper-body"><!-- Wrapper -->
 
     <form class="form-custom">
 
-    <div class="logo-custom animated fadeIn delay">
-	<i class="fa fa-graduation-cap"></i>
+    <div class="form-logo">
+    <i class="fa fa-graduation-cap"></i>
     </div>
 
     <hr class="hr-custom">
 
-    <p class="feedback-sad text-center">You are already signed in which means you haven't forgotten your password.<br><br>If you have, you can sign out and access the "Forgotten your password?" facility.</p>
+    <p class="feedback-sad text-center">You are already logged in. You don't have to log in again.</p>
 
     <hr class="hr-custom">
 
-    <div class="pull-left">
-    <a class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500" href="overview/"><span class="ladda-label">Overview</span></a>
+	<div class="pull-left">
+    <a class="btn btn-success btn-lg ladda-button" data-style="slide-up" href="overview/"><span class="ladda-label">Overview</span></a>
     </div>
-	
+
     <div class="text-right">
-    <a class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500" href="../sign-out/"><span class="ladda-label">Sign Out</span></a>
+    <a class="btn btn-danger btn-lg ladda-button" data-style="slide-up" href="sign-out/"><span class="ladda-label">Sign Out</span></a>
     </div>
 
-	</form>
+    </form>
 
-    </div><!-- /intro-body -->
-    </header>
+    </div><!-- End of Wrapper -->
+    </header><!-- End of Header -->
 
     <!-- Sign Out (Inactive) JS -->
     <script src="../assets/js/sign-out-inactive.js"></script>
@@ -69,15 +63,15 @@ include 'includes/session.php';
     <?php include 'includes/menus/menu.php'; ?>
 
     <header><!-- Header -->
-    <div class="wrapper"><!-- Wrapper -->
+    <div class="wrapper-body"><!-- Wrapper-->
 
-    <form class="form-custom" method="post" name="passwordreset_form">
+    <form class="form-custom" name="signin_form" id="signin_form">
 
-    <div class="logo-custom animated fadeIn delay">
+    <div class="form-logo">
 	<i class="fa fa-key"></i>
     </div>
 
-    <hr>
+    <hr class="hr-custom">
 
     <p id="error" class="feedback-sad text-center"></p>
 	<p id="success" class="feedback-happy text-center"></p>
@@ -109,9 +103,9 @@ include 'includes/session.php';
     <p id="error2" class="feedback-sad text-center"></p>
 
     <input class="form-control" type="hidden" name="token" id="token" value="<?php echo $token; ?>">
-	
+
 	</div>
-	
+
 	<hr class="hr-custom">
 
     <?php if (!empty($success_footer)) {
@@ -123,9 +117,9 @@ include 'includes/session.php';
     </div>
 
     <div id="register-button" class="text-right">
-	<button id="FormSubmit" class="btn btn-primary btn-lg ladda-button" data-style="slide-up"><span class="ladda-label">Continue</span></button>
+	<button id="FormSubmit" class="btn btn-primary btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500"><span class="ladda-label">Continue</span></button>
     </div>
-	
+
 	<div id="success-button" class="text-center" style="display:none;">
     <a class="btn btn-success btn-lg ladda-button" data-style="slide-up" href="/"><span class="ladda-label">Sign In</span></a>
     </div>
@@ -134,6 +128,8 @@ include 'includes/session.php';
 
     </div><!-- End of Wrapper -->
     </header><!-- End of Header -->
+
+    <?php include 'includes/footers/footer.php'; ?>
 
 	<?php endif; ?>
 
@@ -145,104 +141,44 @@ include 'includes/session.php';
     $(document).ready(function() {
 
     //Ladda
-    Ladda.bind('.ladda-button', {timeout: 1000});
+    Ladda.bind('.ladda-button', {timeout: 2000});
 
     //Ajax call
     $("#FormSubmit").click(function (e) {
     e.preventDefault();
-	
+
 	var hasError = false;
-	
-	var token = $("#token").val();
-	
-	var email3 = $("#email").val();
-	if(email3 === '') {
-		$("#error1").show();
+
+	var email = $('#email').val();
+	if (email === '') {
         $("#error1").empty().append("Please enter an email address.");
-		$("#email").css("border-color", "#FF5454");
+		$("#email").addClass("error-style");
 		hasError  = true;
 		return false;
 	} else {
 		$("#error1").hide();
-		$("#email").css("border-color", "#4DC742");
+		$("#email").addClass("success-style");
 	}
-	
-	var password2 = $("#password").val();
-	if(password2 === '') {
+
+	var password = $("#password").val();
+	if(password === '') {
 		$("#error2").show();
         $("#error2").empty().append("Please enter a password.");
-		$("#password").css("border-color", "#FF5454");
+		$("#password").addClass("error-style");
 		hasError  = true;
 		return false;
     } else {
 		$("#error2").hide();
-		$("#password").css("border-color", "#4DC742");
+		$("#password").addClass("success-style");
 	}
-	
-	if (password2.length < 6) {
-		$("#error2").show();
-		$("#error2").empty().append("Passwords must be at least 6 characters long. Please try again.");
-		$("#password").css("border-color", "#FF5454");
-		hasError  = true;
-		return false;
-	} else {
-		$("#error2").hide();
-		$("#password").css("border-color", "#4DC742");
-	}
-	
-	var upperCase= new RegExp('[A-Z]');
-	var lowerCase= new RegExp('[a-z]');
-	var numbers = new RegExp('[0-9]');
-	
-	if(password2.match(upperCase) && password2.match(lowerCase) && password2.match(numbers)) {
-		$("#error2").hide();
-		$("#password").css("border-color", "#4DC742");
-	} else {
-		$("#error2").show();
-		$("#error2").empty().append("Passwords must contain at least one number, one lowercase and one uppercase letter. Please try again.");
-		$("#password").css("border-color", "#FF5454");
-		hasError  = true;
-		return false;
-	}
-	
-	var confirmpwd = $("#confirmpwd").val();
-	if(confirmpwd === '') {
-		$("#error2").show();
-        $("#error2").empty().append("Please enter a password confirmation.");
-		$("#confirmpwd").css("border-color", "#FF5454");
-		hasError  = true;
-		return false;
-    } else {
-		$("#error2").hide();
-		$("#confirmpwd").css("border-color", "#4DC742");
-	}
-	
-	if(password2 != confirmpwd) {
-		$("#error2").show();
-		$("#error2").empty().append("Your password and confirmation do not match. Please try again.");
-		$("#password").css("border-color", "#FF5454");
-		$("#confirmpwd").css("border-color", "#FF5454");
-        hasError  = true;
-		return false;
-	} else {
-		$("#error2").hide();
-		$("#password").css("border-color", "#4DC742");
-		$("#confirmpwd").css("border-color", "#4DC742");
-	}
-	
+
 	if(hasError == false){
     jQuery.ajax({
 	type: "POST",
 	url: "https://student-portal.co.uk/includes/processes.php",
-    data:'token=' + token + '&email3=' + email3 + '&password2=' + password2,
+    data:'email=' + email + '&password=' + password,
     success:function(){
-		$("#hide").hide();
-		$("#signin-button").hide();
-		$("#FormSubmit").hide();
-		$("#email").hide();
-		$("#error").hide();
-		$("#success").append('Your password has been reset successfully. You can now sign in with your new password.');
-		$("#success-button").show();
+		window.location = '../overview/';
     },
     error:function (xhr, ajaxOptions, thrownError){
 		$("#error").show();
@@ -250,101 +186,11 @@ include 'includes/session.php';
     }
 	});
     }
-	
+
 	return true;
-	
+
 	});
 	});
-	</script>
-
-	<script>
-    // jQuery to collapse the navbar on scroll
-    $(window).scroll(function () {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
-    });
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function () {
-        $('a.page-scroll').bind('click', function (event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function () {
-        $('.navbar-toggle:visible').click();
-    });
-	</script>
-
-	<script>
-    $( "#timetable" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Timetable");
-        $('.close').empty().append("<i class=\"fa fa-table\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#exams" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Exams");
-        $('.close').empty().append("<i class=\"fa fa-pencil\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#library" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Library");
-        $('.close').empty().append("<i class=\"fa fa-book\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#transport" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Transport");
-        $('.close').empty().append("<i class=\"fa fa-bus\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#calendar" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Calendar");
-        $('.close').empty().append("<i class=\"fa fa-calendar\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#events" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Events");
-        $('.close').empty().append("<i class=\"fa fa-beer\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#universitymap" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("University Map");
-        $('.close').empty().append("<i class=\"fa fa-map-marker\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#feedback" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Feedback");
-        $('.close').empty().append("<i class=\"fa fa-check-square-o\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#messenger" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Messenger");
-        $('.close').empty().append("<i class=\"fa fa-comments\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
-    $( "#account" ).click(function() {
-        $('#modal-custom').modal('show');
-        $('#modal-custom-label').empty().append("Account");
-        $('.close').empty().append("<i class=\"fa fa-user\"></i>");
-        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis non ante at sollicitudin. Curabitur lorem massa, malesuada sed dapibus at, euismod nec turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur metus arcu, varius eu facilisis sit amet, rutrum eget ligula. Cras tempor sapien at massa pellentesque, fermentum placerat arcu iaculis. Nullam iaculis elit felis, ut vestibulum nisl ornare in. Nam eu orci vitae justo ullamcorper mollis at eget nisi. Pellentesque eleifend massa eget nunc sagittis porta. Nulla a feugiat nisl. Donec turpis ante, mollis a urna nec, mollis bibendum neque. Phasellus in varius metus. Suspendisse nec maximus magna. Sed rhoncus tincidunt turpis at condimentum. Donec a facilisis nisl.</p>");
-    });
 	</script>
 
 </body>
