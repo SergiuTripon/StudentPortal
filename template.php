a90ca633cbe2e7305f8fe21aff2113d41e499a2e
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-07 12:33:21 +0000

    Changed structure

--- template.php
+++ template.php
@@ -20,29 +20,20 @@
     <![endif]-->
 
     <style>
-    /* Sticky footer styles
-    -------------------------------------------------- */
     html {
         position: relative;
         min-height: 100%;
     }
     body {
-        /* Margin bottom by footer height */
         margin-bottom: 60px;
     }
     footer {
         position: absolute;
         bottom: 0;
         width: 100%;
-        /* Set the fixed height of the footer here */
         height: 60px;
         background-color: #f5f5f5;
     }
-
-
-    /* Custom page CSS
-    -------------------------------------------------- */
-    /* Not required for template or sticky footer method. */
 
     body > .container {
         padding: 60px 15px 0;
