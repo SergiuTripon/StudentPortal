d20b4354532b9015cde361eafd0f5b8d753d0d5e
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-07 11:48:06 +0000

    Changed structure

---
+++ includes/footers/footer.php
@@ -0,0 +1,33 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+    <div class="navbar-header">
+    <!-- Toggle -->
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    </button>
+    <!-- End of Toggle -->
+    <a class="navbar-brand" href=""><i class="fa fa-graduation-cap"></i> Student Portal</a>
+    </div>
+
+    <div id="navbar1" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li id="signin"><a href="/">Sign In</a></li>
+        <li><a href="/register/">Register</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Find out more <span class="fa fa-caret-down"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            <li><a href="/about/">About</a></li>
+            <li><a href="/features/">Features</a></li>
+            <li><a href="/user-manual/">User manual</a></li>
+            <li><a href="/contact/">Contact</a></li>
+          </ul>
+        </li>
+    </ul>
+    </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+    </nav>
+    <!-- End of Navigation -->
