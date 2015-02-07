75690bce21ed126780f49d8dfd93e0110f28a64b
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-07 13:06:10 +0000

    Changed structure

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,6 +1,11 @@
     <footer>
     <div class="container-fluid">
     <p>&copy; 2014 Student Portal - All Rights Reserved.</p>
+    <div class="text-right">
     <a href="/about/">About</a>
+    <a href="/features/">Features</a>
+    <a href="/user-manual/">User Manual</a>
+    <a href="/contact/">Contact</a>
+    </div>
     </div>
     </footer>
