20280d42fb4c00a4b3aa1d3a22fb427c6a7cc16e
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-07 02:16:14 +0000

    Changed structure

--- includes/menus/menu.php
+++ includes/menus/menu.php
@@ -15,7 +15,7 @@
 
     <div id="navbar" class="navbar-collapse collapse">
     <ul class="nav navbar-nav navbar-right">
-        <li><a href="/">Sign In</a></li>
+        <li id="signin"><a href="/">Sign In</a></li>
         <li><a href="/register/">Register</a></li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Find out more <span class="fa fa-caret-down"></span></a>
