d9a5a523089c7756d3758daacf0367747b46b0fa
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-07 00:12:59 +0000

    Changed structure

---
+++ includes/footers/footer.php
@@ -0,0 +1,26 @@
+    <style>
+    /* Footer */
+    footer {
+        color: #FFA500;
+        text-align: center;
+        border-top: 1px solid #FFA500;
+    }
+    footer .container {
+        padding-top: 20px;
+        padding-bottom: 10px;
+    }
+    footer p {
+        color: #FFA500;
+        text-align: center;
+    }
+    /* End of Footer */
+    </style>
+
+    <!-- Footer -->
+    <footer>
+    <div class="container">
+    <div class="row">
+    <p>&copy; 2014 Student Portal - All Rights Reserved.</p>
+    </div>
+    </div><!-- /.container -->
+    </footer><!-- /.footer -->
