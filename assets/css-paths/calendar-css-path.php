a979d11d67c4825ac597a463c71cc4ccb91a6e46
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-08 16:56:34 +0000

    Changed structure

--- assets/css-paths/calendar-css-path.php
+++ assets/css-paths/calendar-css-path.php
@@ -1,2 +1,2 @@
     <!-- bootstrap calendar -->
-    <link href="https://student-portal.co.uk/assets/css/calendar/calendar.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/calendar/calendar.min.css" rel="stylesheet">
