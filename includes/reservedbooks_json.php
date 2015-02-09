1e5578a336355b53a7f8813b6fa6b88cb1d3d779
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-09 12:27:33 +0000

    Deleted calendar.css

--- includes/reservedbooks_json.php
+++ includes/reservedbooks_json.php
@@ -3,7 +3,7 @@ include 'session.php';
 
 header("Cache-Control: no-cache, must-revalidate");
 
-$sql = 'SELECT bookid, book_name, book_author, book, task_duedate FROM user_tasks WHERE userid = "'.$userid.'" AND task_status = "active"';
+$sql = 'SELECT reserved_books.bookid, reserved_books.reserved_on, system_books.toreturn_on,  system_books.book_name FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid WHERE reserved_books.userid = "'.$userid.'" AND reserved_books.isReturned = "0" AND system_books.book_status = "active"';
 
 $res = $pdo->query($sql);
 $res->setFetchMode(PDO::FETCH_OBJ);
@@ -13,8 +13,8 @@ foreach($res as $row)
 {
     $out[] = array(
         'id'    => $row->taskid,
-        'title' => $row->task_name,
-        'url'   => $row->task_url,
+        'title' => $row->book_name,
+        'url'   => $row->book_author,
         'class' => $row->task_class,
         'start' => strtotime($row->task_startdate) . '000',
         'end'   => strtotime($row->task_duedate) .'000'
