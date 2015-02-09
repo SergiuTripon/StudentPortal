a1e5b5b0d634bf8c9be3e23b67d087594695ae50
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-09 12:53:54 +0000

    Deleted calendar.css

--- library.php
+++ library.php
@@ -57,6 +57,138 @@ include 'includes/session.php';
 
 	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
+	<div id="books-content" class="panel panel-default">
+
+	<?php
+	$stmt2 = $mysqli->query("SELECT bookid FROM system_books WHERE book_status = 'active'");
+	while($row = $stmt2->fetch_assoc()) {
+
+        $bookid = $row["bookid"];
+
+	  echo '<form id="reserve-book-form-'.$bookid.'" style="display: none;" action="/events/reserve-book/" method="POST">
+			<input type="hidden" name="recordToReserve" id="recordToReserve" value="'.$bookid.'"/>
+			</form>';
+	}
+	$stmt2->close();
+	?>
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Books - click to minimize or maximize</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Books -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom events-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Author</th>
+	<th>Notes</th>
+	<th>Available</th>
+	<th>Reserve</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT bookid, book_name, book_author, book_notes, book_quantity FROM system_books WHERE book_status = 'active'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$bookid = $row["bookid"];
+	$book_name = $row["book_name"];
+	$book_author = $row["book_author"];
+	$book_notes = $row["book_notes"];
+    $book_quantity = $row["book_quantity"];
+
+	echo '<tr id="task-'.$row["eventid"].'">
+
+			<td data-title="Name">'.$book_name.'</td>
+			<td data-title="Author">'.$book_author.'</td>
+			<td data-title="Notes">'.$book_notes.'</td>
+			<td data-title="Available">'.$book_quantity.'</td>
+			<td id="book-hide" data-title="Reserve"><a id="book-'.$bookid.'" class="reserve-button"><i class="fa fa-arrow-right"></i></a></td>
+			</tr>';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	<div id="reservedbooks-content" class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingTwo">
+  	<h4 class="panel-title">
+	<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Reserved books - click to minimize or maximize</a>
+  	</h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+  	<div class="panel-body">
+
+	<!-- Reserved books -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom bookedevents-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Price</th>
+	<th>Quality</th>
+	<th>Booked on</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt2 = $mysqli->query("SELECT reserved_books.bookid, DATE_FORMAT(reserved_books.reserved_on,'%d %b %y %H:%i') as reserved_on, DATE_FORMAT(reserved_books.toreturn_on,'%d %b %y %H:%i') as toreturn_on, system_books.book_name, system_books.book_author, system_books.book_notes FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid  WHERE reserved_books.userid = '$userid' AND system_books.book_status = 'active' AND isReturned = '0'");
+
+	while($row = $stmt2->fetch_assoc()) {
+
+    $book_name = $row["book_name"];
+    $book_author = $row["book_author"];
+    $book_notes = $row["book_notes"];
+    $reserved_on = $row["reserved_on"];
+    $toreturn_on = $row["toreturn_on"];
+
+	echo '<tr>
+
+			<td data-title="Name">'.$book_name.'</td>
+			<td data-title="Price">'.$book_author.'</td>
+			<td data-title="Quantity">'.$book_notes.'</td>
+			<td data-title="From">'.$reserved_on.'</td>
+			<td data-title="From">'.$toreturn_on.'</td>
+			</tr>';
+	}
+
+	$stmt2->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	</div><!-- /panel-group -->
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
 	<div id="calendar-content" class="panel panel-default">
 	<div class="panel-heading" role="tab" id="headingThree">
 	<h4 class="panel-title">
@@ -206,30 +338,47 @@ include 'includes/session.php';
 	});
 
 	//Book event form submit
-	$("body").on("click", ".book-button", function(e) {
+	$("body").on("click", ".reserve-button", function(e) {
     e.preventDefault();
 
 	var clickedID = this.id.split('-');
     var DbNumberID = clickedID[1];
 
-    jQuery.ajax({
-	type: "POST",
-	url: "https://student-portal.co.uk/includes/processes.php",
-    data:'events_soldout_check=' + DbNumberID,
-    success:function(msg){
-		if (msg == 'error') {
-			$("#error").empty().append("The quantity entered exceeds the amount of tickets available.<br>You can check the ticket availability on the Events page.");
-		} else {
-			$("#book-event-form-" + DbNumberID).submit();
-		}
-    },
-    error:function (xhr, ajaxOptions, thrownError){
-        $("#error").show();
-        $("#error").empty().append(thrownError);
-    }
-	});
+    $("#reserve-book-form-" + DbNumberID).submit();
+
 	});
 
+	//Event view/Calendar view toggle
+	$("#calendar-content").hide();
+	$(".book-tile").addClass("tile-selected");
+	$(".book-tile p").addClass("tile-text-selected");
+	$(".book-tile i").addClass("tile-text-selected");
+
+	$("#books-toggle").click(function (e) {
+    e.preventDefault();
+		$("#calendar-content").hide();
+		$("#books-content").show();
+		$("#reservedbooks-content").show();
+		$(".calendar-tile").removeClass("tile-selected");
+		$(".calendar-tile p").removeClass("tile-text-selected");
+		$(".calendar-tile i").removeClass("tile-text-selected");
+		$(".book-tile").addClass("tile-selected");
+		$(".book-tile p").addClass("tile-text-selected");
+		$(".book-tile i").addClass("tile-text-selected");
+	});
+
+	$("#calendar-toggle").click(function (e) {
+    e.preventDefault();
+		$("#books-content").hide();
+		$("#reservedbooks-content").hide();
+		$("#calendar-content").show();
+		$(".book-tile").removeClass("tile-selected");
+		$(".book-tile p").removeClass("tile-text-selected");
+		$(".book-tile i").removeClass("tile-text-selected");
+		$(".calendar-tile").addClass("tile-selected");
+		$(".calendar-tile p").addClass("tile-text-selected");
+		$(".calendar-tile i").addClass("tile-text-selected");
+	});
 
 	});
 	</script>
