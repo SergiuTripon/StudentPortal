bae7772dd512ce3f2fd11e2372a3d3b95935a216
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-10 19:34:16 +0000

    Changed structure

--- library.php
+++ library.php
@@ -109,7 +109,7 @@ include 'includes/session.php';
 	$book_copy_no = $row["book_copy_no"];
 	$book_status = $row["book_status"];
 
-	if ($book_status = 'Reserved') {
+	if ($book_status = 'reserved') {
 		$book_reserved_style = "<style> #reserve-$bookid { display: none; } </style>";
 	}
 
