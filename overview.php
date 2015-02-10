0ec42949be8fbcd629b1ce9561ee44c53dee44a0
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-10 17:12:34 +0000

    Changed structure

--- overview.php
+++ overview.php
@@ -238,7 +238,7 @@ include 'includes/session.php';
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Exams - click to minimize or maximize</a>
+	<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Dashboard - click to minimize or maximize</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
