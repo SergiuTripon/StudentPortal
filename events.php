0695284b12ffb7b1ebdf881b9373f3adcbfaca82
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-10 20:21:38 +0000

    Changed structure

--- events.php
+++ events.php
@@ -120,9 +120,9 @@ include 'includes/session.php';
 			<td data-title="From">'.$event_from.'</td>
 			<td data-title="To">'.$event_to.'</td>
 			<td data-title="Price">'.$event_amount.'</td>
-			<td data-title="Tickets">'.($row["event_ticket_no"] === '0' ? "Sold Out" : "$row\[\"event_ticket_no\"\]").'</td>
+			<td data-title="Tickets">'.($event_ticket_no === '0' ? "Sold Out" : "$event_ticket_no").'</td>
 			<td data-title="Category">'.$event_category.'</td>
-			<td data-title="Book"><a id="book-'.$eventid.'" class="book-button">'.($row["event_ticket_no"] === '0' ? "" : "<i class=\"fa fa-gbp\"></i>").'</a></td>
+			<td data-title="Book"><a id="book-'.$eventid.'" class="book-button">'.($event_ticket_no === '0' ? "" : "<i class=\"fa fa-gbp\"></i>").'</a></td>
 			</tr>';
 	}
 
