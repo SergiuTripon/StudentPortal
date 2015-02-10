dc2a51ee1ff75da51ad686e88b0ec0413cc7d31b
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-10 17:07:49 +0000

    Changed structure

--- overview.php
+++ overview.php
@@ -232,11 +232,20 @@ include 'includes/session.php';
 
     <div class="container">
 
-    <ul class="nav nav-pills" role="tablist">
-        <li role="presentation" class="active"><a href="#">Home <span class="badge">42</span></a></li>
-        <li role="presentation"><a href="#">Profile</a></li>
-        <li role="presentation"><a href="#">Messages <span class="badge">3</span></a></li>
-    </ul>
+    <button class="btn btn-primary" type="button">
+        Messages <span class="badge">4</span>
+    </button>
+    <button class="btn btn-primary" type="button">
+        Messages <span class="badge">4</span>
+    </button>
+    <button class="btn btn-primary" type="button">
+        Messages <span class="badge">4</span>
+    </button>
+    <button class="btn btn-primary" type="button">
+        Messages <span class="badge">4</span>
+    </button>
+
+    <br>
 
     <div class="row">
 
