2b134227487181ecbfc3fd8d4f2d87cbc2efe44c
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-10 21:36:47 +0000

    Changed structure

--- includes/menus/portal_menu.php
+++ includes/menus/portal_menu.php
@@ -2,88 +2,32 @@
 include '../session.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <?php include '../../assets/js-paths/pacejs-js-path.php'; ?>
-
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <?php include '../../assets/css-paths/common-css-paths.php'; ?>
-
-    <title>Student Portal | Payment Completed</title>
-
-</head>
-
-<body>
-	<div class="preloader"></div>
-
-	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
-
-    <?php include '../../includes/menus/portal_menu.php'; ?>
-
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
-
-	<form class="form-custom" name="paypal_success_form">
-
-    <div class="form-logo">
-    <i class="fa fa-paypal"></i>
+    <div class="navbar-header">
+    <!-- Toggle -->
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    </button>
+    <!-- End of Toggle -->
+    <a class="navbar-brand" href=""><i class="fa fa-graduation-cap"></i> Student Portal</a>
     </div>
 
-    <hr>
-    <p class="feedback-happy text-center"><?php echo $session_firstname, '', $session_surname; ?></p>
-    <hr>
-
-    <div class="text-center">
-    <a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href="../../overview/"><span class="ladda-label">Overview</span></a>
-	</div>
-
-    </form>
-
-	</div>
-
-    <?php include '../../includes/footers/footer.php'; ?>
-
-	<?php else : ?>
-
-    <?php include '../../includes/menus/menu.php'; ?>
-
-
-    <div class="container">
-
-    <form class="form-custom">
-
-    <div class="form-logo">
-    <i class="fa fa-graduation-cap"></i>
-    </div>
-
-    <hr>
-	<p class="feedback-sad text-center">Looks like you're not signed in yet. Please sign in before accessing this area.</p>
-    <hr>
-
-    <div class="text-center">
-    <a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href="/"><span class="ladda-label">Sign In</span></a>
-    </div>
-
-    </form>
-	</div>
-
-    <?php include '../../includes/footers/footer.php'; ?>
-
-	<?php endif; ?>
-
-    <?php include '../../assets/js-paths/common-js-paths.php'; ?>
-
-	<script>
-	Ladda.bind( '.ladda-button', { timeout: 2000 } );
-	</script>
-
-</body>
-</html>
-
+    <div id="navbar" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+            <a id="find-out-more" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $session_firstname, ' ', $session_surname; ?> <span class="fa fa-caret-down"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            <li><a href="/account/change-password/">Change password</a></li>
+            <li><a href="/sign-out/">Sign Out</a></li>
+          </ul>
+        </li>
+    </ul>
+    </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+    </nav>
+    <!-- End of Navigation -->
