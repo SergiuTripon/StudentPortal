6fa2f2a9d855f68aa9bb9725364dbb11a44e9832
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-11 14:48:56 +0000

    Changed structure

--- about.php
+++ about.php
@@ -90,36 +90,5 @@ include 'includes/session.php';
     <?php include 'assets/js-paths/common-js-paths.php'; ?>
     <?php include 'assets/js-paths/easing-js-path.php'; ?>
 
-	<script>
-    $(document).ready(function() {
-
-    // jQuery to collapse the navbar on scroll
-    $(window).scroll(function () {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
-    });
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function () {
-        $('a.page-scroll').bind('click', function (event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function () {
-        $('.navbar-toggle:visible').click();
-    });
-
-    });
-	</script>
-
 </body>
 </html>
