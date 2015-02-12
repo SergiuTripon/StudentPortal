3df5d53bb539ebd29a31ca3900d66c08aa1b7e98
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-12 09:37:29 +0000

    Changed structure

--- messenger.php
+++ messenger.php
@@ -89,7 +89,7 @@ include 'includes/session.php';
 			<td data-title="Surname">'.$surname.'</td>
 			<td data-title="Student number">'.$studentno.'</td>
 			<td data-title="Email address">'.$email.'</td>
-			<td id="message-hide" data-title="Message"><a id="message-'.$userid2.'" class="btn btn-default btn-md message-button"><i class="fa fa-envelope"></i></a></td>
+			<td id="message-hide" data-title="Message"><a id="message-'.$userid2.'" class="btn btn-default btn-md message-button"></a></td>
 			</tr>';
 	}
 
