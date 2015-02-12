f17b6bf4b4cb4ee3b60a5b83e33652b7c62ded2d
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-12 22:15:34 +0000

    Added google map xml file

--- university-map.php
+++ university-map.php
@@ -5,9 +5,9 @@ include 'includes/session.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
-	<?php include 'assets/meta-tags.php'; ?>
+    <?php include 'assets/meta-tags.php'; ?>
 
-	<?php include 'assets/css-paths/common-css-paths.php'; ?>
+    <?php include 'assets/css-paths/common-css-paths.php'; ?>
 
     <title>Student Portal | University map</title>
 
@@ -21,169 +21,166 @@ include 'includes/session.php';
     var locationSelect;
 
     function load() {
-      map = new google.maps.Map(document.getElementById("map"), {
+        map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(40, -100),
         zoom: 4,
         mapTypeId: 'roadmap',
         mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-      });
-      infoWindow = new google.maps.InfoWindow();
+        });
+        infoWindow = new google.maps.InfoWindow();
 
-      locationSelect = document.getElementById("locationSelect");
-      locationSelect.onchange = function() {
+        locationSelect = document.getElementById("locationSelect");
+        locationSelect.onchange = function() {
         var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
         if (markerNum != "none"){
           google.maps.event.trigger(markers[markerNum], 'click');
         }
-      };
-   }
-
-   function searchLocations() {
-     var address = document.getElementById("addressInput").value;
-     var geocoder = new google.maps.Geocoder();
-     geocoder.geocode({address: address}, function(results, status) {
-       if (status == google.maps.GeocoderStatus.OK) {
-        searchLocationsNear(results[0].geometry.location);
-       } else {
-         alert(address + ' not found');
-       }
-     });
-   }
-
-   function clearLocations() {
-     infoWindow.close();
-     for (var i = 0; i < markers.length; i++) {
-       markers[i].setMap(null);
-     }
-     markers.length = 0;
-
-     locationSelect.innerHTML = "";
-     var option = document.createElement("option");
-     option.value = "none";
-     option.innerHTML = "See all results:";
-     locationSelect.appendChild(option);
-   }
-
-   function searchLocationsNear(center) {
-     clearLocations();
-
-     var radius = document.getElementById('radiusSelect').value;
-     var searchUrl = 'universitymap_source.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
-     downloadUrl(searchUrl, function(data) {
-       var xml = parseXml(data);
-       var markerNodes = xml.documentElement.getElementsByTagName("marker");
-       var bounds = new google.maps.LatLngBounds();
-       for (var i = 0; i < markerNodes.length; i++) {
-         var name = markerNodes[i].getAttribute("name");
-         var address = markerNodes[i].getAttribute("address");
-         var distance = parseFloat(markerNodes[i].getAttribute("distance"));
-         var latlng = new google.maps.LatLng(
-              parseFloat(markerNodes[i].getAttribute("lat")),
-              parseFloat(markerNodes[i].getAttribute("lng")));
-
-         createOption(name, distance, i);
-         createMarker(latlng, name, address);
-         bounds.extend(latlng);
-       }
-       map.fitBounds(bounds);
-       locationSelect.style.visibility = "visible";
-       locationSelect.onchange = function() {
-         var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-         google.maps.event.trigger(markers[markerNum], 'click');
-       };
-      });
+        };
     }
 
+    function searchLocations() {
+        var address = document.getElementById("addressInput").value;
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({address: address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+        searchLocationsNear(results[0].geometry.location);
+        } else {
+            alert(address + ' not found');
+        }
+        });
+    }
+
+    function clearLocations() {
+        infoWindow.close();
+        for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+        }
+        markers.length = 0;
+
+        locationSelect.innerHTML = "";
+        var option = document.createElement("option");
+        option.value = "none";
+        option.innerHTML = "See all results:";
+        locationSelect.appendChild(option);
+    }
+
+    function searchLocationsNear(center) {
+        clearLocations();
+
+        var radius = document.getElementById('radiusSelect').value;
+        var searchUrl = 'universitymap_source.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
+        downloadUrl(searchUrl, function(data) {
+        var xml = parseXml(data);
+        var markerNodes = xml.documentElement.getElementsByTagName("marker");
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < markerNodes.length; i++) {
+            var name = markerNodes[i].getAttribute("name");
+            var address = markerNodes[i].getAttribute("address");
+            var distance = parseFloat(markerNodes[i].getAttribute("distance"));
+            var latlng = new google.maps.LatLng(
+                parseFloat(markerNodes[i].getAttribute("lat")),
+                parseFloat(markerNodes[i].getAttribute("lng")));
+
+        createOption(name, distance, i);
+        createMarker(latlng, name, address);
+        bounds.extend(latlng);
+        }
+        map.fitBounds(bounds);
+        locationSelect.style.visibility = "visible";
+        locationSelect.onchange = function() {
+        var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+        google.maps.event.trigger(markers[markerNum], 'click');
+        };
+        });
+    }
+  
     function createMarker(latlng, name, address) {
-      var html = "<b>" + name + "</b> <br/>" + address;
-      var marker = new google.maps.Marker({
+        var html = "<b>" + name + "</b> <br/>" + address;
+        var marker = new google.maps.Marker({
         map: map,
         position: latlng
-      });
-      google.maps.event.addListener(marker, 'click', function() {
+        });
+        google.maps.event.addListener(marker, 'click', function() {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
-      });
-      markers.push(marker);
+        });
+        markers.push(marker);
     }
 
     function createOption(name, distance, num) {
-      var option = document.createElement("option");
-      option.value = num;
-      option.innerHTML = name + "(" + distance.toFixed(1) + ")";
-      locationSelect.appendChild(option);
+        var option = document.createElement("option");
+        option.value = num;
+        option.innerHTML = name + "(" + distance.toFixed(1) + ")";
+        locationSelect.appendChild(option);
     }
 
     function downloadUrl(url, callback) {
-      var request = window.ActiveXObject ?
-          new ActiveXObject('Microsoft.XMLHTTP') :
-          new XMLHttpRequest;
+        var request = window.ActiveXObject ?
+        new ActiveXObject('Microsoft.XMLHTTP') :
+        new XMLHttpRequest;
 
-      request.onreadystatechange = function() {
+        request.onreadystatechange = function() {
         if (request.readyState == 4) {
           request.onreadystatechange = doNothing;
           callback(request.responseText, request.status);
         }
-      };
+        };
 
       request.open('GET', url, true);
       request.send(null);
     }
 
     function parseXml(str) {
-      if (window.ActiveXObject) {
+    if (window.ActiveXObject) {
         var doc = new ActiveXObject('Microsoft.XMLDOM');
         doc.loadXML(str);
         return doc;
-      } else if (window.DOMParser) {
+    } else if (window.DOMParser) {
         return (new DOMParser).parseFromString(str, 'text/xml');
-      }
+    }
     }
 
     function doNothing() {}
 
     //]]>
     </script>
-
 </head>
 
-<body style="margin:0px; padding:0px;" onload="load()">
+<body style="margin:0; padding:0;" onload="load()">
 
     <div class="preloader"></div>
 
-	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 
-    <?php include 'includes/menus/portal_menu.php'; ?>
+    <?php include 'includes/menus/menu.php'; ?>
 
-	<div id="timetable-portal" class="container">
+    <div class="container">
 
-	<ol class="breadcrumb">
-    <li><a href="../overview/">Overview</a></li>
-	<li class="active">University map</li>
+    <ol class="breadcrumb">
+      <li><a href="../overview/">Overview</a></li>
+      <li class="active">University map</li>
     </ol>
 
-    <div>
-    <input type="text" id="addressInput" size="10"/>
+    <div id="map-search-controls">
+
+    <form >
+    <input class="form-control" type="text" id="addressInput" size="10"/>
     <select id="radiusSelect">
     <option value="25" selected>25mi</option>
     <option value="100">100mi</option>
     <option value="200">200mi</option>
     </select>
-    <input type="button" onclick="searchLocations()" value="Search"/>
+    <input class="btn btn-primary btn-lg" type="button" onclick="searchLocations()" value="Search"/>
     </div>
+    </form>
     <div><select id="locationSelect" style="width:100%;visibility:hidden"></select></div>
-    <div id="map" style="width: 100%; height: 80%"></div>
+    <div id="map" style="width: 100%; height: 65%"></div>
 
-    </div><!-- /container -->
+    </div>
 
-	<?php include 'includes/footers/footer.php'; ?>
+    <?php include 'includes/footers/footer.php'; ?>
 
-	<!-- Sign Out (Inactive) JS -->
-    <script src="../assets/js/custom/sign-out-inactive.js"></script>
-
-	<?php else : ?>
-
-	<?php include 'includes/menus/menu.php'; ?>
+    <?php else : ?>
 
     <div class="container">
 
