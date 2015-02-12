d9d725a87f150b2056bdb95b928e45173020792f
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-12 20:29:45 +0000

    Added google map xml file

--- university-map.php
+++ university-map.php
@@ -70,7 +70,7 @@ include 'includes/session.php';
      clearLocations();
 
      var radius = document.getElementById('radiusSelect').value;
-     var searchUrl = 'phpsqlsearch_genxml.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
+     var searchUrl = 'https://student-portal.co.uk/phpsqlsearch_genxml.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
      downloadUrl(searchUrl, function(data) {
        var xml = parseXml(data);
        var markerNodes = xml.documentElement.getElementsByTagName("marker");
