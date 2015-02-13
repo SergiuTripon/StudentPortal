7c3b2eb6e59af3674130e96c4673f9a6304ec9e5
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 13:02:07 +0000

    Added google map xml file

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -10,3 +10,25 @@
     </ul>
     </div>
     </footer>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Copyright &copy; 2015 Student Portal</a>
+            </div>
+            <div id="navbar1" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a>About</a></li>
+                    <li><a>Features</a></li>
+                    <li><a>User manual</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
