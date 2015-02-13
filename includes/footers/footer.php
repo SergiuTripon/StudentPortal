41c6b6796aa55beb10bd6ffb56f702bbd4648c15
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 01:31:30 +0000

    Added google map xml file

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,6 +1,6 @@
     <footer>
     <div class="container">
-    <p class="pull-left">&copy; <span id="current-year">2015<span></span> Student Portal - All Rights Reserved.</p>
+    <p class="pull-left">Copyright &copy; 2015 Student Portal</p>
 
     <ul class="list-inline external-pages-links text-right">
         <li><a href="/about/">About</a></li>
@@ -10,9 +10,3 @@
     </ul>
     </div>
     </footer>
-
-    <script>
-    $(document).ready(function() {
-        $('#current-year').html(new Date().getFullYear());
-    });
-    </script>
