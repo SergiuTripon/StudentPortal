e9b832a4642bf20506a356ee3026f1216d42a1ab
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 15:07:27 +0000

    Changed structure

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,21 +1,23 @@
     <nav class="navbar navbar-default navbar-fixed-bottom">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Copyright &copy; 2015 Student Portal</a>
-            </div>
-            <div id="navbar1" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a>About</a></li>
-                    <li><a>Features</a></li>
-                    <li><a>User manual</a></li>
-                    <li><a>Contact</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
+    <div class="container">
+    <div class="navbar-header">
+
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    </button>
+
+    <a class="navbar-brand" href="/">Copyright &copy; 2015 Student Portal</a>
+    </div>
+    <div id="navbar1" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+    <li id="about"><a>About</a></li>
+    <li id="features"><a>Features</a></li>
+    <li id="user-manual"><a>User manual</a></li>
+    <li id="contact"><a>Contact</a></li>
+    </ul>
+    </div><!--/.nav-collapse -->
+    </div>
     </nav>
