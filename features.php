00cabe1825c54b28854e3e8eab4f17aa9bf5ec2d
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 15:13:43 +0000

    Changed structure

--- features.php
+++ features.php
@@ -15,7 +15,6 @@ include 'includes/session.php';
 
     <style>
     #features {
-        border-radius: 3px;
         background-color: #735326;
     }
     #features a {
@@ -34,33 +33,134 @@ include 'includes/session.php';
 
 	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 
-    <header><!-- Header -->
-    <div class="wrapper"><!-- Wrapper -->
+     <?php include 'includes/menus/portal_menu.php'; ?>
 
-    <form class="form-custom">
+    <div id="features-showcase" class="container"><!-- container -->
 
-    <div class="logo-custom">
-    <i class="fa fa-graduation-cap"></i>
+    <h1 class="text-center">Features</h1>
+    <hr class="hr-small">
+
+	<p class="text-left">Click and discover</p>
+
+    <div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+    <div id="timetable">
+	<div class="tile large-tile">
+    <i class="fa fa-clock-o"></i>
+	<p>Timetable</p>
+    </div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+    <div id="exams">
+	<div class="tile">
+	<i class="fa fa-pencil"></i>
+	<p>Exams</p>
+    </div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
+	<div id="library">
+	<div class="tile">
+    <i class="fa fa-book"></i>
+	<p>Library</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
+    <div id="transport">
+	<div class="tile">
+    <i class="fa fa-bus"></i>
+	<p>Transport</p>
+    </div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+    <div id="calendar1">
+	<div class="tile">
+	<i class="fa fa-calendar"></i>
+	<p>Calendar</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+    <div id="events">
+	<div class="tile">
+	<i class="fa fa-ticket"></i>
+	<p>Events</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
+    <div id="university-map">
+	<div class="tile">
+    <i class="fa fa-map-marker"></i>
+	<p>University Map</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
+    <div id="feedback">
+	<div class="tile">
+    <i class="fa fa-check-square-o"></i>
+	<p>Feedback</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+    <div id="messenger">
+	<div class="tile">
+    <i class="fa fa-comments"></i>
+	<p>Messenger</p>
+	</div>
+	</div>
+	</div>
+
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+	<div id="account">
+	<div class="tile">
+    <i class="fa fa-user"></i>
+	<p>Account</p>
+	</div>
+	</div>
+	</div>
+	</div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <!-- Feature Modal -->
+    <div class="modal fade modal-custom" id="modal-features" tabindex="-1" role="dialog" aria-labelledby="-modal-custom-label" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+
+	<div class="modal-header">
+    <div class="close"></div>
+    <h4 class="modal-title" id="modal-custom-label"></h4>
     </div>
 
-    <hr class="hr-custom">
-
-    <p class="feedback-sad text-center">You are already logged in. You don't have to log in again.</p>
-
-    <hr class="hr-custom">
-
-	<div class="pull-left">
-    <a class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500" href="overview/"><span class="ladda-label">Overview</span></a>
+    <div class="modal-body">
     </div>
 
-    <div class="text-right">
-    <a class="btn btn-custom btn-lg ladda-button" data-style="slide-up" data-spinner-color="#FFA500" href="sign-out/"><span class="ladda-label">Sign Out</span></a>
-    </div>
+	<div class="modal-footer">
+	<div class="text-center">
+    <button type="button" class="btn btn-danger btn-lg" data-style="slide-up" data-dismiss="modal">Close</button>
+	</div>
+	</div>
 
-    </form>
+	</div><!-- /modal -->
+    </div><!-- /modal-dialog -->
+    </div><!-- /modal-content -->
+	<!-- End of Feature Modal -->
 
-    </div><!-- End of Wrapper -->
-    </header><!-- End of Header -->
+    <?php include 'includes/footers/footer.php'; ?>
 
     <!-- Sign Out (Inactive) JS -->
     <script src="../assets/js/sign-out-inactive.js"></script>
