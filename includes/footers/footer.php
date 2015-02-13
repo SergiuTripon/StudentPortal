c7e9b0780754e72219636d5b19a491138dcddcce
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 11:00:58 +0000

    Added google map xml file

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,12 +1,38 @@
-    <footer>
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
-    <p class="pull-left">Copyright &copy; 2015 Student Portal</p>
-
-    <ul class="list-inline external-pages-links text-right">
-        <li id="about"><a href="/about/">About</a></li>
-        <li id="features"><a href="/features/">Features</a></li>
-        <li id="user-manual"><a href="/user-manual/">User manual</a></li>
-        <li id="contact"><a href="/contact/">Contact</a></li>
-    </ul>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar1" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../navbar/">Default</a></li>
+                <li><a href="../navbar-static-top/">Static top</a></li>
+                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
     </div>
-    </footer>
+</nav>
