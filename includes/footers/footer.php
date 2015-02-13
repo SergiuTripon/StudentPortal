0abe93121e88ca8c61d380b11dae133f52d7f69b
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-13 13:02:39 +0000

    Added google map xml file

--- includes/footers/footer.php
+++ includes/footers/footer.php
@@ -1,16 +1,3 @@
-    <footer>
-    <div class="container">
-    <p class="pull-left">Copyright &copy; 2015 Student Portal</p>
-
-    <ul class="list-inline external-pages-links text-right">
-        <li id="about"><a href="/about/">About</a></li>
-        <li id="features"><a href="/features/">Features</a></li>
-        <li id="user-manual"><a href="/user-manual/">User manual</a></li>
-        <li id="contact"><a href="/contact/">Contact</a></li>
-    </ul>
-    </div>
-    </footer>
-
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
