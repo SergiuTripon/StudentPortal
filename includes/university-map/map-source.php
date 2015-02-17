43aa49d1d3ba4b5a296bebf970b0315035994db1
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-17 22:00:56 +0000

    Changed structure

---
+++ includes/university-map/map-source.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../session.php';
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Select all the rows in the markers table
+
+$stmt1 = $mysqli->query("SELECT * FROM markers WHERE 1");
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = $stmt1->fetch_assoc()){
+    // ADD TO XML DOCUMENT NODE
+    $node = $dom->createElement("marker");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+}
+
+echo $dom->saveXML();
