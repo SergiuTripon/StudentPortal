609442ab68e5a6d11b79b1cfcc13a6048f468ee5
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-18 21:51:34 +0000

    Changed structure

--- includes/university-map/map_source1.php
+++ includes/university-map/map_source1.php
@@ -13,7 +13,7 @@ $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
 // Search the rows in the markers table
-$query = sprintf("SELECT marker_name, marker_description, marker_lat, marker_long, marker_category, (3959 * acos(cos(radians('%s')) * cos(radians(marker_lat)) * cos(radians(marker_long) - radians('%s')) + sin( radians('%s') ) * sin(radians(marker_lat)))) AS distance FROM system_map_markers HAVING distance < '%s' ORDER BY distance LIMIT 0 , 20",
+$query = sprintf("SELECT marker_title, marker_description, marker_lat, marker_long, marker_category, (3959 * acos(cos(radians('%s')) * cos(radians(marker_lat)) * cos(radians(marker_long) - radians('%s')) + sin( radians('%s') ) * sin(radians(marker_lat)))) AS distance FROM system_map_markers HAVING distance < '%s' ORDER BY distance LIMIT 0 , 20",
     $mysqli->real_escape_string($center_lat),
     $mysqli->real_escape_string($center_lng),
     $mysqli->real_escape_string($center_lat),
@@ -26,7 +26,7 @@ header("Content-type: text/xml");
 while ($row = $result->fetch_assoc()){
       $node = $dom->createElement("marker");
       $newnode = $parnode->appendChild($node);
-      $newnode->setAttribute("name", $row['marker_name']);
+      $newnode->setAttribute("name", $row['marker_title']);
       $newnode->setAttribute("description", $row['marker_description']);
       $newnode->setAttribute("lat", $row['marker_lat']);
       $newnode->setAttribute("lng", $row['marker_long']);
