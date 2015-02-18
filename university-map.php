23eff6db9eea6cc048f49ed2acfaab531ece9d90
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-18 22:09:55 +0000

    Changed structure

--- university-map.php
+++ university-map.php
@@ -1,12 +1,14 @@
+
 <!DOCTYPE html>
-  <head>
+<head>
+
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>PHP/MySQL & Google Maps Example</title>
+
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
     <script type="text/javascript">
     //<![CDATA[
-
     var customIcons = {
     buildings: {
         icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
@@ -26,70 +28,69 @@
     };
 
     function load() {
-      var map = new google.maps.Map(document.getElementById("map"), {
+        var map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(51.527287, -0.103842),
         zoom: 13,
         mapTypeId: 'roadmap'
-      });
-      var infoWindow = new google.maps.InfoWindow;
+        });
+        var infoWindow = new google.maps.InfoWindow;
 
-      // Change this depending on the name of your PHP file
-      downloadUrl("../includes/university-map/map_source.php", function(data) {
+    // Change this depending on the name of your PHP file
+    downloadUrl("../includes/university-map/map_source.php", function(data) {
         var xml = data.responseXML;
         var markers = xml.documentElement.getElementsByTagName("marker");
         for (var i = 0; i < markers.length; i++) {
-          var name = markers[i].getAttribute("name");
-          var description = markers[i].getAttribute("description");
-          var type = markers[i].getAttribute("type");
-          var point = new google.maps.LatLng(
-              parseFloat(markers[i].getAttribute("lat")),
-              parseFloat(markers[i].getAttribute("lng")));
-          var html = "<b>" + name + "</b> <br/>" + description;
-          var icon = customIcons[type] || {};
-          var marker = new google.maps.Marker({
+        var name = markers[i].getAttribute("name");
+        var description = markers[i].getAttribute("description");
+        var type = markers[i].getAttribute("type");
+        var point = new google.maps.LatLng(
+            parseFloat(markers[i].getAttribute("lat")),
+            parseFloat(markers[i].getAttribute("lng")));
+        var html = "<b>" + name + "</b> <br/>" + description;
+        var icon = customIcons[type] || {};
+        var marker = new google.maps.Marker({
             map: map,
             position: point,
             icon: icon.icon,
             animation: google.maps.Animation.DROP
-          });
-          bindInfoWindow(marker, map, infoWindow, html);
+        });
+        bindInfoWindow(marker, map, infoWindow, html);
         }
-      });
+    });
     }
 
     function bindInfoWindow(marker, map, infoWindow, html) {
-      google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', function() {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
-      });
+        });
     }
 
     function downloadUrl(url, callback) {
-      var request = window.ActiveXObject ?
-          new ActiveXObject('Microsoft.XMLHTTP') :
-          new XMLHttpRequest;
+        var request = window.ActiveXObject ?
+        new ActiveXObject('Microsoft.XMLHTTP') :
+        new XMLHttpRequest;
 
-      request.onreadystatechange = function() {
+    request.onreadystatechange = function() {
         if (request.readyState == 4) {
-          request.onreadystatechange = doNothing;
-          callback(request, request.status);
+        request.onreadystatechange = doNothing;
+        callback(request, request.status);
         }
-      };
+    };
 
-      request.open('GET', url, true);
-      request.send(null);
+    request.open('GET', url, true);
+    request.send(null);
     }
 
     function doNothing() {}
 
     //]]>
+    </script>
 
-  </script>
+</head>
+<body onload="load()">
 
-  </head>
-
-  <body onload="load()">
     <div id="map" style="width: 100%; height: 80%"></div>
-  </body>
 
+</body>
 </html>
