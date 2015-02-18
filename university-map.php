d65837543142427e7f43c94806a6cdb4e7155781
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-18 22:10:14 +0000

    Changed structure

--- university-map.php
+++ university-map.php
@@ -11,19 +11,19 @@
     //<![CDATA[
     var customIcons = {
     buildings: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+        icon: 'https://labs.google.com/ridefinder/images/mm_20_blue.png'
     },
     studentCentre: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
+        icon: 'https://labs.google.com/ridefinder/images/mm_20_red.png'
     },
     lectureTheatres: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png'
+        icon: 'https://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png'
     },
     computerLabs: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_yellow.png'
+        icon: 'https://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_yellow.png'
     },
     libraries: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_black.png'
+        icon: 'https://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_black.png'
     }
     };
 
