45b2e5d843f36c12bf4e780e12576b209fa117ef
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 20:24:35 +0000

    Changed structure

--- university-map/search.php
+++ university-map/search.php
@@ -18,6 +18,9 @@ include '../includes/session.php';
 
 </head>
 <body onload="load()">
+<div class="preloader"></div>
+
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
 
     <?php include '../includes/menus/portal_menu.php'; ?>
 
@@ -64,6 +67,39 @@ include '../includes/session.php';
     </div>
 
     <?php include '../includes/footers/footer.php'; ?>
+
+    <!-- Sign Out (Inactive) JS -->
+    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+
+    <?php else : ?>
+
+    <?php include '../includes/menus/menu.php'; ?>
+
+    <div class="container">
+
+	<form class="form-custom">
+
+    <div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+
+    <p class="feedback-sad text-center">Looks like you're not signed in yet. Please sign in before accessing this area.</p>
+
+    <hr>
+
+    <div class="text-center">
+	<a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href="/"><span class="ladda-label">Sign In</span></a>
+    </div>
+
+    </form>
+
+	</div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+
+	<?php endif; ?>
 
     <?php include '../assets/js-paths/common-js-paths.php'; ?>
 
