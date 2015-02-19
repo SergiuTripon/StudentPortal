f48851937a1c9a07d88b620aecd09a8e708feec2
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 20:41:18 +0000

    Changed structure

--- assets/js/google-maps/search.js
+++ assets/js/google-maps/search.js
@@ -5,31 +5,6 @@
     var locationSelect;
     var type;
 
-    function load() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: new google.maps.LatLng(51.527287, -0.103842),
-            zoom: 15,
-            navigationControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-            draggable: false,
-            mapTypeId: 'roadmap',
-            mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-        });
-
-        infoWindow = new google.maps.InfoWindow({
-            maxWidth: 400
-        });
-
-        locationSelect = document.getElementById("locationSelect");
-        locationSelect.onchange = function() {
-            var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-            if (markerNum != "none") {
-                google.maps.event.trigger(markers[markerNum], 'click');
-            }
-        };
-    }
-
     function searchLocations() {
         var address = document.getElementById("addressInput").value;
         var geocoder = new google.maps.Geocoder();
--- university-map/search.php
+++ university-map/search.php
@@ -17,7 +17,7 @@ include '../includes/session.php';
     <?php include '../assets/js-paths/google-maps-js-path.php'; ?>
 
 </head>
-<body onload="load()">
+<body>
 <div class="preloader"></div>
 
     <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
