20e931f732e8be63814b2849cd2952a7b79849ef
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 21:25:46 +0000

    Changed structure

---
+++ university-map.php
@@ -0,0 +1,92 @@
+<?php
+include 'includes/session.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<?php include 'assets/meta-tags.php'; ?>
+
+	<?php include 'assets/css-paths/common-css-paths.php'; ?>
+
+    <title>Student Portal | University Map</title>
+
+</head>
+
+<body>
+<div class="preloader"></div>
+
+	<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
+
+	<?php include 'includes/menus/portal_menu.php'; ?>
+
+	<div id="events-portal" class="container">
+
+	<ol class="breadcrumb">
+    <li><a href="../overview/">Overview</a></li>
+	<li class="active">Calendar</li>
+    </ol>
+
+	<div class="row mb10">
+
+	<div class="col-xs-6 col-sm-4 col-md-6 col-lg-6">
+	<a id="task-button">
+    <div class="tile task-tile">
+	<i class="fa fa-beer"></i>
+	<p class="tile-text">Event view</p>
+    </div>
+    </a>
+	</div>
+
+	<div class="col-xs-6 col-sm-4 col-md-6 col-lg-6">
+	<a id="calendar-button">
+	<div class="tile calendar-tile">
+    <i class="fa fa-calendar"></i>
+	<p class="tile-text">Calendar view</p>
+    </div>
+    </a>
+	</div>
+
+	</div><!-- /row -->
+
+    </div><!-- /container -->
+
+	<?php include 'includes/footers/footer.php'; ?>
+
+	<!-- Sign Out (Inactive) JS -->
+    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+
+	<?php else : ?>
+
+	<?php include 'includes/menus/menu.php'; ?>
+
+    <div class="container">
+
+	<form class="form-custom">
+
+    <div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+    <p class="feedback-sad text-center">Looks like you're not signed in yet. Please sign in before accessing this area.</p>
+    <hr>
+
+    <div class="text-center">
+	<a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href="/"><span class="ladda-label">Sign In</span></a>
+    </div>
+
+    </form>
+
+	</div>
+
+	<?php include 'includes/footers/footer.php'; ?>
+
+	<?php endif; ?>
+
+	<?php include 'assets/js-paths/common-js-paths.php'; ?>
+
+</body>
+</html>
