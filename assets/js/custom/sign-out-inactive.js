a0a69f31acc17b6229f76f0dd867ccf9c7eef8fe
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 20:39:19 +0000

    Changed structure

--- assets/js/custom/sign-out-inactive.js
+++ assets/js/custom/sign-out-inactive.js
@@ -1,1 +1,9 @@
+	var timeOut;
+	function reset() {
+    window.clearTimeout(timeOut);
+    timeOut = window.setTimeout( "redir()" , 900000 );
+	}
+	function redir() {
+    window.location = "../../../sign-out-inactive.php";
+	}
 
