5b6d4c4f5b7305fd7128aaffbecb2101fea658fc
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 21:42:12 +0000

    Changed structure

--- calendar.php
+++ calendar.php
@@ -289,43 +289,7 @@ include 'includes/session.php';
 	<script>
     $(document).ready(function () {
 
-	(function($) {
 
-	"use strict";
-
-	var options = {
-		events_source: '../../includes/calendar-source/tasks_json.php',
-		view: 'month',
-		tmpl_path: '../assets/tmpls/',
-		tmpl_cache: false,
-		onAfterViewLoad: function(view) {
-			$('.page-header h3').text(this.getTitle());
-			$('.btn-group button').removeClass('active');
-			$('button[data-calendar-view="' + view + '"]').addClass('active');
-		},
-		classes: {
-			months: {
-				general: 'label'
-			}
-		}
-	};
-
-	var calendar = $('#calendar').calendar(options);
-
-	$('.btn-group button[data-calendar-nav]').each(function() {
-		var $this = $(this);
-		$this.click(function() {
-			calendar.navigate($this.data('calendar-nav'));
-		});
-	});
-
-	$('.btn-group button[data-calendar-view]').each(function() {
-		var $this = $(this);
-		$this.click(function() {
-			calendar.view($this.data('calendar-view'));
-		});
-	});
-	}(jQuery));
 
     $('.table-custom').dataTable({
         "iDisplayLength": 10,
