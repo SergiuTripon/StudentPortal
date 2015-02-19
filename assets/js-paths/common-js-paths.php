047b98bb8b71097297bef58f61f065225390325d
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 21:46:03 +0000

    Changed structure

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -6,8 +6,6 @@
 
 
 
-    <!-- custom -->
-    <script src="https://student-portal.co.uk/assets/js/custom/custom.js"></script>
 
     <!-- common-->
     <script src="https://student-portal.co.uk/assets/js/common/ie10-viewport-bug-workaround.js"></script>
