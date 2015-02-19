69b13850d99390561cc8c1a2d0acfa450f152434
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 18:26:53 +0000

    Changed structure

--- university-map/search.php
+++ university-map/search.php
@@ -161,7 +161,7 @@ include '../includes/session.php';
         <li class="active">Search</li>
     </ol>
 
-    <form class="form-custom" style="width: 100%;">
+    <form class="form-custom" style="max-width: 100%;">
 
     <input class="form-control" type="text" id="addressInput"/>
 
