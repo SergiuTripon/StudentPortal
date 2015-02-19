60defde764c98e55635f593ae40c050ac87c84c2
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-19 21:46:32 +0000

    Changed structure

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -1,6 +1,5 @@
     <!-- js library -->
     <script src="https://student-portal.co.uk/assets/js/jquery/jquery-latest.min.js"></script>
 
-    <!-- bootstrap -->
-    <script src="https://student-portal.co.uk/assets/js/bootstrap/bootstrap.min.js"></script>
+
 
