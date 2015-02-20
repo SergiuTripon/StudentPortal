57bf79e49ee626d79b5710607b247cceb8163dae
Sergiu Tripon <contact@sergiu-tripon.com>
2015-02-20 10:33:22 +0000

    Changed structure

--- includes/university-map/test.php
+++ includes/university-map/test.php
@@ -1,10 +1,6 @@
 <?php
 
 $latlng = "-0.10267093777656555,51.527640252365465";
-explode(',', $latlng);
 
-$title1 = $array[0];
-$title2 = $array[1];
-
-echo $title1;
-echo $title2;
+list($lat, $lng) = explode(',', $latlng));
+print($lat);
