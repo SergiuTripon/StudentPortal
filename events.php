e1f16550200492a84ad34c08f6590161d7e8cbd2
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-20 19:11:45 +0000

    Changed structure

--- events.php
+++ events.php
@@ -69,7 +69,7 @@ include 'includes/session.php';
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><i class="fa fa-chevron-right"></i> Events - click to minimize or maximize</a>
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Events - click to minimize or maximize</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
