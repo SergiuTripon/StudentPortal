08b3d08a4030b33f545b6838baa243be4c017667
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-20 18:48:50 +0000

    Changed structure

--- events.php
+++ events.php
@@ -384,6 +384,16 @@ include 'includes/session.php';
 		$(".calendar-tile i").addClass("tile-text-selected");
 	});
 
+    $('#collapseOne').on('shown.bs.collapse', function () {
+        $('#headingOne .fa').removeClass(".fa-chevron-right);
+        $('#headingOne .fa').addClass(".fa-chevron-down);
+    })
+
+    $('#collapseOne').on('hidden.bs.collapse', function () {
+        $('#headingOne .fa').removeClass(".fa-chevron-down);
+        $('#headingOne .fa').addClass(".fa-chevron-right);
+    })
+
 	});
 	</script>
 
