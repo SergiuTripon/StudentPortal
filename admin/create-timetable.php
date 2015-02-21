89ff07e259c1b54d6a58ab15a198202efbdede25
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-21 23:52:01 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -81,8 +81,8 @@ include '../includes/session.php';
     <div class="form-group">
     <div class="col-xs-12 col-sm-12 full-width pr0 pl0">
     <label>Lecturer</label>
-    <select class="selectpicker" name="lecturers" id="lecturers" multiple title='Choose one of the following...'>
-        <option data-hidden="true" disabled="disabled">Select a lecturer</option>
+    <select class="selectpicker" name="lecturers" id="lecturers">
+        <option data-hidden="true" style="color: gray !important;">Select a lecturer</option>
     <?php
     $stmt1 = $mysqli->query("SELECT userid FROM user_signin WHERE account_type = 'lecturer'");
 
