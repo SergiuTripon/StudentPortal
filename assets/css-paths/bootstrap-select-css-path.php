d3ed7cab50e2cad1e162e5863605fda8709d34f8
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-21 17:57:23 +0000

    Changed structure

--- assets/css-paths/bootstrap-select-css-path.php
+++ assets/css-paths/bootstrap-select-css-path.php
@@ -1,2 +1,3 @@
     <!-- bootstrap select -->
     <link href="https://student-portal.co.uk/assets/css/bootstrap-select/bootstrap-select.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/bootstrap-select/bootstrap-select.css.map" rel="stylesheet">
--- assets/js-paths/bootstrap-select-js-path.php
+++ assets/js-paths/bootstrap-select-js-path.php
@@ -1,2 +1,3 @@
     <!-- Bootstrap Select JS -->
     <script src="https://student-portal.co.uk/assets/js/bootstrap-select/bootstrap-select.min.js"></script>
+    <script src="https://student-portal.co.uk/assets/js/bootstrap-select/bootstrap-select.js.map"></script>
