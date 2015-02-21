f8185f7a53af58fa69553f4243e7861627e8ffa3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-21 19:05:26 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -31,7 +31,7 @@ include '../includes/session.php';
 
     <ol class="breadcrumb">
     <li><a href="../../overview/">Overview</a></li>
-	<li><a href="../../account/">Timetable</a></li>
+	<li><a href="../../timetable/">Timetable</a></li>
     <li class="active">Create timetable</li>
     </ol>
 
