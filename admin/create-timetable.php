3e0058e199f71c3783565564b816e2ef1f6a0065
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 22:57:05 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -451,14 +451,14 @@ include '../includes/session.php';
     if (lecturer_check === 'Select an option') {
         $("#error3").show();
         $("#error3").empty().append("Please select a lecturer.");
-        $(".filter-option").css("color", "white");
+        $(".error-style > .filter-option").css("color", "white");
         $(".lecturer > .selectpicker").addClass("error-style");
         hasError  = true;
         return false;
     }
     else {
         $("#error3").hide();
-        $(".filter-option").css("color", "white");
+        $(".error-style > .filter-option").css("color", "white");
         $(".lecturer > .selectpicker").addClass("success-style");
     }
 
