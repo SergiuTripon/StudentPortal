28c3dd66791cc3f90e6963b361e36be2f2b26220
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 15:53:32 +0000

    Changed structure

--- admin/assign-timetable.php
+++ admin/assign-timetable.php
@@ -49,13 +49,13 @@ if (isset($_POST["recordToAssign"])) {
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a id="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Users</a>
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Users</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
   	<div class="panel-body">
 
-	<!-- Due tasks -->
+	<!-- Users -->
 	<section id="no-more-tables">
 	<table class="table table-condensed table-custom">
 
