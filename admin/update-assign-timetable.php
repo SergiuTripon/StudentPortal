6df43816423dbd004e013440cff392b317fde26e
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 14:13:58 +0000

    Changed structure

--- admin/update-assign-timetable.php
+++ admin/update-assign-timetable.php
@@ -38,6 +38,16 @@ include '../includes/session.php';
 
 	<div class="panel panel-default">
 
+    <?php
+	$stmt2 = $mysqli->query("SELECT moduleid FROM system_modules WHERE module_status = 'active'");
+	while($row = $stmt2->fetch_assoc()) {
+	  echo '<form id="assign-timetable-form-'.$row["moduleid"].'" style="display: none;" action="/admin/assign-timetable/" method="POST">
+			<input type="hidden" name="recordToAssign" id="recordToAssign" value="'.$row["moduleid"].'"/>
+			</form>';
+	}
+	$stmt2->close();
+	?>
+
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Modules</a>
@@ -63,7 +73,7 @@ include '../includes/session.php';
 	<tbody>
 	<?php
 
-	$stmt1 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules");
+	$stmt1 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'active'");
 
 	while($row = $stmt1->fetch_assoc()) {
 
