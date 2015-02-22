c747d486ece98e4b1e31659dd69224a54d8016b8
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 21:06:41 +0000

    Changed structure

--- admin/update-timetable.php
+++ admin/update-timetable.php
@@ -137,7 +137,7 @@ WHERE system_modules.moduleid = ? LIMIT 1
 
     <div class="form-group">
     <div class="col-xs-6 col-sm-6 full-width pl0">
-    <label>Lecturer</label>
+    <label>Current lecturer</label>
     <select class="selectpicker lecturer" name="lecturer" id="lecturer">
     <?php
     $stmt1 = $mysqli->query("SELECT userid FROM user_signin WHERE account_type = 'lecturer' AND userid = '$lecture_lecturer'");
@@ -164,7 +164,7 @@ WHERE system_modules.moduleid = ? LIMIT 1
 
     <div class="form-group">
     <div class="col-xs-6 col-sm-6 full-width pr0">
-    <label>Lecturer</label>
+    <label>Update lecturer</label>
     <select class="selectpicker update_lecturer" name="update_lecturer" id="update_lecturer">
         <option data-hidden="true">Select an option</option>
     <?php
