7094037e150fa23fff2f8f7aec983c7793d7b33a
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 14:01:37 +0000

    Changed structure

--- admin/update-assign-timetable.php
+++ admin/update-assign-timetable.php
@@ -39,7 +39,7 @@ include '../includes/session.php';
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Lectures</a>
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Lectures</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
