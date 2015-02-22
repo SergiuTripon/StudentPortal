68d0f65c09dc0e6e967e4262ab7f3c511502c12d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 23:35:31 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -10,6 +10,7 @@ include '../includes/session.php';
 	<?php include '../assets/meta-tags.php'; ?>
 
     <?php include '../assets/css-paths/bootstrap-select-css-path.php'; ?>
+    <?php include '../assets/js-paths/bootstrap-select-js-path.php'; ?>
     <?php include '../assets/css-paths/common-css-paths.php'; ?>
     <?php include '../assets/css-paths/datetimepicker-css-path.php'; ?>
 
@@ -331,7 +332,6 @@ include '../includes/session.php';
 	<?php endif; ?>
 
     <?php include '../assets/js-paths/common-js-paths.php'; ?>
-    <?php include '../assets/js-paths/bootstrap-select-js-path.php'; ?>
 	<?php include '../assets/js-paths/datetimepicker-js-path.php'; ?>
 
 	<script>
