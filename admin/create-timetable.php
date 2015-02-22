565242eacb8ce9e48c75f71a8ea80b16c7ad6f7d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-22 23:21:07 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -345,13 +345,7 @@ include '../includes/session.php';
     $(".filter-option").css("color", "gray");
 
     $( ".bootstrap-select" ).click(function() {
-        var lecturer_check = $(".bootstrap-select button.selectpicker").attr('title');
-
-        if (lecturer_check === 'Select a lecturer') {
-            $(".filter-option").css("color", "gray");
-        } else {
-            $(".filter-option").css("cssText", "color: #333333;");
-        }
+        $(".filter-option").css("cssText", "color: #333333;");
     });
 
     // Date Time Picker
