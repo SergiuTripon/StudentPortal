8f509aec7d1655fb504f7b77c22926a7b09b2303
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 20:31:16 +0000

    Changed structure

--- admin/assign-timetable.php
+++ admin/assign-timetable.php
@@ -91,7 +91,7 @@ if (isset($_POST["recordToAssign"])) {
 
 			<td data-title="First name">'.$firstname.'</td>
 			<td data-title="Surname">'.$surname.'</td>
-			<td data-title="Email address">'.$email.'</td>
+			<td data-title="Email address">'.$email.' '.$already_assigned.'</td>
 			<td data-title="Action">'.($already_assigned === '0' ? "Already assigned" : "<a id=\"assign-'.$db_userid.'\" class=\"btn btn-primary btn-md assign-button\">Assign</a>").'</td>
 			</tr>';
 	}
