80b90b8e260cc455bbf50f9e663f81a59df118a6
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 22:11:07 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -308,61 +308,6 @@ include 'includes/session.php';
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
 
-    <div id="completedtasks-toggle" class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
-  	<h4 class="panel-title">
-	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"> Cancelled timetables</a>
-  	</h4>
-    </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-  	<div class="panel-body">
-
-	<!-- Cancelled timetables -->
-	<section id="no-more-tables">
-	<table class="table table-condensed table-custom module-table">
-
-	<thead>
-	<tr>
-	<th>Name</th>
-	<th>Notes</th>
-	<th>External URL</th>
-	<th>Start</th>
-	<th>Due</th>
-	<th>Category</th>
-	</tr>
-	</thead>
-
-	<tbody>
-	<?php
-
-	$stmt2 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'cancelled'");
-
-	while($row = $stmt3->fetch_assoc()) {
-
-    $moduleid = $row["moduleid"];
-	$module_name = $row["module_name"];
-	$module_notes = $row["module_notes"];
-	$module_url = $row["module_url"];
-
-	echo '<tr id="cancel-'.$moduleid.'">
-
-			<td data-title="Name">'.$module_name.'</td>
-			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
-            <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-			</tr>';
-	}
-
-	$stmt3->close();
-	?>
-	</tbody>
-
-	</table>
-	</section>
-
-  	</div><!-- /panel-body -->
-    </div><!-- /panel-collapse -->
-  	</div><!-- /panel-default -->
-
 	</div><!-- /panel-group -->
 
     </div><!-- /container -->
