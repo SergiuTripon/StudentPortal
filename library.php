1432ac67d4ad8b7cd5c41f10111a4dcc9b302af9
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 13:48:35 +0000

    Changed structure

--- library.php
+++ library.php
@@ -257,6 +257,148 @@ include 'includes/session.php';
 
     <a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href=""><span class="ladda-label"><i class="fa fa-plus"></i> Create book</span></a>
 
+    	<div class="panel-group book-view" id="accordion" role="tablist" aria-multiselectable="true">
+
+	<div id="books-content" class="panel panel-default">
+
+	<?php
+	$stmt2 = $mysqli->query("SELECT bookid FROM system_books WHERE NOT book_status = 'cancelled'");
+	while($row = $stmt2->fetch_assoc()) {
+
+	$bookid = $row["bookid"];
+
+ 	echo '<form id="reserve-book-form-'.$bookid.'" style="display: none;" action="/library/reserve-book/" method="POST">
+		<input type="hidden" name="recordToReserve" id="recordToReserve" value="'.$bookid.'"/>
+		</form>';
+	}
+	$stmt2->close();
+	?>
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Books - click to minimize or maximize</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Books -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom books-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Author</th>
+	<th>Notes</th>
+	<th>Copy no.</th>
+	<th>Status</th>
+	<th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT bookid, book_name, book_author, book_notes, book_copy_no, book_status FROM system_books WHERE NOT book_status = 'cancelled'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$bookid = $row["bookid"];
+	$book_name = $row["book_name"];
+	$book_author = $row["book_author"];
+	$book_notes = $row["book_notes"];
+	$book_copy_no = $row["book_copy_no"];
+	$book_status = $row["book_status"];
+	$book_status = ucfirst($book_status);
+
+	echo '<tr id="book-'.$bookid.'">
+
+			<td data-title="Name">'.$book_name.'</td>
+			<td data-title="Author">'.$book_author.'</td>
+			<td data-title="Notes">'.$book_notes.'</td>
+			<td data-title="Copy no.">'.$book_copy_no.'</td>
+			<td data-title="Status">'.$book_status.'</td>
+			<td data-title="Action">'.($book_status === 'Reserved' ? "<a id=\"request-$bookid\" class=\"btn btn-default btn-md request-button\">Request</a>" : "<a id=\"reserve-$bookid\" class=\"btn btn-default btn-md reserve-button\">Reserve</a>").'</td>
+			</tr>';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	<div id="reservedbooks-content" class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingTwo">
+  	<h4 class="panel-title">
+	<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Reserved books - click to minimize or maximize</a>
+  	</h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+  	<div class="panel-body">
+
+	<!-- Reserved books -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom reservedbooks-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Author</th>
+	<th>Notes</th>
+	<th>Booked on</th>
+	<th>Return on</th>
+	<th>Status</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt2 = $mysqli->query("SELECT reserved_books.bookid, DATE_FORMAT(reserved_books.reserved_on,'%d %b %y') as reserved_on, DATE_FORMAT(reserved_books.toreturn_on,'%d %b %y') as toreturn_on, system_books.book_name, system_books.book_author, system_books.book_notes, system_books.book_status FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid  WHERE reserved_books.userid = '$userid' AND system_books.book_status = 'reserved' AND isReturned = '0'");
+
+	while($row = $stmt2->fetch_assoc()) {
+
+    $book_name = $row["book_name"];
+    $book_author = $row["book_author"];
+    $book_notes = $row["book_notes"];
+    $reserved_on = $row["reserved_on"];
+    $toreturn_on = $row["toreturn_on"];
+	$book_status = $row["book_status"];
+
+	$book_status = ucfirst($book_status);
+
+	echo '<tr>
+
+			<td data-title="Name">'.$book_name.'</td>
+			<td data-title="Author">'.$book_author.'</td>
+			<td data-title="Notes">'.$book_notes.'</td>
+			<td data-title="Booken on">'.$reserved_on.'</td>
+			<td data-title="Return on">'.$toreturn_on.'</td>
+			<td data-title="Status">'.$book_status.'</td>
+			</tr>';
+	}
+
+	$stmt2->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	</div><!-- /panel-group -->
+
     </div><!-- /container -->
 
 	<?php include 'includes/footers/footer.php'; ?>
