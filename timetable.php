68460deacfc72511b64faf4f5b3a53972ac39d79
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 22:28:12 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -319,7 +319,7 @@ include 'includes/session.php';
 
 	<!-- Completed tasks -->
 	<section id="no-more-tables">
-	<table class="table table-condensed table-custom">
+	<table class="table table-condensed table-custom module-table">
 
 	<thead>
 	<tr>
@@ -327,8 +327,6 @@ include 'includes/session.php';
 	<th>Notes</th>
 	<th>URL</th>
 	<th>Action</th>
-    <th>Action</th>
-    <th>Action</th>
 	</tr>
 	</thead>
 
