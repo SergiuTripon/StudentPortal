c7811b382a4ff2339e5896b62ae543e433adfab0
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 19:24:54 +0000

    Changed structure

--- includes/processes.php
+++ includes/processes.php
@@ -5,15 +5,7 @@ include 'functions.php';
 
 //Call UpdateEvent function
 if (isset(
-    $_POST['eventid1'],
-    $_POST['event_name1'],
-    $_POST['event_notes1'],
-    $_POST['event_url1'],
-    $_POST['event_from1'],
-    $_POST['event_to1'],
-    $_POST['event_amount1'],
-    $_POST['event_ticket_no1'],
-    $_POST['event_category1'])) {
+    $_POST['eventid1'])) {
     UpdateEvent();
 } else {
     header('HTTP/1.0 550 blah blah blah.');
