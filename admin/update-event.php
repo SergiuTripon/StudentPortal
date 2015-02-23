5232d189fba4010102c57713d91a3d3ab5a75a51
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-23 19:03:16 +0000

    Changed structure

--- admin/update-event.php
+++ admin/update-event.php
@@ -343,15 +343,15 @@ if (isset($_POST["eventToUpdate"])) {
     jQuery.ajax({
 	type: "POST",
 	url: "https://student-portal.co.uk/includes/processes.php",
-    data:'eventid='          + eventid +
-         'event_name='       + event_name +
-         '&event_notes='     + event_notes +
-         '&event_url='       + event_url +
-         '&event_from='      + event_from +
-         '&event_to='        + event_to +
-         '&event_amount='    + event_amount +
-         '&event_ticket_no=' + event_ticket_no +
-         '&event_category='  + event_category,
+    data:'eventid1='          + eventid +
+         'event_name1='       + event_name +
+         '&event_notes1='     + event_notes +
+         '&event_url1='       + event_url +
+         '&event_from1='      + event_from +
+         '&event_to1='        + event_to +
+         '&event_amount1='    + event_amount +
+         '&event_ticket_no1=' + event_ticket_no +
+         '&event_category1='  + event_category,
 
     success:function(){
 		$("#error").hide();
