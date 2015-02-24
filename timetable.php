0189d0ee9e41877d9eeda932e9cb31d6f3fdf9f4
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 13:28:02 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -390,6 +390,9 @@ include 'includes/session.php';
 	<script>
     $(document).ready(function () {
 
+    //Ladda
+    Ladda.bind('.ladda-button', {timeout: 2000});
+
 	//DataTables
     $('.lecture-table').dataTable({
         "iDisplayLength": 10,
