e97b8d963bbca6b06ee004e5d270aaff27ac132e
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 19:47:33 +0000

    Changed structure

--- account/change-password.php
+++ account/change-password.php
@@ -38,16 +38,19 @@ include '../includes/session.php';
     <div id="hide">
 
 	<div class="form-group">
-	<div class="col-xs-6 col-sm-6 full-width pl0">
+	<div class="col-xs-12 col-sm-12 full-width pl0">
     <label for="password">New password</label>
     <input class="form-control" type="password" name="password" id="password" placeholder="New password">
 	</div>
-	<div class="col-xs-6 col-sm-6 full-width pr0">
+    </div>
+
+    <div class="form-group">
+	<div class="col-xs-12 col-sm-12 full-width pr0">
     <label for="confirmpwd">New password confirmation</label>
     <input class="form-control" type="password" name="confirmpwd" id="confirmpwd" placeholder="Confirm new password">
 	</div>
-    <p id="error1" class="feedback-sad text-center"></p>
 	</div>
+    <p id="error" class="feedback-sad text-center"></p>
 
 	<hr>
 
