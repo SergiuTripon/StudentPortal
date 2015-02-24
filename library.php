c2f1c4d72e60706494a9ae2e2ddf4cd647935e7d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 22:54:22 +0000

    Changed structure

--- library.php
+++ library.php
@@ -74,7 +74,7 @@ include 'includes/session.php';
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a clas="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Available books</a>
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Available books</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
