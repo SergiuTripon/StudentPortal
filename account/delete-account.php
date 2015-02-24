b4345b0100d19bff5fce675359c76b032b0f21e2
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 20:00:54 +0000

    Changed structure

--- account/delete-account.php
+++ account/delete-account.php
@@ -2,7 +2,7 @@
 include '../includes/session.php';
 
 $stmt1 = $mysqli->prepare("SELECT user_signin.email, user_details.studentno, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid = user_details.userid WHERE user_signin.userid = ? LIMIT 1");
-$stmt1->bind_param('i', $userid);
+$stmt1->bind_param('i', $session_userid);
 $stmt1->execute();
 $stmt1->store_result();
 $stmt1->bind_result($email, $studentno, $firstname, $surname);
