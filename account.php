ca5e3ad5c1c7934cd1fe0657f8f6cdf645765c55
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 13:08:18 +0000

    Changed structure

--- account.php
+++ account.php
@@ -238,38 +238,123 @@ include 'includes/session.php';
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
 
+    	<div class="panel panel-default">
+
+	<?php
+    //Update an account
+	$stmt2 = $mysqli->query("SELECT user_signin.userid FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE NOT user_signin.userid = '$userid'");
+	while($row = $stmt2->fetch_assoc()) {
+		echo '<form id="update-an-account-form-'.$row["userid"].'" style="display: none;" action="../update-an-account/" method="POST">
+		<input type="hidden" name="userToUpdate" id="userToUpdate" value="'.$row["userid"].'"/>
+		</form>';
+	}
+	$stmt2->close();
+	?>
+
+    <?php
+    //Change an account's password
+	$stmt2 = $mysqli->query("SELECT user_signin.userid FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE NOT user_signin.userid = '$userid'");
+	while($row = $stmt2->fetch_assoc()) {
+		echo '<form id="change-password-form-'.$row["userid"].'" style="display: none;" action="../change-account-password/" method="POST">
+		<input type="hidden" name="userToChangePassword" id="userToChangePassword" value="'.$row["userid"].'"/>
+		</form>';
+	}
+	$stmt2->close();
+	?>
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Users</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Update an account -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom">
+
+	<thead>
+	<tr>
+	<th>User ID</th>
+	<th>Full name</th>
+	<th>Account type</th>
+    <th>Created on</th>
+	<th>Updated on</th>
+	<th>Action</th>
+    <th>Action</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt4 = $mysqli->query("SELECT user_signin.userid, user_details.firstname, user_details.surname, user_signin.email, user_signin.account_type, DATE_FORMAT(user_signin.created_on,'%d %b %y %H:%i') as created_on, DATE_FORMAT(user_details.updated_on,'%d %b %y %H:%i') as updated_on FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE NOT user_signin.userid = '$userid'");
+
+	while($row = $stmt4->fetch_assoc()) {
+
+	$account_type = ucfirst($row["account_type"]);
+
+	echo '<tr id="user-'.$row["userid"].'">
+
+			<td data-title="User ID">'.$row["userid"].'</td>
+			<td data-title="Full name">'.$row["firstname"].' '.$row["surname"].'</td>
+			<td data-title="Account type">'.$account_type.'</td>
+			<td data-title="Created on">'.$row["created_on"].'</td>
+            <td data-title="Updated on">'.$row["updated_on"].'</td>
+			<td data-title="Update"><a id="update-'.$row["userid"].'" class="btn btn-primary btn-md update-button">Update</a></td>
+            <td data-title="Change"><a id="change-'.$row["userid"].'" class="btn btn-primary btn-md change-button">Change</a></td>
+            <td data-title="Delete"><a class="btn btn-primary btn-md delete-button" href="#modal-'.$row["userid"].'" data-toggle="modal">Delete</a></td>
+			</tr>
+
+			<!-- Delete an account modal -->
+    		<div class="modal fade modal-custom" id="modal-'.$row["userid"].'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+    		<div class="modal-dialog">
+    		<div class="modal-content">
+
+			<div class="modal-header">
+			<div class="form-logo text-center">
+			<i class="fa fa-trash"></i>
+			</div>
+			</div>
+
+			<div class="modal-body">
+			<p id="success" class="feedback-custom text-center">Are you sure you want to delete this account?</p>
+			</div>
+
+			<div class="modal-footer">
+			<div id="hide">
+			<div class="pull-left">
+			<a id="delete-'.$row["userid"].'" class="btn btn-danger btn-lg delete-button1 ladda-button" data-style="slide-up">Yes</a>
+			</div>
+			<div class="text-right">
+			<button type="button" class="btn btn-success btn-lg ladda-button" data-style="slide-up" data-dismiss="modal">No</button>
+			</div>
+			</div>
+			<div class="text-center">
+			<a id="success-button" class="btn btn-primary btn-lg ladda-button" style="display: none;" data-style="slide-up">Continue</a>
+			</div>
+			</div>
+
+			</div><!-- /modal -->
+			</div><!-- /modal-dialog -->
+			</div><!-- /modal-content -->
+			<!-- End of Delete an account modal -->';
+	}
+
+	$stmt4->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
     </div><!-- /panel-group -->
-
-    <div class="row">
-
-	<div class="col-xs-12 col-sm-6 col-md-3 col-lg-6">
-    <a href="/admin/create-an-account/">
-    <div class="tile">
-    <i class="fa fa-user-plus"></i>
-	<p class="tile-text">Create an account</p>
-    </div>
-    </a>
-	</div>
-
-    <!--<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-    <a href="/create-multiple-accounts/">
-    <div class="tile">
-    <i class="fa fa-users"></i>
-    <p class="tile-text">Create multiple accounts</p>
-    </div>
-    </a>
-    </div>-->
-	
-    <div class="col-xs-12 col-sm-4 col-md-3 col-lg-6">
-	<a href="/admin/update-delete-an-account/">
-    <div class="tile">
-    <i class="fa fa-wrench"></i>
-	<p class="tile-text">Update/Delete an account</p>
-	</div>
-    </a>
-	</div>
-
-    </div><!-- /row -->
 
     </div><!-- /container -->
 	
