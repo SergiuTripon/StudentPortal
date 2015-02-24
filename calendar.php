383967849f59d9fb097443bd06e45b86f067b038
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 17:59:53 +0000

    Changed structure

--- calendar.php
+++ calendar.php
@@ -205,12 +205,12 @@ include 'includes/session.php';
   	</div><!-- /panel-default -->
 
 	<div id="cancelledtasks-toggle" class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
+    <div class="panel-heading" role="tab" id="headingThree">
   	<h4 class="panel-title">
-	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"> Completed tasks</a>
+	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree"> Cancelled tasks</a>
   	</h4>
     </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
   	<div class="panel-body">
 
 	<!-- Completed tasks -->
