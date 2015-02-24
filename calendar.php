2a79d2e7e3584bd311d8275b99ba5d0eb0d43b5d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 18:13:59 +0000

    Changed structure

--- calendar.php
+++ calendar.php
@@ -355,6 +355,9 @@ include 'includes/session.php';
 	<script>
     $(document).ready(function () {
 
+    //Ladda
+    Ladda.bind('.ladda-button', {timeout: 2000});
+
 	(function($) {
 
 	"use strict";
