43176d3653d3feb6507d11e8d79504e6ca322b50
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-24 21:42:04 +0000

    Changed structure

--- messenger/message-user.php
+++ messenger/message-user.php
@@ -6,17 +6,17 @@ if (isset($_POST["recordToMessage"])) {
     $idToMessage = filter_input(INPUT_POST, 'recordToMessage', FILTER_SANITIZE_NUMBER_INT);
 
     $stmt1 = $mysqli->prepare("SELECT user_signin.userid, user_signin.email, user_details.studentno, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
-    $stmt1->bind_param('i', $userid);
+    $stmt1->bind_param('i', $session_userid);
     $stmt1->execute();
     $stmt1->store_result();
-    $stmt1->bind_result($userid1, $email1, $studentno1, $firstname1, $surname1);
+    $stmt1->bind_result($userid, $email1, $firstname1, $surname1);
     $stmt1->fetch();
 
     $stmt1 = $mysqli->prepare("SELECT user_signin.userid, user_signin.email, user_details.studentno, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
     $stmt1->bind_param('i', $idToMessage);
     $stmt1->execute();
     $stmt1->store_result();
-    $stmt1->bind_result($userid2, $email2, $studentno2, $firstname2, $surname2);
+    $stmt1->bind_result($userid, $email2, $firstname2, $surname2);
     $stmt1->fetch();
 
 } else {
