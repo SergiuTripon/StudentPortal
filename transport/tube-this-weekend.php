cedf2b076a3c502acd13be3709f0464edacea7d3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-25 21:54:29 +0000

    Changed structure

--- transport/tube-this-weekend.php
+++ transport/tube-this-weekend.php
@@ -42,7 +42,7 @@ GetThisWeekendTubeStatus();
 	<div class="panel panel-default">
 	<div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
-	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Tube | This weekend | Line status</a>
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Tube | This weekend | Line status</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
