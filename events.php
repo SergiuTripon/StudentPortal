583738fb76c7c29678828c7e1ad277844389783f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-25 21:00:12 +0000

    Changed structure

--- events.php
+++ events.php
@@ -122,7 +122,7 @@ include 'includes/session.php';
 			<td data-title="Price">'.$event_amount.'</td>
 			<td data-title="Tickets">'.($event_ticket_no === '0' ? "Sold Out" : "$event_ticket_no").'</td>
 			<td data-title="Category">'.$event_category.'</td>
-			<td data-title="Action">'.($event_ticket_no === '0' ? "Sold Out" : "<a id=\"book-$eventid\" class=\"btn btn-primary btn-md book-button\">Book</a>").'</td>
+			<td data-title="Action">'.($event_ticket_no === '0' ? "Sold Out" : "<a id=\"book-$eventid\" class=\"btn btn-primary btn-md ladda-button book-button\" data-style=\"slide-up\"><span class=\"ladda-label\">Book</span></a>").'</td>
 			</tr>';
 	}
 
