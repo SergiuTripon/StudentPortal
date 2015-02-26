587594f17ab1f4020fef55321e8c94817adf2f9f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 19:02:50 +0000

    Changed structure

--- includes/timetable/getUsers.php
+++ includes/timetable/getUsers.php
@@ -13,15 +13,12 @@ include '../../includes/session.php';
     $firstname = $row["firstname"];
     $surname = $row["surname"];
 
-    $stmt3 = $mysqli->prepare("SELECT userid FROM user_timetable WHERE userid = ? AND moduleid = ?");
-    $stmt3->bind_param('ii', $db_userid, $moduleToAssign);
-    $stmt3->execute();
-    $stmt3->store_result();
-    $stmt3->bind_result($db_userid1);
-    $stmt3->fetch();
+    $stmt2 = $mysqli->prepare("SELECT userid FROM user_timetable WHERE userid = ? AND moduleid = ?");
+    $stmt2->bind_param('ii', $db_userid, $moduleToAssign);
+    $stmt2->execute();
 
-    $assignment_check = $stmt3->num_rows ? 'Already assigned' : '<a id="assign-'.$db_userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
-    $assignment_check1 = $stmt3->num_rows ? '<a id="assign-'.$db_userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
+    $assignment_check = $stmt2->num_rows === 0 ? 'Already assigned' : '<a id="assign-'.$db_userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
+    $unassignment_check = $stmt2->num_rows === 0 ? '<a id="unnasign-'.$db_userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
 
 	echo '<tr id="assign-'.$db_userid.'">
 
@@ -29,9 +26,9 @@ include '../../includes/session.php';
 			<td data-title="Surname">'.$surname.'</td>
 			<td data-title="Email address">'.$email.'</td>
 			<td data-title="Action">'.$assignment_check.'</td>
-			<td data-title="Action">'.$assignment_check1.'</td>
+			<td data-title="Action">'.$unassignment_check.'</td>
 			</tr>';
-	}
-
+	$stmt2->close();
+    }
 	$stmt1->close();
 	?>
