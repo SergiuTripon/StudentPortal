bedc64adab4cd6b9fb93c3173d8a8b6f20da565f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 18:01:39 +0000

    Changed structure

--- includes/timetable/getModules.php
+++ includes/timetable/getModules.php
@@ -1,7 +1,7 @@
 <?php
 include '../../includes/session.php';
 ?>
-
+    <!-- Update timetable -->
     <?php
 	$stmt1 = $mysqli->query("SELECT moduleid FROM system_modules WHERE module_status = 'active'");
 	while($row = $stmt1->fetch_assoc()) {
@@ -12,6 +12,7 @@ include '../../includes/session.php';
 	$stmt1->close();
 	?>
 
+    <!-- Assign timetable -->
     <?php
     $stmt2 = $mysqli->query("SELECT moduleid FROM system_modules WHERE module_status = 'active'");
     while($row = $stmt2->fetch_assoc()) {
@@ -38,9 +39,9 @@ include '../../includes/session.php';
 			<td data-title="Name">'.$module_name.'</td>
 			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
             <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-            <td data-title="Action"><a id="assign-'.$moduleid.'" class="btn btn-primary btn-md ladda-button assign-button">Assign</a></td>
-			<td data-title="Action"><a id="update-'.$moduleid.'" class="btn btn-primary btn-md ladda-butto update-button">Update</a></td>
-            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md ladda-button cancel-button">Cancel</a></td>
+            <td data-title="Action"><a id="assign-'.$moduleid.'" class="btn btn-primary btn-md ladda-button assign-button" data-style=\"slide-up\"><span class=\"ladda-label\">Assign</span></a></td>
+			<td data-title="Action"><a id="update-'.$moduleid.'" class="btn btn-primary btn-md ladda-butto update-button" data-style=\"slide-up\"><span class=\"ladda-label\">Update</span></a></td>
+            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md ladda-button cancel-button" data-style=\"slide-up\"><span class=\"ladda-label\">Cancel</span></a></td>
 			</tr>';
 	}
 
