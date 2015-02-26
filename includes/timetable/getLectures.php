80c1232fa9c3d77d1c33f1f15653030b217f1268
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 12:12:35 +0000

    Changed structure

--- includes/timetable/getLectures.php
+++ includes/timetable/getLectures.php
@@ -2,73 +2,40 @@
 include '../../includes/session.php';
 ?>
 
-    <!-- Lectures -->
-	<section id="no-more-tables">
-	<table class="table table-condensed table-custom student-lecture-table">
-	<thead>
+    <thead>
 	<tr>
 	<th>Name</th>
-	<th>Lecturer</th>
-	<th>Day</th>
-	<th>From</th>
-    <th>To</th>
-    <th>Location</th>
-    <th>Capacity</th>
+	<th>Notes</th>
+	<th>URL</th>
+	<th>Action</th>
+    <th>Action</th>
+    <th>Action</th>
 	</tr>
 	</thead>
 
 	<tbody>
 	<?php
 
-	$stmt1 = $mysqli->query("SELECT system_lectures.lecture_name, system_lectures.lecture_lecturer, system_lectures.lecture_day, DATE_FORMAT(system_lectures.lecture_from_time,'%H:%i') as lecture_from_time, DATE_FORMAT(system_lectures.lecture_to_time,'%H:%i') as lecture_to_time, system_lectures.lecture_location, system_lectures.lecture_capacity FROM user_timetable LEFT JOIN system_modules ON user_timetable.moduleid=system_modules.moduleid LEFT JOIN system_lectures ON user_timetable.moduleid=system_lectures.moduleid WHERE user_timetable.userid='$session_userid' AND system_lectures.lecture_status='active' LIMIT 1");
+	$stmt3 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'active'");
 
-	while($row = $stmt1->fetch_assoc()) {
+	while($row = $stmt3->fetch_assoc()) {
 
-	$lecture_name = $row["lecture_name"];
-	$lecture_lecturer = $row["lecture_lecturer"];
-	$lecture_day = $row["lecture_day"];
-	$lecture_from_time = $row["lecture_from_time"];
-	$lecture_to_time = $row["lecture_to_time"];
-	$lecture_location = $row["lecture_location"];
-	$lecture_capacity = $row["lecture_capacity"];
+    $moduleid = $row["moduleid"];
+	$module_name = $row["module_name"];
+	$module_notes = $row["module_notes"];
+	$module_url = $row["module_url"];
 
-	$stmt2 = $mysqli->prepare("SELECT firstname, surname FROM user_details WHERE userid = ? LIMIT 1");
-	$stmt2->bind_param('i', $lecture_lecturer);
-	$stmt2->execute();
-	$stmt2->store_result();
-	$stmt2->bind_result($firstname, $surname);
-	$stmt2->fetch();
+	echo '<tr id="activate-'.$moduleid.'">
 
-	echo '<tr>
-
-			<td data-title="Name">'.$lecture_name.'</td>
-			<td data-title="Lecturer">'.$firstname.' '.$surname.'</td>
-			<td data-title="From">'.$lecture_day.'</td>
-			<td data-title="From">'.$lecture_from_time.'</td>
-			<td data-title="To">'.$lecture_to_time.'</td>
-			<td data-title="Location">'.$lecture_location.'</td>
-			<td data-title="Capacity">'.$lecture_capacity.'</td>
+			<td data-title="Name">'.$module_name.'</td>
+			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
+            <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
+            <td data-title="Action"><a id="assign-'.$moduleid.'" class="btn btn-primary btn-md assign-button">Assign</a></td>
+			<td data-title="Action"><a id="update-'.$moduleid.'" class="btn btn-primary btn-md update-button">Update</a></td>
+            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md cancel-button">Cancel</a></td>
 			</tr>';
 	}
 
-	$stmt1->close();
+	$stmt3->close();
 	?>
 	</tbody>
-	</table>
-	</section>
-    <!-- End of Lectures -->
-
-    <?php include '../../includes/assets/js-paths/datatables-js-path.php'; ?>
-
-    <script>
-	//DataTables
-    $('.student-lecture-table').dataTable({
-        "iDisplayLength": 10,
-		"paging": true,
-		"ordering": true,
-		"info": false,
-		"language": {
-			"emptyTable": "You have no lectures on this day."
-		}
-	});
-	</script>
