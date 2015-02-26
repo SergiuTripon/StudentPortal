796a5bf17522ced7f434e14e68302d37e056e562
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 11:50:39 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -50,7 +50,6 @@ include 'includes/session.php';
 	</div><!-- /panel-default -->
 
 	<div class="panel panel-default">
-
     <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Tutorials</a>
@@ -59,6 +58,72 @@ include 'includes/session.php';
     </div>
     <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
   	<div id="student-tutorials" class="panel-body">
+    <!-- Update timetable -->
+    <?php
+	$stmt1 = $mysqli->query("SELECT moduleid FROM system_modules WHERE module_status = 'active'");
+	while($row = $stmt1->fetch_assoc()) {
+	  echo '<form id="update-timetable-form-'.$row["moduleid"].'" style="display: none;" action="/admin/update-timetable/" method="POST">
+			<input type="hidden" name="timetableToUpdate" id="timetableToUpdate" value="'.$row["moduleid"].'"/>
+			</form>';
+	}
+	$stmt1->close();
+	?>
+
+    <!-- Assign timetable -->
+    <?php
+    $stmt2 = $mysqli->query("SELECT moduleid FROM system_modules WHERE module_status = 'active'");
+    while($row = $stmt2->fetch_assoc()) {
+        echo '<form id="assign-timetable-form-'.$row["moduleid"].'" style="display: none;" action="/admin/assign-timetable/" method="POST">
+        <input type="hidden" name="timetableToAssign" id="timetableToAssign" value="'.$row["moduleid"].'"/>
+        </form>';
+    }
+    $stmt2->close();
+    ?>
+
+	<!-- Modules -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom admin-modules-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Notes</th>
+	<th>URL</th>
+	<th>Action</th>
+    <th>Action</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt3 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'active'");
+
+	while($row = $stmt3->fetch_assoc()) {
+
+    $moduleid = $row["moduleid"];
+	$module_name = $row["module_name"];
+	$module_notes = $row["module_notes"];
+	$module_url = $row["module_url"];
+
+	echo '<tr id="cancel-'.$moduleid.'">
+
+			<td data-title="Name">'.$module_name.'</td>
+			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
+            <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
+            <td data-title="Action"><a id="assign-'.$moduleid.'" class="btn btn-primary btn-md ladda-button assign-button" data-style="slide-up"><span class="ladda-label">Assign</span></a></td>
+			<td data-title="Action"><a id="update-'.$moduleid.'" class="btn btn-primary btn-md ladda-button update-button ladda-button" data-style="slide-up"><span class="ladda-label">Update</span></a></td>
+            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md ladda-button cancel-button ladda-button" data-style="slide-up"><span class="ladda-label">Cancel</span></a></td>
+			</tr>';
+	}
+
+	$stmt3->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
     </div><!-- /panel-body -->
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
@@ -173,7 +238,6 @@ include 'includes/session.php';
 
     $('#student-lectures').load('https://student-portal.co.uk/includes/timetable/getLectures.php');
     $('#student-tutorials').load('https://student-portal.co.uk/includes/timetable/getTutorials.php');
-    $('#admin-modules').load('https://student-portal.co.uk/includes/timetable/getModules.php');
     $('#admin-cancelled-modules').load('https://student-portal.co.uk/includes/timetable/getCancelledModules.php');
 
     });
