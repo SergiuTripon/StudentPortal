5aea83e0c099e3d685d0c547fda1e8497b85fba2
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 22:22:10 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -250,9 +250,9 @@ include 'includes/session.php';
 			<td data-title="Name">'.$module_name.'</td>
 			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
             <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-            <td data-title="Action"><a class="btn btn-primary btn-md assign-button" href="/admin/assign-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Assign</span></a></a></td>
-			<td data-title="Action"><a class="btn btn-primary btn-md update-button" href="/admin/update-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></a></td>
-            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md cancel-button" data-style="slide-up"><span class="ladda-label">Cancel</span></a></a></td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button assign-button" href="/admin/assign-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Assign</span></a></a></td>
+			<td data-title="Action"><a class="btn btn-primary btn-md ladda-button update-button" href="/admin/update-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></a></td>
+            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md ladda-button cancel-button" data-style="slide-up"><span class="ladda-label">Cancel</span></a></a></td>
 			</tr>';
 	}
 
@@ -369,7 +369,6 @@ include 'includes/session.php';
 	<?php include 'assets/js-paths/calendar-js-path.php'; ?>
 
 	<script>
-
     //Ladda
     Ladda.bind('.ladda-button', {timeout: 2000});
 
