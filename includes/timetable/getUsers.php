e1d2551ce559696dcad6c33f664775138883461e
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 19:15:03 +0000

    Changed structure

--- includes/timetable/getUsers.php
+++ includes/timetable/getUsers.php
@@ -30,8 +30,8 @@ include '../../includes/session.php';
         $stmt2->bind_param('ii', $userid, $moduleToAssign);
         $stmt2->execute();
 
-        $assignment_check = $stmt2->num_rows > 1 ? 'Already assigned' : '<a id="assign-'.$userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
-        $unassignment_check = $stmt2->num_rows === '' ? '<a id="unnasign-'.$userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
+        $assignment_check = $stmt2->num_rows === 0 ? 'Already assigned' : '<a id="assign-'.$userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
+        $unassignment_check = $stmt2->num_rows != 0 ? '<a id="unnasign-'.$userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
 
         echo '<tr id="assign-'.$userid.'">
 
