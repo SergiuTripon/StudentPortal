6d460698ccdc743973a5b5d688934eaa906b814d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 19:56:50 +0000

    Changed structure

--- includes/timetable/getUsers.php
+++ includes/timetable/getUsers.php
@@ -2,38 +2,28 @@
 include '../../includes/session.php';
 ?>
 
-<table class="table table-condensed table-custom">
-
-    <thead>
-    <tr>
-        <th>First Name</th>
-        <th>Surname</th>
-        <th>Email address</th>
-        <th>Action</th>
-        <th>Action</th>
-    </tr>
-    </thead>
-
-    <tbody id="loadUsers-table">
     <?php
 
-    $stmt1 = $mysqli->query("SELECT user_signin.userid, user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.account_type = 'student'");
+	$stmt1 = $mysqli->query("SELECT user_signin.userid, user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.account_type = 'student'");
 
-    while($row = $stmt1->fetch_assoc()) {
+	while($row = $stmt1->fetch_assoc()) {
 
-        $db_userid = $row["userid"];
-        $email = $row["email"];
-        $firstname = $row["firstname"];
-        $surname = $row["surname"];
+	$userid = $row["userid"];
+	$email = $row["email"];
+    $firstname = $row["firstname"];
+    $surname = $row["surname"];
 
-        $stmt2 = $mysqli->prepare("SELECT userid FROM user_timetable WHERE userid = ? AND moduleid = ?");
-        $stmt2->bind_param('ii', $db_userid, $moduleToAssign);
-        $stmt2->execute();
+    $stmt2 = $mysqli->prepare("SELECT userid FROM user_timetable WHERE userid = ? AND moduleid = ?");
+    $stmt2->bind_param('ii', $userid, $moduleToAssign);
+    $stmt2->execute();
+    $stmt2->store_result();
+    $stmt2->bind_result($db_userid);
+    $stmt2->fetch();
 
-        $assignment_check = $stmt2->num_rows === '' ? 'Already assigned' : '<a id="assign-'.$db_userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
-        $unassignment_check = $stmt2->num_rows === '' ? '<a id="unnasign-'.$db_userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
+    $assignment_check = (empty($db_userid)) ? 'Already assigned' : '<a id="assign-'.$userid.'" class="btn btn-primary btn-md assign-button">Assign</a>';
+    $unassignment_check = (!empty($db_userid)) ? '<a id="unnasign-'.$userid.'" class="btn btn-primary btn-md unassign-button">Unassign</a>' : 'Not assigned yet';
 
-        echo '<tr id="assign-'.$db_userid.'">
+	echo '<tr id="assign-'.$userid.'">
 
 			<td data-title="First name">'.$firstname.'</td>
 			<td data-title="Surname">'.$surname.'</td>
@@ -41,10 +31,7 @@ include '../../includes/session.php';
 			<td data-title="Action">'.$assignment_check.'</td>
 			<td data-title="Action">'.$unassignment_check.'</td>
 			</tr>';
-        $stmt2->close();
+    $stmt2->close();
     }
-    $stmt1->close();
-    ?>
-    </tbody>
-
-</table>
+	$stmt1->close();
+	?>
