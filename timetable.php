4f5c2e65656cddcfa70c5be2f3d2025dab604ef3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 13:33:41 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -208,13 +208,13 @@ include 'includes/session.php';
     <a class="btn btn-success btn-lg ladda-button mt10" data-style="slide-up" href="/admin/create-timetable/"><span class="ladda-label">Create timetable</span></a>
 
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-
 	<div class="panel panel-default">
 
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Timetables</a>
-  	</h4>
+    <a id="loadModules"><i class="fa fa-refresh"></i></a>
+    </h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
   	<div class="panel-body">
@@ -294,12 +294,13 @@ include 'includes/session.php';
     <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
 	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">  Cancelled timetables</a>
-  	</h4>
+    <a id="loadCancelledModules"><i class="fa fa-refresh"></i></a>
+    </h4>
     </div>
     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
   	<div class="panel-body">
 
-	<!-- Completed tasks -->
+	<!-- Cancelled modules -->
 	<section id="no-more-tables">
 	<table class="table table-condensed table-custom module-table">
 
