96c61a203cda2303c23f251b93e2d4410cc83fbb
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 14:19:00 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -36,10 +36,11 @@ include 'includes/session.php';
 	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
 	<div class="panel panel-default">
+
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Lectures</a>
-    <a id="loadLectures" class="pull-right"><i class="fa fa-refresh"></i></a>
+    <a id="loadLectures"><i class="fa fa-refresh"></i></a>
     </h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
@@ -111,7 +112,7 @@ include 'includes/session.php';
     <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Tutorials</a>
-    <a id="loadTutorials" class="pull-right"><i class="fa fa-refresh"></i></a>
+    <a id="loadTutorials"><i class="fa fa-refresh"></i></a>
     </h4>
     </div>
     <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
@@ -292,7 +293,7 @@ include 'includes/session.php';
     <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
 	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">  Cancelled timetables</a>
-    <a id="loadCancelledModules" class="pull-right"><i class="fa fa-refresh"></i></a>
+    <a id="loadCancelledModules"><i class="fa fa-refresh"></i></a>
     </h4>
     </div>
     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
