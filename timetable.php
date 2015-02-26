cfebba51b60115f2f83c128601e4e9461e865c4a
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 09:25:47 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -389,7 +389,7 @@ include 'includes/session.php';
 	});
 
     function getLectures(){
-        $('#collapseOne .panel-body').load('https://student-portal.co.uk/includes/timetable/getLectures.php');
+        $('.panel-body').load('https://student-portal.co.uk/includes/timetable/getLectures.php');
     }):
 
 	</script>
