d3474a7d12561c0a40b6a6eaa50018abdf45bb96
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 20:12:04 +0000

    Changed structure

--- admin/assign-timetable.php
+++ admin/assign-timetable.php
@@ -63,15 +63,13 @@ if (isset($_GET['id'])) {
 	<th>First Name</th>
 	<th>Surname</th>
 	<th>Email address</th>
-	<th>Action</th>
-    <th>Action</th>
 	</tr>
 	</thead>
 
 	<tbody id="loadUsers-table">
     <?php
 
-	$stmt1 = $mysqli->query("SELECT user_signin.userid, user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid LEFT JOIN user_timetable ON user_signin.userid=user_timetable.userid WHERE user_signin.account_type = 'student' AND user_timetable.moduleid = '$timetableToAssign'");
+	$stmt1 = $mysqli->query("SELECT user_signin.userid, user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid LEFT JOIN user_timetable ON user_signin.userid=user_timetable.userid WHERE user_signin.account_type = 'student' AND NOT user_timetable.moduleid = '$timetableToAssign'");
 
 	while($row = $stmt1->fetch_assoc()) {
 
