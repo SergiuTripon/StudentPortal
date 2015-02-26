3713260685e9c8ed826c39bca67cb6264a49e526
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-26 22:26:19 +0000

    Changed structure

--- admin/assign-timetable.php
+++ admin/assign-timetable.php
@@ -236,7 +236,9 @@ if (isset($_GET['id'])) {
 	data:'userToAssign='+ userToAssign + '&timetableToAssign='+ timetableToAssign,
 	success:function(){
         $('#assign-'+userToAssign).hide();
-        $('#loadUnallocatedStudents-table').load('https://student-portal.co.uk/includes/timetable/getAllocatedStudents.php');
+        setTimeout(function(){
+            location.reload();
+        }, 1000);
     },
 
 	error:function (xhr, ajaxOptions, thrownError){
@@ -263,7 +265,9 @@ if (isset($_GET['id'])) {
 	data:'userToUnassign='+ userToUnassign + '&timetableToUnassign='+ timetableToUnassign,
 	success:function(){
         $('#unassign-'+userToUnassign).hide();
-        $('#loadAllocatedStudents-table').load('https://student-portal.co.uk/includes/timetable/getUnallocatedStudents.php');
+        setTimeout(function(){
+            location.reload();
+        }, 1000);
     },
 
 	error:function (xhr, ajaxOptions, thrownError){
