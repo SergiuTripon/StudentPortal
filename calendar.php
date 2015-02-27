857b440f6a6a99dbd1a80249b5502e1dafc419a9
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-27 09:49:09 +0000

    Changed structure

--- calendar.php
+++ calendar.php
@@ -411,7 +411,7 @@ include 'includes/session.php';
     //Responsiveness
 	$(window).resize(function(){
 		var width = $(window).width();
-		if(width <= 600){
+		if(width <= 500){
 			$('.btn-group').addClass('btn-group-vertical full-width');
             $('#calendar-buttons2').addClass("mt10");
         } else {
