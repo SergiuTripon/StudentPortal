48a8445d58f9eb5d3250d149a6637472629de0ac
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-27 23:29:37 +0000

    Changed structure

--- account.php
+++ account.php
@@ -230,9 +230,9 @@ include 'includes/session.php';
 			<td data-title="Account type">'.$account_type.'</td>
 			<td data-title="Created on">'.$created_on.'</td>
             <td data-title="Updated on">'.$updated_on.'</td>
-			<td data-title="Update"><a class="btn btn-primary btn-md" href="/admin/update-an-account?id='.$userid.'">Update</a></td>
-            <td data-title="Change"><a id="change-'.$userid.'" class="btn btn-primary btn-md change-button">Change</a></td>
-            <td data-title="Delete"><a class="btn btn-primary btn-md delete-button" href="#modal-'.$userid.'" data-toggle="modal">Delete</a></td>
+			<td data-title="Update"><a class="btn btn-primary btn-md ladda-button" href="/admin/update-an-account?id='.$userid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></td>
+            <td data-title="Change"><a id="change-'.$userid.'" class="btn btn-primary btn-md ladda-button change-button" data-style="slide-up"><span class="ladda-label">Change password</span></a></td>
+            <td data-title="Delete"><a class="btn btn-primary btn-md ladda-button delete-button" href="#modal-'.$userid.'" data-toggle="modal" data-style="slide-up"><span class="ladda-label">Delete</span></a></td>
 			</tr>
 
     		<div class="modal modal-custom fade" id="modal-'.$userid.'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
