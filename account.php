4bc1dc7b9a7b68c6ee8bc92058e712f313de8fc3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-27 23:33:00 +0000

    Changed structure

--- account.php
+++ account.php
@@ -146,7 +146,7 @@ include 'includes/session.php';
 			
     <div class="row">
 
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
     <a href="/account/update-account/">
     <div class="tile">
     <i class="fa fa-refresh"></i>
