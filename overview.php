20d9e9aaca9ec573b42e17f9c73d598013887555
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-27 09:23:14 +0000

    Changed structure

--- overview.php
+++ overview.php
@@ -2,6 +2,15 @@
 include 'includes/session.php';
 include 'includes/functions.php';
 
+    global $mysqli;
+    global $session_userid;
+    global $timetable_count;
+    global $exams_count;
+    global $library_count;
+    global $calendar_count;
+    global $events_count;
+    global $messenger_count;
+
     GetDashboardData();
 
 ?>
@@ -42,86 +51,86 @@ include 'includes/functions.php';
 	</div>
     </a>
 
+    <a href="../exams/">
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-	<a href="../exams/">
     <div class="tile">
 	<i class="fa fa-pencil"></i>
 	<p>Exams<span class="badge"><?php echo ($exams_count == '0' ? "" : "$exams_count"); ?></span></p>
     </div>
-    </a>
 	</div>
+    </a>
 
+    <a href="../library/">
     <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-	<a href="../library/">
 	<div class="tile">
     <i class="fa fa-book"></i>
 	<p>Library<span class="badge"><?php echo ($library_count == '0' ? "" : "$library_count"); ?></span></p>
     </div>
-    </a>
 	</div>
+    </a>
 
+    <a href="../transport/">
     <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-	<a href="../transport/">
     <div class="tile">
     <i class="fa fa-subway"></i>
 	<p>Transport</p>
     </div>
-    </a>
 	</div>
-	
+    </a>
+
+    <a href="../calendar/">
 	<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-	<a href="../calendar/">
 	<div class="tile">
     <i class="fa fa-calendar"></i>
 	<p>Calendar<span class="badge"><?php echo ($calendar_count == '0' ? "" : "$calendar_count"); ?></span></p>
     </div>
-    </a>
 	</div>
+    </a>
 
+    <a href="../events/">
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-	<a href="../events/">
     <div class="tile">
 	<i class="fa fa-ticket"></i>
 	<p>Events<span class="badge"><?php echo ($events_count == '0' ? "" : "$events_count"); ?></span></p>
     </div>
-    </a>
 	</div>
+    </a>
 
+    <a href="../university-map/">
     <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-	<a href="../university-map/">
     <div class="tile">
     <i class="fa fa-map-marker"></i>
 	<p>University Map</p>
     </div>
-	<a>
 	</div>
+    <a>
 
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
 	<a href="../feedback/">
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
     <div class="tile">
     <i class="fa fa-check-square-o"></i>
 	<p>Feedback</p>
     </div>
-    </a>
 	</div>
+    </a>
 
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
 	<a href="../messenger/">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
     <div class="tile">
     <i class="fa fa-comments"></i>
 	<p>Messenger<span class="badge"><?php echo ($messenger_count == '0' ? "" : "$messenger_count"); ?></span></p>
     </div>
-    </a>
 	</div>
+    </a>
 
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
 	<a href="../account/">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
     <div class="tile">
     <i class="fa fa-user"></i>
 	<p>Account</p>
     </div>
-    </a>
 	</div>
+    </a>
 	
 	</div><!-- /row -->
 	
