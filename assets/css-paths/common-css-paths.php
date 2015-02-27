72b10e99a56b206abd64dc21b74c113a46ec0020
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-27 21:04:17 +0000

    Changed structure

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -2,7 +2,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
 
     <!-- open-sans-font -->
-    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
 
     <!-- bootstrap -->
     <link href="https://student-portal.co.uk/assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
