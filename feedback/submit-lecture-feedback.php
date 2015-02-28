eeced9e9d3e4995e60170cf63c4b913e6f3d89e2
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 15:51:00 +0000

    Changed structure

--- feedback/submit-lecture-feedback.php
+++ feedback/submit-lecture-feedback.php
@@ -50,11 +50,11 @@ if (isset($_GET["id"])) {
     <ol class="breadcrumb">
     <li><a href="../../overview/">Overview</a></li>
 	<li><a href="../../feedback/">Feedback</a></li>
-    <li class="active">Lecture feedback</li>
+    <li class="active">Submit lecture feedback</li>
     </ol>
 
 	<!-- Message user -->
-    <form class="form-custom" style="max-width: 100%;" method="post" name="messageuser_form" id="messageuser_form" novalidate>
+    <form class="form-custom" style="max-width: 100%;" method="post" name="submitlecturefeedaback_form" id="submitlecturefeedaback_form" novalidate>
 
     <p id="error" class="feedback-sad text-center"></p>
     <p id="error1" class="feedback-sad text-center"></p>
