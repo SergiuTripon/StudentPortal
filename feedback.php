68aaf7d3bc840219e39b652fc2cd46f54bc79c6f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 22:00:17 +0000

    Changed structure

--- feedback.php
+++ feedback.php
@@ -100,6 +100,86 @@ include 'includes/session.php';
 
     <?php endif; ?>
 
+        <?php if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'lecturer') : ?>
+
+    <?php include 'includes/menus/portal_menu.php'; ?>
+
+	<div id="timetable-portal" class="container">
+
+	<ol class="breadcrumb">
+    <li><a href="../overview/">Overview</a></li>
+	<li class="active">Feedback</li>
+    </ol>
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+	<div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Feedback</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Lectures -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom feedback-table">
+
+	<thead>
+	<tr>
+	<th>From</th>
+	<th>Name</th>
+    <th>Subject</th>
+    <th>Feedback</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT user_details.firstname, user_details.surname, system_modules.module_name, user_feedback.feedback_subject, user_feedback.feedback_body FROM user_feedback_lookup LEFT JOIN user_details ON user_feedback_lookup.feedback_from=user_details.userid LEFT JOIN system_modules ON user_feedback_lookup.moduleid=system_modules.moduleid LEFT JOIN system_lectures ON user_feedback_lookup.moduleid=system_lectures.moduleid LEFT JOIN system_tutorials ON user_feedback_lookup.moduleid=system_tutorials.moduleid LEFT JOIN user_feedback ON user_feedback_lookup.feedbackid=user_feedback.feedbackid WHERE user_feedback_lookup.isApproved = 1 AND (system_lectures.lecture_lecturer='$session_userid' OR system_tutorials.tutorial_assistant='$session_userid')");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+    $firstname = $row["firstname"];
+    $surname = $row["surname"];
+	$module_name = $row["module_name"];
+    $feedback_subject = $row["feedback_subject"];
+    $feedback_body = $row["feedback_body"];
+
+	echo '<tr>
+
+			<td data-title="From">'.$firstname.' '.$surname.'</td>
+			<td data-title="Lecture name">'.$module_name.'</td>
+			<td data-title="Subject">'.$feedback_subject.'</td>
+			<td data-title="Feedback">'.$feedback_body.'</td>
+			</tr>';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	</div><!-- /.panel-group -->
+
+    </div><!-- /container -->
+
+	<?php include 'includes/footers/footer.php'; ?>
+
+	<!-- Sign Out (Inactive) JS -->
+    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+
+    <?php endif; ?>
+
     <?php if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'admin') : ?>
 
     <?php include 'includes/menus/portal_menu.php'; ?>
@@ -158,86 +238,6 @@ include 'includes/session.php';
 			<td data-title="Subject">'.$feedback_subject.'</td>
 			<td data-title="Feedback">'.$feedback_body.'</td>
             <td data-title="Action"><a id="approve-'.$feedbackid.'" class="btn btn-primary btn-md ladda-button approve-button" data-style="slide-up"><span class="ladda-label">Approve</span></a></a></td>
-			</tr>';
-	}
-
-	$stmt1->close();
-	?>
-	</tbody>
-
-	</table>
-	</section>
-
-  	</div><!-- /panel-body -->
-    </div><!-- /panel-collapse -->
-	</div><!-- /panel-default -->
-
-	</div><!-- /.panel-group -->
-
-    </div><!-- /container -->
-
-	<?php include 'includes/footers/footer.php'; ?>
-
-	<!-- Sign Out (Inactive) JS -->
-    <script src="../assets/js/custom/sign-out-inactive.js"></script>
-
-    <?php endif; ?>
-
-    <?php if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'lecturer') : ?>
-
-    <?php include 'includes/menus/portal_menu.php'; ?>
-
-	<div id="timetable-portal" class="container">
-
-	<ol class="breadcrumb">
-    <li><a href="../overview/">Overview</a></li>
-	<li class="active">Feedback</li>
-    </ol>
-
-	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-
-	<div class="panel panel-default">
-
-    <div class="panel-heading" role="tab" id="headingOne">
-  	<h4 class="panel-title">
-	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Feedback</a>
-  	</h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  	<div class="panel-body">
-
-	<!-- Lectures -->
-	<section id="no-more-tables">
-	<table class="table table-condensed table-custom feedback-table">
-
-	<thead>
-	<tr>
-	<th>From</th>
-	<th>Name</th>
-    <th>Subject</th>
-    <th>Feedback</th>
-	</tr>
-	</thead>
-
-	<tbody>
-	<?php
-
-	$stmt1 = $mysqli->query("SELECT user_details.firstname, user_details.surname, system_modules.module_name, user_feedback.feedback_subject, user_feedback.feedback_body FROM user_feedback_lookup LEFT JOIN user_details ON user_feedback_lookup.feedback_from=user_details.userid LEFT JOIN system_modules ON user_feedback_lookup.moduleid=system_modules.moduleid LEFT JOIN system_lectures ON user_feedback_lookup.moduleid=system_lectures.moduleid LEFT JOIN system_tutorials ON user_feedback_lookup.moduleid=system_tutorials.moduleid LEFT JOIN user_feedback ON user_feedback_lookup.feedbackid=user_feedback.feedbackid WHERE user_feedback_lookup.isApproved = 1 AND (system_lectures.lecture_lecturer='$session_userid' OR system_tutorials.tutorial_assistant='$session_userid')");
-
-	while($row = $stmt1->fetch_assoc()) {
-
-    $firstname = $row["firstname"];
-    $surname = $row["surname"];
-	$module_name = $row["module_name"];
-    $feedback_subject = $row["feedback_subject"];
-    $feedback_body = $row["feedback_body"];
-
-	echo '<tr>
-
-			<td data-title="From">'.$firstname.' '.$surname.'</td>
-			<td data-title="Lecture name">'.$module_name.'</td>
-			<td data-title="Subject">'.$feedback_subject.'</td>
-			<td data-title="Feedback">'.$feedback_body.'</td>
 			</tr>';
 	}
 
