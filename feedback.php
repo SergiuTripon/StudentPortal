97d51f6f8bd0144ecfe0d9f7fb46b9ef4cfc61aa
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 17:15:40 +0000

    Changed structure

--- feedback.php
+++ feedback.php
@@ -158,6 +158,142 @@ include 'includes/session.php';
 
     <?php endif; ?>
 
+    <?php if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'admin') : ?>
+
+    <?php include 'includes/menus/portal_menu.php'; ?>
+
+	<div id="timetable-portal" class="container">
+
+	<ol class="breadcrumb">
+    <li><a href="../overview/">Overview</a></li>
+	<li class="active">Feedback</li>
+    </ol>
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+	<div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Lectures</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Lectures -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom lecture-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Lecturer</th>
+	<th>Day</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT system_lectures.lectureid, system_lectures.lecture_name, user_details.firstname, user_details.surname, system_lectures.lecture_day FROM system_lectures LEFT JOIN system_modules ON system_lectures.moduleid=system_modules.moduleid LEFT JOIN user_timetable ON system_lectures.moduleid=user_timetable.moduleid LEFT JOIN user_details ON system_lectures.lecture_lecturer=user_details.userid WHERE user_timetable.userid='$session_userid' AND system_lectures.lecture_status='active'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+    $lectureid = $row["lectureid"];
+	$lecture_name = $row["lecture_name"];
+	$firstname = $row["firstname"];
+    $surname = $row["surname"];
+	$lecture_day = $row["lecture_day"];
+
+	echo '<tr>
+
+			<td data-title="Name">'.$lecture_name.'</td>
+			<td data-title="Lecturer">'.$firstname.' '.$surname.'</td>
+			<td data-title="From">'.$lecture_day.'</td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="../feedback/submit-lecture-feedback?id='.$lectureid.'" data-style="slide-up"><span class="ladda-label">Feedback</span></a></td>
+			</tr>';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	<div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingTwo">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Tutorials</a>
+  	</h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+  	<div class="panel-body">
+
+	<!-- Tutorials -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom tutorial-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Tutorial assistant</th>
+	<th>Day</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt3 = $mysqli->query("SELECT system_tutorials.tutorial_name, user_details.firstname, user_details.surname, system_tutorials.tutorial_day FROM system_tutorials LEFT JOIN system_modules ON system_tutorials.moduleid=system_modules.moduleid LEFT JOIN user_timetable ON system_tutorials.moduleid=user_timetable.moduleid LEFT JOIN user_details ON system_tutorials.tutorial_assistant=user_details.userid WHERE user_timetable.userid='$session_userid' AND system_tutorials.tutorial_status='active'");
+
+	while($row = $stmt3->fetch_assoc()) {
+
+    $tutorialid = $row["tutorialid"];
+	$tutorial_name = $row["tutorial_name"];
+	$firstname = $row["firstname"];
+    $surname = $row["surname"];
+	$tutorial_day = $row["tutorial_day"];
+
+	echo '<tr>
+
+			<td data-title="Name">'.$tutorial_name.'</td>
+			<td data-title="Notes">'.$firstname.' '.$surname.'</td>
+			<td data-title="Notes">'.$tutorial_day.'</td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="../feedback/submit-tutorial-feedback?id='.$tutorialid.'" data-style="slide-up"><span class="ladda-label">Feedback</span></a></td>
+			</tr>';
+	}
+
+	$stmt3->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	</div><!-- /.panel-group -->
+
+    </div><!-- /container -->
+
+	<?php include 'includes/footers/footer.php'; ?>
+
+	<!-- Sign Out (Inactive) JS -->
+    <script src="../assets/js/custom/sign-out-inactive.js"></script>
+
+    <?php endif; ?>
+
     <?php else : ?>
 
 	<?php include 'includes/menus/menu.php'; ?>
