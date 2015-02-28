97e216088e384515b4afe20e79bc6d899c87f3db
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 23:02:06 +0000

    Changed structure

--- feedback/submit-feedback.php
+++ feedback/submit-feedback.php
@@ -19,11 +19,11 @@ if (isset($_GET["id"])) {
     $stmt2->bind_result($lecturer_feedback_to_email, $lecturer_feedback_to_firstname, $lecturer_feedback_to_surname, $lecture_lecturer);
     $stmt2->fetch();
 
-    $stmt3 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname, user_details.surname, tutorial_assistant FROM system_tutorials LEFT JOIN user_signin ON system_tutorials.tutorial_assistant=user_signin.userid LEFT JOIN user_details ON system_tutorials.tutorial_assistant=user_details.userid WHERE system_tutorials.moduleid=?");
+    $stmt3 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname, user_details.surname FROM system_tutorials LEFT JOIN user_signin ON system_tutorials.tutorial_assistant=user_signin.userid LEFT JOIN user_details ON system_tutorials.tutorial_assistant=user_details.userid WHERE system_tutorials.moduleid=?");
     $stmt3->bind_param('i', $moduleToFeedback);
     $stmt3->execute();
     $stmt3->store_result();
-    $stmt3->bind_result($tutorial_assistant_feedback_to_email, $tutorial_assistant_feedback_to_firstname, $tutorial_assistant_feedback_to_surname, $tutorial_assistant);
+    $stmt3->bind_result($tutorial_assistant_feedback_to_email, $tutorial_assistant_feedback_to_firstname, $tutorial_assistant_feedback_to_surname);
     $stmt3->fetch();
 
     $stmt4 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
