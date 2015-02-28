b3509ba724d70a78cd5bd509e9596e47ccd2174b
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 13:13:22 +0000

    Changed structure

--- library.php
+++ library.php
@@ -148,7 +148,7 @@ include 'includes/session.php';
 	<tbody>
 	<?php
 
-	$stmt2 = $mysqli->query("SELECT reserved_books.bookid, DATE_FORMAT(reserved_books.reserved_on,'%d %b %y') as reserved_on, DATE_FORMAT(reserved_books.toreturn_on,'%d %b %y') as toreturn_on, DATE_FORMAT(reserved_books.returned_on,'%d %b %y') as toreturn_on, reserved_books.isReturned, system_books.book_name, system_books.book_author, system_books.book_notes, system_books.book_status FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid  WHERE reserved_books.userid = '$session_userid'");
+	$stmt2 = $mysqli->query("SELECT reserved_books.bookid, DATE_FORMAT(reserved_books.reserved_on,'%d %b %y') as reserved_on, DATE_FORMAT(reserved_books.toreturn_on,'%d %b %y') as toreturn_on, DATE_FORMAT(reserved_books.returned_on,'%d %b %y') as returned_on, reserved_books.isReturned, system_books.book_name, system_books.book_author, system_books.book_notes, system_books.book_status FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid  WHERE reserved_books.userid = '$session_userid'");
 
 	while($row = $stmt2->fetch_assoc()) {
 
