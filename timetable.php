eb0fdc57e1c5e8cd15136b3d8b7c211ec425ff52
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 14:46:17 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -127,25 +127,18 @@ include 'includes/session.php';
 
 	<tbody>
 	<?php
-
-	$stmt3 = $mysqli->query("SELECT system_tutorials.tutorial_name, system_tutorials.tutorial_assistant, system_tutorials.tutorial_day, DATE_FORMAT(system_tutorials.tutorial_from_time,'%H:%i') as tutorial_from_time, DATE_FORMAT(system_tutorials.tutorial_to_time,'%H:%i') as tutorial_to_time, system_tutorials.tutorial_location, system_tutorials.tutorial_capacity FROM user_timetable LEFT JOIN system_modules ON user_timetable.moduleid=system_modules.moduleid LEFT JOIN system_tutorials ON user_timetable.moduleid=system_tutorials.moduleid WHERE user_timetable.userid='$session_userid' AND system_tutorials.tutorial_status ='active'");
+	$stmt3 = $mysqli->query("SELECT system_tutorials.tutorial_name, user_details.firstname, user_details.surname, system_tutorials.tutorial_day, DATE_FORMAT(system_tutorials.tutorial_from_time,'%H:%i') as tutorial_from_time, DATE_FORMAT(system_tutorials.tutorial_to_time,'%H:%i') as tutorial_to_time, system_tutorials.tutorial_location, system_tutorials.tutorial_capacity FROM system_tutorials LEFT JOIN system_modules ON system_tutorials.moduleid=system_modules.moduleid LEFT JOIN user_timetable ON system_tutorials.moduleid=user_timetable.moduleid LEFT JOIN user_details ON system_tutorials.tutorial_assistant=user_details.userid WHERE user_timetable.userid='$session_userid' AND system_tutorials.tutorial_status='active'");
 
 	while($row = $stmt3->fetch_assoc()) {
 
 	$tutorial_name = $row["tutorial_name"];
-	$tutorial_assistant = $row["tutorial_assistant"];
+	$firstname = $row["firstname"];
+    $surname = $row["surname"];
 	$tutorial_day = $row["tutorial_day"];
 	$tutorial_from_time = $row["tutorial_from_time"];
 	$tutorial_to_time = $row["tutorial_to_time"];
 	$tutorial_location = $row["tutorial_location"];
 	$tutorial_capacity = $row["tutorial_capacity"];
-
-	$stmt4 = $mysqli->prepare("SELECT firstname, surname FROM user_details WHERE userid = ?");
-	$stmt4->bind_param('i', $tutorial_assistant);
-	$stmt4->execute();
-	$stmt4->store_result();
-	$stmt4->bind_result($firstname, $surname);
-	$stmt4->fetch();
 
 	echo '<tr>
 
