30bf1d0f36e645874dd073eec3e50877ee8c42e4
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 15:07:07 +0000

    Changed structure

--- feedback/submit-feedback.php
+++ feedback/submit-feedback.php
@@ -3,17 +3,10 @@ include '../includes/session.php';
 
 if (isset($_GET["id"])) {
 
-    $idToMessage = $_GET["id"];
-
-    $stmt1 = $mysqli->prepare("SELECT user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
-    $stmt1->bind_param('i', $session_userid);
-    $stmt1->execute();
-    $stmt1->store_result();
-    $stmt1->bind_result($message_from_email, $message_from_firstname, $message_from_surname);
-    $stmt1->fetch();
+    $moduleFeedback = $_GET["id"];
 
     $stmt1 = $mysqli->prepare("SELECT user_signin.userid, user_signin.email, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid = ? LIMIT 1");
-    $stmt1->bind_param('i', $idToMessage);
+    $stmt1->bind_param('i', $moduleFeedback);
     $stmt1->execute();
     $stmt1->store_result();
     $stmt1->bind_result($message_to_userid, $message_to_email, $message_to_firstname, $message_to_surname);
