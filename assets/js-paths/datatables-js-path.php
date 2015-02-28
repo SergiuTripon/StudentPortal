c95a046090ab33fe014efccdc81ac96ac0d122f5
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 00:01:31 +0000

    Changed structure

--- assets/js-paths/datatables-js-path.php
+++ assets/js-paths/datatables-js-path.php
@@ -1,2 +1,3 @@
     <!-- dataTables JS -->
     <script src="https://student-portal.co.uk/assets/js/data-tables/jquery.dataTables.min.js"></script>
+    <script src="https://student-portal.co.uk/assets/js/data-tables/dataTables.bootstrap.js"></script>
