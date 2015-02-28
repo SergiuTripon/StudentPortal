5557e43c18f310ef7376c225ffd0b5d2113bee5d
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 17:43:36 +0000

    Changed structure

--- assets/css-paths/datatables-css-path.php
+++ assets/css-paths/datatables-css-path.php
@@ -1,3 +1,2 @@
     <!-- data-tables -->
     <link href="https://student-portal.co.uk/assets/css/data-tables/dataTables.bootstrap.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/data-tables/dataTables.fontAwesome.css" rel="stylesheet">
