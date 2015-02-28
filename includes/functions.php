b7b476784d9beca87a1bf3df97d4cf4d921f0c33
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-02-28 12:59:28 +0000

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -410,7 +410,7 @@ function GetDashboardData() {
 	$stmt3->fetch();
 
     $book_reserved = 'reserved';
-    $book_cancelled = 'reserved';
+    $book_cancelled = 'cancelled';
 
 	$stmt4 = $mysqli->prepare("	SELECT reserved_books.bookid FROM reserved_books LEFT JOIN system_books ON reserved_books.bookid=system_books.bookid  WHERE reserved_books.userid = ? AND system_books.book_status = ? AND isReturned = '0' AND NOT system_books.book_status=?");
 	$stmt4->bind_param('sis', $book_reserved, $session_userid, $book_cancelled);
