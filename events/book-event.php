c1fa38ec397ee5e34798bd9f49e7bb65fb9ba642
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-01 16:59:03 +0000

    Changed structure

--- events/book-event.php
+++ events/book-event.php
@@ -136,7 +136,7 @@ if (isset($_GET["id"])) {
 
     <div class="form-group">
     <div class="col-xs-12 col-sm-12 full-width pr0 pl0">
-    <label>Total to pay</label>
+    <label>Total to pay (&pound;)</label>
     <input class="form-control" type="text" name="product_amount" id="product_amount" placeholder="Total to pay" readonly="readonly">
 	</div>
     </div>
