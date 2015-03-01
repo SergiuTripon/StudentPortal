b82077d2923ca588b78bc4e34fc26fe24d7bc408
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-01 17:36:05 +0000

    Changed structure

--- events.php
+++ events.php
@@ -152,7 +152,7 @@ include 'includes/session.php';
 	<tbody>
 	<?php
 
-	$stmt2 = $mysqli->query("SELECT event_name, event_amount, tickets_quantity, DATE_FORMAT(booked_on,'%d %b %y %H:%i') as booked_on FROM booked_events WHERE userid = '$session_userid'");
+	$stmt2 = $mysqli->query("SELECT event_amount, ticket_quantity, DATE_FORMAT(booked_on,'%d %b %y %H:%i') as booked_on FROM booked_events LEFT JOIN system_events ON booked_events.eventid=system_events.eventid WHERE userid = '$session_userid'");
 
 	while($row = $stmt2->fetch_assoc()) {
 
