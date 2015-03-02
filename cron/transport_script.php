2def1996943b313f629ef0584c7b81bb5f648260
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-02 15:39:47 +0000

    Changed structure

--- cron/transport_script.php
+++ cron/transport_script.php
@@ -1,8 +1,8 @@
 <?php
 include '../includes/session.php';
 
-UpdateTransportStatus();
-//InsertTransportStatus();
+//UpdateTransportStatus();
+InsertTransportStatus();
 
 //GetTransportStatus function
 function UpdateTransportStatus () {
