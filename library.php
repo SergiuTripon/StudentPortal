56776825a1157edc3de50b7bfaa0566eacf77f25
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-02 23:01:50 +0000

    Changed structure

--- library.php
+++ library.php
@@ -1012,7 +1012,7 @@ include 'includes/session.php';
 			</div><!-- /modal-dialog -->
 			</div><!-- /modal-content -->
 
-            <div id="approve-'.$bookid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+            <div id="approve-'.$requestid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
     		<div class="modal-dialog">
     		<div class="modal-content">
 
