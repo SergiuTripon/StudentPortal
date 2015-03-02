c27e5820051bd5c7ead4000532ad9eb4785861a3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-02 21:56:07 +0000

    Changed structure

--- library.php
+++ library.php
@@ -436,7 +436,7 @@ include 'includes/session.php';
     		<div class="modal-content">
 
 			<div class="modal-header">
-            <div class="close"><i class="fa fa-clock-o"></i></div>
+            <div class="close"><i class="fa fa-book"></i></div>
             <h4 class="modal-title" id="modal-custom-label">'.$book_name.'</h4>
 			</div>
 
