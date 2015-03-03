718196bebcba58001ced059ff474a56bc6dbf275
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 15:06:58 +0000

    Changed structure

--- overview.php
+++ overview.php
@@ -62,6 +62,15 @@ include 'includes/functions.php';
 	</div>
     </a>
 
+    <a href="../results/">
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+    <div class="tile">
+	<i class="fa fa-star-o"></i>
+	<p>Results<span class="badge"><?php echo ($exams_count == '0' ? "" : "$exams_count"); ?></span></p>
+    </div>
+	</div>
+    </a>
+
     <a href="../library/">
     <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
 	<div class="tile">
