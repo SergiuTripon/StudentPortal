e32cfe73b2b17db20c442be326e1878f9517bf93
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 15:45:23 +0000

    Changed structure

--- results.php
+++ results.php
@@ -203,35 +203,28 @@ include 'includes/session.php';
 
 	<thead>
 	<tr>
-	<th>Name</th>
-	<th>Notes</th>
-	<th>URL</th>
-	<th>Action</th>
-    <th>Action</th>
-    <th>Action</th>
+	<th>Full name</th>
+	<th>Student number</th>
 	</tr>
 	</thead>
 
-	<tbody id="loadModules-table">
+	<tbody>
 	<?php
 
-	$stmt3 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'active'");
+	$stmt3 = $mysqli->query("SELECT user_signin.userid, user_details.firstname, user_details.surname, user_details.studentno FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE account_type = 'student'");
 
 	while($row = $stmt3->fetch_assoc()) {
 
-    $moduleid = $row["moduleid"];
-	$module_name = $row["module_name"];
-	$module_notes = $row["module_notes"];
-	$module_url = $row["module_url"];
+    $userid = $row["userid"];
+	$firstname = $row["firstname"];
+	$surname = $row["surname"];
+	$studentno = $row["studentno"];
 
-	echo '<tr id="cancel-'.$moduleid.'">
+	echo '<tr id="assign-'.$userid.'">
 
-			<td data-title="Name">'.$module_name.'</td>
-			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
-            <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button assign-button" href="/admin/assign-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Assign</span></a></a></td>
-			<td data-title="Action"><a class="btn btn-primary btn-md ladda-button update-button" href="/admin/update-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></a></td>
-            <td data-title="Action"><a id="cancel-'.$moduleid.'" class="btn btn-primary btn-md ladda-button cancel-button" data-style="slide-up"><span class="ladda-label">Cancel</span></a></a></td>
+			<td data-title="Name">'.$firstname.' '.$surname.'</td>
+            <td data-title="Student number">'.$studentno.'</td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button assign-button" href="/admin/assign-results?id='.$userid.'" data-style="slide-up"><span class="ladda-label">Select</span></a></a></td>
 			</tr>';
 	}
 
@@ -245,60 +238,6 @@ include 'includes/session.php';
   	</div><!-- /panel-body -->
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
-
-    <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
-  	<h4 class="panel-title">
-	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">  Cancelled timetables</a>
-    </h4>
-    </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-  	<div class="panel-body">
-
-	<!-- Cancelled modules -->
-	<section id="no-more-tables">
-	<table class="table table-condensed table-custom module-table">
-
-	<thead>
-	<tr>
-	<th>Name</th>
-	<th>Notes</th>
-	<th>URL</th>
-	<th>Action</th>
-	</tr>
-	</thead>
-
-	<tbody id="loadCancelledModules-table">
-	<?php
-
-	$stmt3 = $mysqli->query("SELECT moduleid, module_name, module_notes, module_url FROM system_modules WHERE module_status = 'cancelled'");
-
-	while($row = $stmt3->fetch_assoc()) {
-
-    $moduleid = $row["moduleid"];
-	$module_name = $row["module_name"];
-	$module_notes = $row["module_notes"];
-	$module_url = $row["module_url"];
-
-	echo '<tr id="activate-'.$moduleid.'">
-
-			<td data-title="Name">'.$module_name.'</td>
-			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
-            <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-            <td data-title="Action"><a id="activate-'.$moduleid.'" class="btn btn-primary btn-md ladda-button activate-button" data-style="slide-up"><span class="ladda-label">Activate</span></a></td>
-			</tr>';
-	}
-
-	$stmt3->close();
-	?>
-	</tbody>
-
-	</table>
-	</section>
-
-  	</div><!-- /panel-body -->
-    </div><!-- /panel-collapse -->
-  	</div><!-- /panel-default -->
 
 	</div><!-- /.panel-group -->
 
