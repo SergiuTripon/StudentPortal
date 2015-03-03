17ff83eb30399f7e4e5dd2972b034f5ccf9da8f8
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 21:51:40 +0000

    Changed structure

--- university-map.php
+++ university-map.php
@@ -214,8 +214,8 @@ include 'includes/session.php';
 
 	<?php endif; ?>
 
-    <?php include 'assets/js-paths/common-js-paths.php'; ?>
     <?php include 'assets/js-paths/tilejs-js-path.php'; ?>
+    <?php include 'assets/js-paths/common-js-paths.php'; ?>
     <?php include 'assets/js-paths/datatables-js-path.php'; ?>
 
     <script>
