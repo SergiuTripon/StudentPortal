5d9b85de9be9cb1bffa3ba5d9d593d82f11c8181
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 15:53:15 +0000

    Changed structure

---
+++ admin/assign-results.php
@@ -0,0 +1,266 @@
+<?php
+include '../includes/session.php';
+
+if (isset($_GET['id'])) {
+
+    $moduleToAssignResults = $_GET['id'];
+
+} else {
+    header('Location: ../../timetable/');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<?php include '../assets/meta-tags.php'; ?>
+
+	<?php include '../assets/css-paths/datatables-css-path.php'; ?>
+	<?php include '../assets/css-paths/common-css-paths.php'; ?>
+
+    <title>Student Portal | Assign timetable</title>
+
+</head>
+
+<body>
+<div class="preloader"></div>
+
+	<?php if (isset($_SESSION['signedIn']) && $_SESSION['signedIn'] == true) : ?>
+
+	<?php include '../includes/menus/portal_menu.php'; ?>
+
+	<div class="container">
+
+	<ol class="breadcrumb">
+		<li><a href="../../overview/">Overview</a></li>
+        <li><a href="../../timetable/">Timetable</a></li>
+		<li class="active">Assign timetable</li>
+	</ol>
+
+    <div id="moduleid" style="display: none !important;"><?php echo $timetableToAssign; ?></div>
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+	<div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Unallocated students</a>
+    </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Allocated students -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom">
+
+	<thead>
+	<tr>
+	<th>Full name</th>
+	<th>Student number</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+    <?php
+
+	$stmt1 = $mysqli->query("SELECT user_signin.userid, user_details.studentno, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid NOT IN (SELECT DISTINCT(user_timetable.userid) FROM user_timetable WHERE user_timetable.moduleid = '$timetableToAssign') AND user_signin.account_type = 'student'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$userid = $row["userid"];
+    $studentno = $row["studentno"];
+    $firstname = $row["firstname"];
+    $surname = $row["surname"];
+
+	echo '<tr id="allocate-'.$userid.'">
+
+			<td data-title="Full name">'.$firstname.' '.$surname.'</td>
+			<td data-title="Student number">'.$studentno.'</td>
+			<td data-title="Action"><a id="allocate-'.$userid.'" class="btn btn-primary btn-md ladda-button allocate-button" data-style="slide-up"><span class="ladda-label">Allocate</span></a></td>
+			</tr>';
+    }
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+    <div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingTwo">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Allocated students</a>
+    </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
+  	<div class="panel-body">
+
+	<!-- Unallocated students -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom">
+
+	<thead>
+	<tr>
+	<th>Full name</th>
+	<th>Student number</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+    <?php
+
+	$stmt2 = $mysqli->query("SELECT user_signin.userid, user_details.studentno, user_details.firstname, user_details.surname FROM user_signin LEFT JOIN user_details ON user_signin.userid=user_details.userid WHERE user_signin.userid IN (SELECT DISTINCT(user_timetable.userid) FROM user_timetable WHERE user_timetable.moduleid = '$timetableToAssign') AND user_signin.account_type = 'student'");
+
+	while($row = $stmt2->fetch_assoc()) {
+
+	$userid = $row["userid"];
+    $studentno = $row["studentno"];
+    $firstname = $row["firstname"];
+    $surname = $row["surname"];
+
+	echo '<tr id="unallocate-'.$userid.'">
+
+			<td data-title="First name">'.$firstname.' '.$surname.'</td>
+			<td data-title="Student number">'.$studentno.'</td>
+			<td data-title="Action"><a id="deallocate-'.$userid.'" class="btn btn-primary btn-md ladda-button deallocate-button" data-style="slide-up"><span class="ladda-label">Deallocate</span></a></td>
+			</tr>';
+    }
+	$stmt2->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+	</div><!-- /panel-group -->
+
+    </div><!-- /container -->
+
+	<?php include '../includes/footers/footer.php'; ?>
+
+	<!-- Sign Out (Inactive) JS -->
+    <script src="../../assets/js/custom/sign-out-inactive.js"></script>
+
+	<?php else : ?>
+
+	<?php include '../includes/menus/menu.php'; ?>
+
+    <div class="container">
+
+	<form class="form-custom">
+
+    <div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+
+    <p class="feedback-sad text-center">Looks like you're not signed in yet. Please sign in before accessing this area.</p>
+
+    <hr>
+
+    <div class="text-center">
+	<a class="btn btn-primary btn-lg ladda-button" data-style="slide-up" href="/"><span class="ladda-label">Sign In</span></a>
+    </div>
+
+    </form>
+
+	</div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+
+	<?php endif; ?>
+
+	<?php include '../assets/js-paths/common-js-paths.php'; ?>
+	<?php include '../assets/js-paths/tilejs-js-path.php'; ?>
+	<?php include '../assets/js-paths/datatables-js-path.php'; ?>
+
+	<script>
+    $(document).ready(function () {
+
+    //Ladda
+    Ladda.bind('.ladda-button', {timeout: 2000});
+
+    //DataTables
+    $('.table-custom').dataTable({
+        "iDisplayLength": 10,
+		"paging": true,
+		"ordering": true,
+		"info": false,
+		"language": {
+			"emptyTable": "There are no users to display."
+		}
+	});
+
+    //Assign timetable
+	$("body").on("click", ".allocate-button", function(e) {
+    e.preventDefault();
+
+    var clickedID = this.id.split('-');
+    var userToAllocate = clickedID[1];
+    var timetableToAllocate = $("#moduleid").html();
+
+	jQuery.ajax({
+	type: "POST",
+	url: "https://student-portal.co.uk/includes/processes.php",
+	dataType:"text",
+	data:'userToAllocate='+ userToAllocate + '&timetableToAllocate='+ timetableToAllocate,
+	success:function(){
+        $('#allocate-'+userToAllocate).hide();
+        location.reload();
+    },
+	error:function (xhr, ajaxOptions, thrownError){
+		$("#error").show();
+		$("#error").empty().append(thrownError);
+	}
+
+	});
+
+    });
+
+    //Unassign timetable
+    $("body").on("click", ".deallocate-button", function(e) {
+    e.preventDefault();
+
+    var clickedID = this.id.split('-');
+    var userToDeallocate = clickedID[1];
+    var timetableToDeallocate = $("#moduleid").html();
+
+	jQuery.ajax({
+	type: "POST",
+	url: "https://student-portal.co.uk/includes/processes.php",
+	dataType:"text",
+	data:'userToDeallocate='+ userToDeallocate + '&timetableToDeallocate='+ timetableToDeallocate,
+	success:function(){
+        $('#deallocate-'+userToDeallocate).hide();
+        location.reload();
+    },
+	error:function (xhr, ajaxOptions, thrownError){
+		$("#error").show();
+		$("#error").empty().append(thrownError);
+	}
+
+	});
+
+    });
+
+	});
+	</script>
+
+</body>
+</html>
