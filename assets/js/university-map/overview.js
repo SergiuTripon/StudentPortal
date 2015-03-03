8ec7031b065ea0bb25d2075857af111a341835ab
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 13:10:47 +0000

    Changed structure

--- assets/js/university-map/overview.js
+++ assets/js/university-map/overview.js
@@ -18,14 +18,15 @@
     };
 
     function loadMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(51.527287, -0.103842),
-        zoom: 15,
-        mapTypeId: 'roadmap'
-        });
-        var infoWindow = new google.maps.InfoWindow({
-            maxWidth: 400
-        });
+        var mapOptions = {
+            center: new google.maps.LatLng(51.527287, -0.103842),
+            zoom: 15,
+            mapTypeId: 'roadmap',
+            styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+        };
+        var mapElement = document.getElementById('map');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var infoWindow = new google.maps.InfoWindow({ maxWidth: 400 });
 
     // Change this depending on the name of your PHP file
     downloadUrl("../../includes/university-map/source/overview_source.php", function(data) {
