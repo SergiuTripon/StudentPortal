8f3ab1b6e638caaf0fbad166c33d9551c31ae6e9
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-03 01:37:17 +0000

    Changed structure

--- cron/transport_script.php
+++ cron/transport_script.php
@@ -253,7 +253,7 @@ function GetTransportStatus () {
     $cron_job = 'transport_script.txt';
 
     $cron_log = fopen("cron_log.txt", "w") or die("Unable to open file!");
-    $cron_log_content = "$cron_job ran successfully at $updated_on.\n";
+    $cron_log_content = "$cron_job ran successfully at $updated_on.'\n'";
     fwrite($cron_log, $cron_log_content);
     fclose($cron_log);
 
