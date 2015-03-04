376909fb4eae3fc96e1ad816b2aa2448f760a961
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-04 21:29:55 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -283,12 +283,12 @@ include 'includes/session.php';
 	</div><!-- /panel-default -->
 
     <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
+    <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
-	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Inactive timetables</a>
+	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Inactive timetables</a>
     </h4>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
   	<div class="panel-body">
 
 	<!-- Inactive timetables -->
