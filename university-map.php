ce0992f57f249fee84ab6c50213fcedad3fe6b47
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-04 22:47:55 +0000

    Changed structure

--- university-map.php
+++ university-map.php
@@ -177,6 +177,137 @@ include 'includes/session.php';
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
 
+    <div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingOne">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Inactive locations</a>
+  	</h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+  	<div class="panel-body">
+
+	<!-- Inactive locations -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom location-table">
+
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Latitude</th>
+	<th>Longitude</th>
+	<th>Category</th>
+	<th>Created on</th>
+    <th>Updated on</th>
+    <th>Action</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT markerid, marker_title, marker_lat, marker_long, marker_category, DATE_FORMAT(created_on,'%d %b %y %H:%i') as created_on, DATE_FORMAT(updated_on,'%d %b %y %H:%i') as updated_on FROM system_map_markers WHERE marker_status='inactive'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$markerid = $row["markerid"];
+    $marker_title = $row["marker_title"];
+    $marker_lat = $row["marker_lat"];
+    $marker_long = $row["marker_long"];
+    $marker_category = ucfirst($row["marker_category"]);
+    $created_on = $row["created_on"];
+    $updated_on = $row["updated_on"];
+
+
+	echo '<tr id="location-'.$markerid.'">
+
+			<td data-title="Name">'.$marker_title.'</td>
+			<td data-title="Latitude">'.$marker_lat.'</td>
+			<td data-title="Longitude">'.$marker_long.'</td>
+			<td data-title="Category">'.$marker_category.'</td>
+			<td data-title="Created on">'.$created_on.'</td>
+			<td data-title="Updated on">'.$updated_on.'</td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="#reactivate-'.$markerid.'" data-toggle="modal" data-style="slide-up"><span class="ladda-label">Reactivate</span></a></td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="#delete-'.$markerid.'" data-toggle="modal" data-style="slide-up"><span class="ladda-label">Delete</span></a></td>
+			</tr>
+
+			<div class="modal modal-custom fade" id="reactivate-'.$markerid.'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+    		<div class="modal-dialog">
+    		<div class="modal-content">
+
+			<div class="modal-header">
+			<div class="form-logo text-center">
+			<i class="fa fa-trash"></i>
+			</div>
+			</div>
+
+			<div class="modal-body">
+			<p class="text-center feedback-sad">Are you sure you want to reactivate '.$marker_title.'?</p>
+			</div>
+
+			<div class="modal-footer">
+			<div id="hide-reactivate">
+			<div class="pull-left">
+			<a id="reactivate-'.$markerid.'" class="btn btn-danger btn-lg reactivate-button ladda-button" data-style="slide-up">Yes</a>
+			</div>
+			<div class="text-right">
+			<button type="button" class="btn btn-success btn-lg ladda-button" data-style="slide-up" data-dismiss="modal">No</button>
+			</div>
+			</div>
+			<div class="text-center">
+			<a id="success-button-reactivate" class="btn btn-primary btn-lg ladda-button" style="display: none;" data-style="slide-up">Continue</a>
+			</div>
+			</div>
+
+			</div><!-- /modal -->
+			</div><!-- /modal-dialog -->
+			</div><!-- /modal-content -->
+
+			<div class="modal modal-custom fade" id="delete-'.$markerid.'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+    		<div class="modal-dialog">
+    		<div class="modal-content">
+
+			<div class="modal-header">
+			<div class="form-logo text-center">
+			<i class="fa fa-trash"></i>
+			</div>
+			</div>
+
+			<div class="modal-body">
+			<p class="text-center feedback-sad">Are you sure you want to delete '.$marker_title.'?</p>
+			</div>
+
+			<div class="modal-footer">
+			<div id="hide-delete">
+			<div class="pull-left">
+			<a id="delete-'.$markerid.'" class="btn btn-danger btn-lg delete-button ladda-button" data-style="slide-up">Yes</a>
+			</div>
+			<div class="text-right">
+			<button type="button" class="btn btn-success btn-lg ladda-button" data-style="slide-up" data-dismiss="modal">No</button>
+			</div>
+			</div>
+			<div class="text-center">
+			<a id="success-button-delete" class="btn btn-primary btn-lg ladda-button" style="display: none;" data-style="slide-up">Continue</a>
+			</div>
+			</div>
+
+			</div><!-- /modal -->
+			</div><!-- /modal-dialog -->
+			</div><!-- /modal-content -->';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
 	</div><!-- /panel-group -->
 
     </div><!-- /container -->
@@ -233,7 +364,7 @@ include 'includes/session.php';
         }
     });
 
-    //Cancel event process
+    //Deactivate location
     $("body").on("click", ".deactivate-button", function(e) {
     e.preventDefault();
 
@@ -255,6 +386,70 @@ include 'includes/session.php';
         $('#hide-deactivate').hide();
         $('#success-button-deactivate').show();
         $("#success-button-deactivate").click(function () {
+            location.reload();
+        });
+	},
+	error:function (xhr, ajaxOptions, thrownError){
+		$("#error").show();
+		$("#error").empty().append(thrownError);
+	}
+	});
+    });
+
+    //Reactivate location
+    $("body").on("click", ".reactivate-button", function(e) {
+    e.preventDefault();
+
+    var clickedID = this.id.split('-');
+    var locationToReactivate = clickedID[1];
+
+	jQuery.ajax({
+	type: "POST",
+	url: "https://student-portal.co.uk/includes/processes.php",
+	dataType:"text",
+	data:'locationToReactivate='+ locationToReactivate,
+	success:function(){
+		$('#location-'+locationToReactivate).fadeOut();
+        $('.form-logo i').removeClass('fa-trash');
+        $('.form-logo i').addClass('fa-check-square-o');
+        $('.modal-body p').removeClass('feedback-sad');
+        $('.modal-body p').addClass('feedback-happy');
+        $('.modal-body p').empty().append('The location has been reactivated successfully.');
+        $('#hide-reactivate').hide();
+        $('#success-button-reactivate').show();
+        $("#success-button-reactivate").click(function () {
+            location.reload();
+        });
+	},
+	error:function (xhr, ajaxOptions, thrownError){
+		$("#error").show();
+		$("#error").empty().append(thrownError);
+	}
+	});
+    });
+
+    //Delete location
+    $("body").on("click", ".delete-button", function(e) {
+    e.preventDefault();
+
+    var clickedID = this.id.split('-');
+    var locationToDelete = clickedID[1];
+
+	jQuery.ajax({
+	type: "POST",
+	url: "https://student-portal.co.uk/includes/processes.php",
+	dataType:"text",
+	data:'locationToDelete='+ locationToDelete,
+	success:function(){
+		$('#location-'+locationToDelete).fadeOut();
+        $('.form-logo i').removeClass('fa-trash');
+        $('.form-logo i').addClass('fa-check-square-o');
+        $('.modal-body p').removeClass('feedback-sad');
+        $('.modal-body p').addClass('feedback-happy');
+        $('.modal-body p').empty().append('The location has been deleted successfully.');
+        $('#hide-delete').hide();
+        $('#success-button-delete').show();
+        $("#success-button-delete").click(function () {
             location.reload();
         });
 	},
