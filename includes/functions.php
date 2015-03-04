ba872053b3600f9dea301db88945b9c4caf9d3a6
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-04 21:09:08 +0000

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -906,7 +906,7 @@ function ReactivateTimetable() {
 
     $feedback_status = 'active';
 
-    $stmt1 = $mysqli->prepare("UPDATE user_feedback SET feedback_status=?, updated_on=? WHERE moduleid=?");
+    $stmt1 = $mysqli->prepare("UPDATE user_feedback_lookup SET feedback_status=?, updated_on=? WHERE moduleid=?");
     $stmt1->bind_param('ssi', $feedback_status, $updated_on, $timetableToReactivate);
     $stmt1->execute();
     $stmt1->close();
