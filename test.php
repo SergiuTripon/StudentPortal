1c8bd51dbcff16dc4fee921714bd3a461fcad9ce
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 23:09:38 +0000

    Changed structure

--- test.php
+++ test.php
@@ -10,185 +10,33 @@
     <?php include 'assets/css-paths/common-css-paths.php'; ?>
 
     <style>
-        .timeline {
+        #hexagon {
+            width: 100px;
+            height: 55px;
+            background: red;
             position: relative;
-            padding: 20px 0 20px;
-            list-style: none;
         }
-
-        .timeline:before {
-            content: " ";
+        #hexagon:before {
+            content: "";
             position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 50%;
-            width: 3px;
-            margin-left: -1.5px;
-            background-color: #eeeeee;
+            top: -25px;
+            left: 0;
+            width: 0;
+            height: 0;
+            border-left: 50px solid transparent;
+            border-right: 50px solid transparent;
+            border-bottom: 25px solid red;
         }
-
-        .timeline > li {
-            position: relative;
-            margin-bottom: 20px;
-        }
-
-        .timeline > li:before,
-        .timeline > li:after {
-            content: " ";
-            display: table;
-        }
-
-        .timeline > li:after {
-            clear: both;
-        }
-
-        .timeline > li:before,
-        .timeline > li:after {
-            content: " ";
-            display: table;
-        }
-
-        .timeline > li:after {
-            clear: both;
-        }
-
-        .timeline > li > .timeline-panel {
-            float: left;
-            position: relative;
-            width: 46%;
-            padding: 20px;
-            border: 1px solid #d4d4d4;
-            border-radius: 2px;
-            -webkit-box-shadow: 0 1px 6px rgba(0,0,0,0.175);
-            box-shadow: 0 1px 6px rgba(0,0,0,0.175);
-        }
-
-        .timeline > li > .timeline-panel:before {
-            content: " ";
-            display: inline-block;
+        #hexagon:after {
+            content: "";
             position: absolute;
-            top: 26px;
-            right: -15px;
-            border-top: 15px solid transparent;
-            border-right: 0 solid #ccc;
-            border-bottom: 15px solid transparent;
-            border-left: 15px solid #ccc;
-        }
-
-        .timeline > li > .timeline-panel:after {
-            content: " ";
-            display: inline-block;
-            position: absolute;
-            top: 27px;
-            right: -14px;
-            border-top: 14px solid transparent;
-            border-right: 0 solid #fff;
-            border-bottom: 14px solid transparent;
-            border-left: 14px solid #fff;
-        }
-
-        .timeline > li > .timeline-badge {
-            z-index: 100;
-            position: absolute;
-            top: 16px;
-            left: 50%;
-            width: 50px;
-            height: 50px;
-            margin-left: -25px;
-            border-radius: 50% 50% 50% 50%;
-            text-align: center;
-            font-size: 1.4em;
-            line-height: 50px;
-            color: #fff;
-            background-color: #999999;
-        }
-
-        .timeline > li.timeline-inverted > .timeline-panel {
-            float: right;
-        }
-
-        .timeline > li.timeline-inverted > .timeline-panel:before {
-            right: auto;
-            left: -15px;
-            border-right-width: 15px;
-            border-left-width: 0;
-        }
-
-        .timeline > li.timeline-inverted > .timeline-panel:after {
-            right: auto;
-            left: -14px;
-            border-right-width: 14px;
-            border-left-width: 0;
-        }
-
-        .timeline-badge.primary {
-            background-color: #2e6da4 !important;
-        }
-
-        .timeline-badge.success {
-            background-color: #3f903f !important;
-        }
-
-        .timeline-badge.warning {
-            background-color: #f0ad4e !important;
-        }
-
-        .timeline-badge.danger {
-            background-color: #d9534f !important;
-        }
-
-        .timeline-badge.info {
-            background-color: #5bc0de !important;
-        }
-
-        .timeline-title {
-            margin-top: 0;
-            color: inherit;
-        }
-
-        .timeline-body > p,
-        .timeline-body > ul {
-            margin-bottom: 0;
-        }
-
-        .timeline-body > p + p {
-            margin-top: 5px;
-        }
-
-        @media(max-width:767px) {
-            ul.timeline:before {
-                left: 40px;
-            }
-
-            ul.timeline > li > .timeline-panel {
-                width: calc(100% - 90px);
-                width: -moz-calc(100% - 90px);
-                width: -webkit-calc(100% - 90px);
-            }
-
-            ul.timeline > li > .timeline-badge {
-                top: 16px;
-                left: 15px;
-                margin-left: 0;
-            }
-
-            ul.timeline > li > .timeline-panel {
-                float: right;
-            }
-
-            ul.timeline > li > .timeline-panel:before {
-                right: auto;
-                left: -15px;
-                border-right-width: 15px;
-                border-left-width: 0;
-            }
-
-            ul.timeline > li > .timeline-panel:after {
-                right: auto;
-                left: -14px;
-                border-right-width: 14px;
-                border-left-width: 0;
-            }
+            bottom: -25px;
+            left: 0;
+            width: 0;
+            height: 0;
+            border-left: 50px solid transparent;
+            border-right: 50px solid transparent;
+            border-top: 25px solid red;
         }
     </style>
 
@@ -202,111 +50,7 @@
 
     <div id="overview-portal" class="container">
 
-    <div class="panel panel-default">
-    <div class="panel-heading">
-    <i class="fa fa-clock-o fa-fw"></i> Responsive Timeline
-    </div>
-    <!-- /.panel-heading -->
-
-    <div class="panel-body">
-    <ul class="timeline">
-    <li>
-    <div class="timeline-badge"><i class="fa fa-check"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Timetable</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li class="timeline-inverted">
-    <div class="timeline-badge warning"><i class="fa fa-credit-card"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Exams</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li>
-    <div class="timeline-badge danger"><i class="fa fa-bomb"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Results</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li class="timeline-inverted">
-    <div class="timeline-badge"><i class="fa fa-check"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Library</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li>
-    <div class="timeline-badge info"><i class="fa fa-save"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Calendar</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li class="timeline-inverted">
-    <div class="timeline-badge"><i class="fa fa-check"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Transport</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    <li>
-    <div class="timeline-badge info"><i class="fa fa-save"></i>
-    </div>
-    <div class="timeline-panel">
-    <div class="timeline-heading">
-    <h4 class="timeline-title">Library</h4>
-    </div>
-    <div class="timeline-body">
-    <p></p>
-    </div>
-    </div>
-    </li>
-
-    </ul>
-    </div>
-    <!-- /.panel-body -->
-    </div>
-    <!-- /.panel -->
-    </div>
+    <div id="hexagon"></div>
 
 	</div> <!-- /container -->
 
