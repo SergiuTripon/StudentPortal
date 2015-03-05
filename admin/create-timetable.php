500dd059149364df6dd138199a054f137d855611
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 20:04:59 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -369,7 +369,7 @@ include '../includes/session.php';
 
 	<script>
     $(document).ready(function () {
-        $(".bootstrap-select > .selectpicker").css("cssText", "color: gray !important;");
+        $(".bootstrap-select > .selectpicker").css("cssText", "color: gray;");
     });
 
     $( ".lecture_lecturer .dropdown-menu > li > a" ).click(function() {
