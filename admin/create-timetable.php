d46d00721ca758f2e6248ce8cc4c5845df73ef40
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 17:51:16 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -91,7 +91,7 @@ include '../includes/session.php';
     <div class="col-xs-12 col-sm-12 full-width pr0 pl0">
     <label for="lecture_lecturer">Lecturer<span class="field-required">*</span></label>
     <select class="mobileSelect lecture_lecturer" name="lecture_lecturer" id="lecture_lecturer">
-        <option disabled>Select an option</option>
+        <option selected>Select an option</option>
     <?php
     $stmt1 = $mysqli->query("SELECT userid FROM user_signin WHERE account_type = 'lecturer'");
 
