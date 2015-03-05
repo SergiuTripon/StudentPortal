8be63b771d53e1136bc1d66bd02e4fa332b56d31
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 21:51:45 +0000

    Changed structure

--- test.php
+++ test.php
@@ -44,29 +44,6 @@ include 'includes/functions.php';
     <div id="overview-portal" class="container">
     <div class="row">
 
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div class="panel panel-primary">
-    <div class="panel-heading">
-    <div class="row">
-    <div class="col-xs-3">
-    <i class="fa fa-comments fa-5x"></i>
-    </div>
-    <div class="col-xs-9 text-right">
-    <div class="huge">26</div>
-    <div>New Comments!</div>
-    </div>
-    </div>
-    </div>
-    <a href="#">
-    <div class="panel-footer">
-    <span class="pull-left">View Details</span>
-    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-    <div class="clearfix"></div>
-    </div>
-    </a>
-    </div>
-    </div>
-
     <a href="../timetable/">
     <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
     <div class="tile large-tile">
