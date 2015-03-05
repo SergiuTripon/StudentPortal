f70552888574dca3c0ad9c3ae3bae7e9edc32eb5
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 17:32:27 +0000

    Changed structure

--- assets/css-paths/bootstrap-fullscreen-select-css-path.php
+++ assets/css-paths/bootstrap-fullscreen-select-css-path.php
@@ -1,2 +1,2 @@
     <!-- bootstrap fullscreen select -->
-    <link href="https://student-portal.co.uk/assets/css/bootstrap-select-fullscreen/bootstrap-fullscreen-select.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/bootstrap-fullscreen-select/bootstrap-fullscreen-select.css" rel="stylesheet">
