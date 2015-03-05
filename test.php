ee8572ab90c83b0e13cecbf61506e6296d93fed1
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 23:17:31 +0000

    Changed structure

--- test.php
+++ test.php
@@ -44,7 +44,9 @@
 
     <div id="overview-portal" class="container">
 
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     <div class="hex-tile"></div>
+    </div>
 
 	</div> <!-- /container -->
 
