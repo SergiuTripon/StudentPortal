5ee6068b8c59681045478f5f3430538ac6fa0e88
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 21:45:15 +0000

    Changed structure

--- test.php
+++ test.php
@@ -67,104 +67,120 @@ include 'includes/functions.php';
     </div>
     </div>
 
-    <a href="../timetable/">
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div class="tile large-tile">
-    <i class="fa fa-clock-o"></i>
-	<p>Timetable<span class="badge"><?php echo ($timetable_count == '0' ? "" : "$timetable_count"); ?></span></p>
+    <div class="col-lg-3 col-md-6">
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+    <div class="row">
+    <div class="col-xs-3">
+    <i class="fa fa-comments fa-5x"></i>
     </div>
-	</div>
+    <div class="col-xs-9 text-right">
+    <div class="huge">26</div>
+    <div>New Comments!</div>
+    </div>
+    </div>
+    </div>
+    <a href="#">
+    <div class="panel-footer">
+    <span class="pull-left">View Details</span>
+    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    <div class="clearfix"></div>
+    </div>
     </a>
-
-    <a href="../exams/">
-    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-    <div class="tile">
-	<i class="fa fa-pencil"></i>
-	<p>Exams<span class="badge"><?php echo ($exams_count == '0' ? "" : "$exams_count"); ?></span></p>
     </div>
-	</div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+    <div class="row">
+    <div class="col-xs-3">
+    <i class="fa fa-comments fa-5x"></i>
+    </div>
+    <div class="col-xs-9 text-right">
+    <div class="huge">26</div>
+    <div>New Comments!</div>
+    </div>
+    </div>
+    </div>
+    <a href="#">
+    <div class="panel-footer">
+    <span class="pull-left">View Details</span>
+    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    <div class="clearfix"></div>
+    </div>
     </a>
-
-    <a href="../results/">
-    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div class="tile">
-	<i class="fa fa-trophy"></i>
-	<p>Results<span class="badge"><?php echo ($results_count == '0' ? "" : "$results_count"); ?></span></p>
     </div>
-	</div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+    <div class="row">
+    <div class="col-xs-3">
+    <i class="fa fa-comments fa-5x"></i>
+    </div>
+    <div class="col-xs-9 text-right">
+    <div class="huge">26</div>
+    <div>New Comments!</div>
+    </div>
+    </div>
+    </div>
+    <a href="#">
+    <div class="panel-footer">
+    <span class="pull-left">View Details</span>
+    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    <div class="clearfix"></div>
+    </div>
     </a>
-
-    <a href="../transport/">
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div class="tile">
-    <i class="fa fa-subway"></i>
-	<p>Transport</p>
     </div>
-	</div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+    <div class="row">
+    <div class="col-xs-3">
+    <i class="fa fa-comments fa-5x"></i>
+    </div>
+    <div class="col-xs-9 text-right">
+    <div class="huge">26</div>
+    <div>New Comments!</div>
+    </div>
+    </div>
+    </div>
+    <a href="#">
+    <div class="panel-footer">
+    <span class="pull-left">View Details</span>
+    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    <div class="clearfix"></div>
+    </div>
     </a>
-
-    <a href="../library/">
-    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-	<div class="tile">
-    <i class="fa fa-book"></i>
-	<p>Library<span class="badge"><?php echo ($library_count == '0' ? "" : "$library_count"); ?></span></p>
     </div>
-	</div>
+    </div>
+
+	<div class="col-lg-3 col-md-6">
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+    <div class="row">
+    <div class="col-xs-3">
+    <i class="fa fa-comments fa-5x"></i>
+    </div>
+    <div class="col-xs-9 text-right">
+    <div class="huge">26</div>
+    <div>New Comments!</div>
+    </div>
+    </div>
+    </div>
+    <a href="#">
+    <div class="panel-footer">
+    <span class="pull-left">View Details</span>
+    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    <div class="clearfix"></div>
+    </div>
     </a>
-
-    <a href="../calendar/">
-	<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-	<div class="tile">
-    <i class="fa fa-calendar"></i>
-	<p>Calendar<span class="badge"><?php echo ($calendar_count == '0' ? "" : "$calendar_count"); ?></span></p>
     </div>
-	</div>
-    </a>
-
-    <a href="../university-map/">
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-    <div class="tile">
-    <i class="fa fa-map-marker"></i>
-	<p>University Map</p>
     </div>
-	</div>
-    <a>
-
-    <a href="../events/">
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div class="tile">
-	<i class="fa fa-ticket"></i>
-	<p>Events<span class="badge"><?php echo ($events_count == '0' ? "" : "$events_count"); ?></span></p>
-    </div>
-	</div>
-    </a>
-
-	<a href="../feedback/">
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div class="tile">
-    <i class="fa fa-check-square-o"></i>
-	<p>Feedback</p>
-    </div>
-	</div>
-    </a>
-
-	<a href="../messenger/">
-    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div class="tile">
-    <i class="fa fa-comments"></i>
-	<p>Messenger<span class="badge"><?php echo ($messenger_count == '0' ? "" : "$messenger_count"); ?></span></p>
-    </div>
-	</div>
-    </a>
-
-	<a href="../account/">
-    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div class="tile">
-    <i class="fa fa-user"></i>
-	<p>Account</p>
-    </div>
-	</div>
-    </a>
 	
 	</div><!-- /row -->
 	
