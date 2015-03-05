5ace142a5401182c592d4cf48541ef3230d8df8e
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-05 09:59:45 +0000

    Changed structure

--- overview.php
+++ overview.php
@@ -65,7 +65,7 @@ include 'includes/functions.php';
     <a href="../results/">
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     <div class="tile">
-	<i class="fa fa-cup"></i>
+	<i class="fa fa-trophy"></i>
 	<p>Results<span class="badge"><?php echo ($results_count == '0' ? "" : "$results_count"); ?></span></p>
     </div>
 	</div>
@@ -272,7 +272,7 @@ include 'includes/functions.php';
     <a href="../results/">
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     <div class="tile">
-	<i class="fa fa-cup"></i>
+	<i class="fa fa-trophy"></i>
 	<p>Results</p>
     </div>
 	</div>
