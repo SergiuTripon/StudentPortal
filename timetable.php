530602cf7bbd63f75e07fc03b9e2a0e9cf90efb6
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-06 13:04:25 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -232,7 +232,7 @@ include 'includes/session.php';
 			<td data-title="Name">'.$module_name.'</td>
 			<td data-title="Notes">'.($module_notes === '' ? "No notes" : "$module_notes").'</td>
             <td data-title="URL">'.($module_url === '' ? "No link" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$module_url\">Link</a>").'</td>
-            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button assign-button" href="/admin/assign-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Assign</span></a></a></td>
+            <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="/admin/allocate-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Allocate</span></a></a></td>
 			<td data-title="Action"><a class="btn btn-primary btn-md ladda-button update-button" href="/admin/update-timetable?id='.$moduleid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></a></td>
             <td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="#deactivate-'.$moduleid.'" data-toggle="modal" data-style="slide-up"><span class="ladda-label">Deactivate</span></a></a></td>
 			</tr>
