cee227ec8437b380b3c00625a9e37ed62aef0d23
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-06 14:37:06 +0000

    Changed structure

--- admin/select-modules.php
+++ admin/select-modules.php
@@ -134,8 +134,7 @@ if (isset($_GET['id'])) {
 			<td data-title="Coursework mark">'.$result_coursework_mark.'</td>
 			<td data-title="Exam mark">'.$result_exam_mark.'</td>
 			<td data-title="Overall mark">'.$result_overall_mark.'</td>
-			<td data-title="Action"><a class="btn btn-primary btn-md ladda-button" href="../update-results/?id='.$resultid.'" data-style="slide-up"><span class="ladda-label">Update</span></a></td>
-			<td data-title="Action"><a id="delete-'.$resultid.'" class="btn btn-primary btn-md ladda-button delete-trigger" href="#modal-'.$resultid.'" data-toggle="modal" data-style="slide-up"><span class="ladda-label">Delete</span></a></td>
+			<td data-title="Action">
 
 			<div class="btn-group btn-action">
             <a class="btn btn-primary" href="../update-results/?id='.$resultid.'">Update</a>
