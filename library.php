82423bb8147a91a2e842626cc13048c0b0ed52e2
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-06 15:28:10 +0000

    Changed structure

--- library.php
+++ library.php
@@ -404,7 +404,7 @@ include 'includes/session.php';
 			<td data-title="Copy no.">'.$book_copy_no.'</td>
 			<td data-title="Status">'.$book_status.'</td>
 			<td data-title="Action">
-			<div class="btn-group">
+			<div class="btn-group btn-action">
             <a class="btn btn-primary" href="#reactivate-'.$bookid.'" data-toggle="modal">Reactivate</a>
             <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
             <span class="fa fa-caret-down"></span>
