7f21f5237aeb14eae8117f96d3b635b154628b6b
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-09 12:18:03 +0000

    Changed structure

--- library.php
+++ library.php
@@ -28,7 +28,7 @@ include 'includes/session.php';
 
 	<div id="library-portal" class="container">
 
-	<ol class="breadcrumb breadcrumb-admin">
+	<ol class="breadcrumb breadcrumb-custom breadcrumb-admin">
     <li><a href="../overview/">Overview</a></li>
 	<li class="active">Library</li>
     </ol>
@@ -242,7 +242,7 @@ include 'includes/session.php';
 
     <div id="library-portal" class="container">
 
-	<ol class="breadcrumb breadcrumb-admin">
+	<ol class="breadcrumb breadcrumb-custom breadcrumb-admin">
     <li><a href="../overview/">Overview</a></li>
     <li class="active">Library</li>
     </ol>
