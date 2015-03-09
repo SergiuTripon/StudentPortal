6e73f3c679d4f1b3a8c35b5800b46964182bd3cb
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-09 13:07:19 +0000

    Changed structure

--- admin/create-timetable.php
+++ admin/create-timetable.php
@@ -69,47 +69,6 @@ include '../includes/session.php';
 	</div>
     <!-- End of Create module -->
 
-    <!-- Create exam -->
-    <h4 class="title-separator text-center">Exam</h4>
-    <hr class="hr-separator">
-
-	<div class="form-group">
-	<div class="col-xs-12 col-sm-12 full-width pr0 pl0">
-	<label for="exam_name">Exam name<span class="field-required">*</span></label>
-    <input class="form-control" type="text" name="exam_name" id="exam_name" placeholder="Enter a name">
-	</div>
-	</div>
-
-	<div class="form-group">
-	<div class="col-xs-12 col-sm-12 full-width pr0 pl0">
-	<label for="exam_notes">Exam notes</label>
-    <textarea class="form-control" rows="5" name="exam_notes" id="exam_notes" placeholder="Enter notes"></textarea>
-	</div>
-	</div>
-
-    <div class="form-group">
-	<div class="col-xs-6 col-sm-6 full-width pl0">
-	<label for="exam_date">Exam date<span class="field-required">*</span></label>
-	<input type="text" class="form-control" name="exam_date" id="exam_date" placeholder="Select a date">
-	</div>
-	<div class="col-xs-6 col-sm-6 full-width pr0">
-	<label for="exam_time">Exam time<span class="field-required">*</span></label>
-	<input type="text" class="form-control" name="exam_time" id="exam_time" placeholder="Select a time">
-	</div>
-	</div>
-
-    <div class="form-group">
-	<div class="col-xs-6 col-sm-6 full-width pl0">
-	<label for="exam_location">Exam location<span class="field-required">*</span></label>
-	<input type="text" class="form-control" name="exam_location" id="exam_location" placeholder="Enter a location">
-	</div>
-	<div class="col-xs-6 col-sm-6 full-width pr0">
-	<label for="exam_capacity">Exam capacity<span class="field-required">*</span></label>
-	<input type="text" class="form-control" name="exam_capacity" id="exam_capacity" placeholder="Enter a capacity">
-	</div>
-	</div>
-    <!-- End of Create exam -->
-
 	</div>
 
 	<hr>
