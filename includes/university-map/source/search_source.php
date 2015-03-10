d26420e71456f6600f7d9f282ce8e9d4c49e6c0f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-10 12:13:54 +0000

    Changed structure

--- includes/university-map/source/search_source.php
+++ includes/university-map/source/search_source.php
@@ -26,8 +26,8 @@ header("Content-type: text/xml");
 while ($row = $result->fetch_assoc()){
       $node = $dom->createElement("marker");
       $newnode = $parnode->appendChild($node);
-      $newnode->setAttribute("name", $row['marker_title']);
-      $newnode->setAttribute("description", $row['marker_description']);
+      $newnode->setAttribute("name", $row['marker_name']);
+      $newnode->setAttribute("description", $row['marker_notes']);
       $newnode->setAttribute("lat", $row['marker_lat']);
       $newnode->setAttribute("lng", $row['marker_long']);
       $newnode->setAttribute("type", $row['marker_category']);
