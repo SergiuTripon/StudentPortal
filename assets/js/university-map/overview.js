42ea671050549ac84ef094692c52471e5aeb497a
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-10 11:59:57 +0000

    Changed structure

--- assets/js/university-map/overview.js
+++ assets/js/university-map/overview.js
@@ -18,16 +18,14 @@
     };
 
     function loadMap() {
-        var mapOptions = {
-            center: new google.maps.LatLng(51.527287, -0.103842),
-            zoom: 15,
-            mapTypeId: 'roadmap',
-            styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
-        };
-
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var infoWindow = new google.maps.InfoWindow({ maxWidth: 400 });
+        var map = new google.maps.Map(document.getElementById("map"), {
+        center: new google.maps.LatLng(51.527287, -0.103842),
+        zoom: 15,
+        mapTypeId: 'roadmap'
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            maxWidth: 400
+        });
 
     // Change this depending on the name of your PHP file
     downloadUrl("../../includes/university-map/source/overview_source.php", function(data) {
