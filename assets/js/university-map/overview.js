ce96669dddedb12ae37dcfeadcf8aa1570755ce6
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-10 19:18:23 +0000

    Changed structure

--- assets/js/university-map/overview.js
+++ assets/js/university-map/overview.js
@@ -1,19 +1,18 @@
+    var infoWindow = new google.maps.InfoWindow();
+
     var customIcons = {
-    building: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
-    },
-    student_centre: {
-        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
-    },
-    lecture_theatre: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png'
-    },
-    computer_lab: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_yellow.png'
-    },
-    library: {
-        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_black.png'
-    }
+        building: {
+            icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png'
+        },
+        student_centre: {
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green.png'
+        },
+        lecture_theatre: {
+            icon: 'http://maps.google.com/mapfiles/ms/icons/yellow.png'
+        },
+        computer_lab: {
+            icon: 'http://maps.google.com/mapfiles/ms/icons/purple.png'
+        }
     };
 
     var markerGroups = {
@@ -24,65 +23,78 @@
         "library": [],
         "cycle_hire": [],
         "cycle_parking": [],
-        "ATM": []
+        "atm": []
     };
 
-    function loadMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
+    function load() {
+    var map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(51.527287, -0.103842),
-        zoom: 15,
+        zoom: 12,
         mapTypeId: 'roadmap'
-        });
-        var infoWindow = new google.maps.InfoWindow({
-            maxWidth: 400
-        });
+    });
 
-    // Change this depending on the name of your PHP file
     downloadUrl("../../includes/university-map/source/overview_source.php", function(data) {
-        var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName("marker");
-        for (var i = 0; i < markers.length; i++) {
+    var xml = data.responseXML;
+    var markers = xml.documentElement.getElementsByTagName("marker");
+    for (var i = 0; i < markers.length; i++) {
         var name = markers[i].getAttribute("name");
         var notes = markers[i].getAttribute("notes");
         var category = markers[i].getAttribute("category");
         var point = new google.maps.LatLng(
             parseFloat(markers[i].getAttribute("lat")),
             parseFloat(markers[i].getAttribute("lng")));
-        var html = "<b>" + name + "</b> <br/>" + notes;
+        var marker = createMarker(point, name, notes, category, map);
+    }
+    });
+    }
+
+    function createMarker(point, name, notes, category, map) {
         var icon = customIcons[category] || {};
         var marker = new google.maps.Marker({
             map: map,
             position: point,
             icon: icon.icon,
-            type: category,
-            animation: google.maps.Animation.DROP
+            type: category
         });
+        if (!markerGroups[category]) markerGroups[category] = [];
+        markerGroups[category].push(marker);
+        var html = "<b>" + name + "</b> <br/>" + notes;
         bindInfoWindow(marker, map, infoWindow, html);
+        return marker;
+    }
+
+    function toggleGroup(category) {
+        for (var i = 0; i < markerGroups[category].length; i++) {
+            var marker = markerGroups[category][i];
+            if (!marker.getVisible()) {
+                marker.setVisible(true);
+            } else {
+                marker.setVisible(false);
+            }
         }
-    });
     }
 
     function bindInfoWindow(marker, map, infoWindow, html) {
         google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(html);
-        infoWindow.open(map, marker);
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+
         });
     }
 
     function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
+        var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest();
 
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-        request.onreadystatechange = doNothing;
-        callback(request, request.status);
-        }
-    };
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                request.onreadystatechange = doNothing;
+                callback(request, request.status);
+            }
+        };
 
-    request.open('GET', url, true);
-    request.send(null);
+        request.open('GET', url, true);
+        request.send(null);
     }
 
     function doNothing() {}
+    google.maps.event.addDomListener(window, 'load', load);
