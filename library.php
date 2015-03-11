57d622535167cc0182c434607b8f12643550d521
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-11 12:28:42 +0100

    Changed structure

--- library.php
+++ library.php
@@ -513,7 +513,66 @@ include 'includes/session.php';
 	<tbody>
 	<?php
 
-	$stmt1 = $mysqli->query("SELECT system_books_reserved.bookid, system_books_reserved.userid, user_details.firstname, user_details.surname, system_books.book_name, system_books.book_author, system_books.book_status FROM system_books_reserved LEFT JOIN user_details ON system_books_reserved.userid=user_details.userid LEFT JOIN system_books ON system_books_reserved.bookid=system_books.bookid WHERE system_books.book_status = 'reserved'");
+	$stmt1 = $mysqli->query("SELECT system_books_reserved.bookid, system_books_reserved.userid, user_details.firstname, user_details.surname, system_books.book_name, system_books.book_author, system_books.book_status FROM system_books_reserved LEFT JOIN user_details ON system_books_reserved.userid=user_details.userid LEFT JOIN system_books ON system_books_reserved.bookid=system_books.bookid WHERE system_books_reserved.userid='$session_userid' AND system_books.book_status = 'reserved'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$bookid = $row["bookid"];
+    $userid = $row["userid"];
+    $firstname = $row["firstname"];
+    $surname = $row["surname"];
+	$book_name = $row["book_name"];
+	$book_author = $row["book_author"];
+
+	echo '<tr id="return-'.$bookid.'">
+
+            <td data-title="Reserved by">'.$firstname.' '.$surname.'</td>
+			<td data-title="Name">'.$book_name.'</td>
+			<td data-title="Author">'.$book_author.'</td>
+			<td data-title="Action"><a id=return-'.$userid.' class="btn btn-primary btn-md ladda-button" href="../messenger/message-user?id='.$userid.'" data-style="slide-up"><span class="ladda-label">Message</span></a></td>
+            <td data-title="Action"><a id=return-'.$bookid.' class="btn btn-primary btn-md return-button ladda-button" data-style="slide-up"><span class="ladda-label">Mark returned</span></a></td>
+			</tr>';
+	}
+
+	$stmt1->close();
+	?>
+	</tbody>
+
+	</table>
+	</section>
+
+  	</div><!-- /panel-body -->
+    </div><!-- /panel-collapse -->
+	</div><!-- /panel-default -->
+
+    <div class="panel panel-default">
+
+    <div class="panel-heading" role="tab" id="headingThree">
+  	<h4 class="panel-title">
+	<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree"> Reserved books</a>
+  	</h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+  	<div class="panel-body">
+
+	<!-- Reserved books -->
+	<section id="no-more-tables">
+	<table class="table table-condensed table-custom book-table">
+
+	<thead>
+	<tr>
+    <th>Reserved by</th>
+	<th>Name</th>
+	<th>Author</th>
+	<th>Action</th>
+    <th>Action</th>
+	</tr>
+	</thead>
+
+	<tbody>
+	<?php
+
+	$stmt1 = $mysqli->query("SELECT system_books_requested.bookid, system_books_requested.userid, user_details.firstname, user_details.surname, system_books.book_name, system_books.book_author, system_books.book_status FROM system_books_requested LEFT JOIN user_details ON system_books_requested.userid=user_details.userid LEFT JOIN system_books ON system_books_requested.bookid=system_books.bookid WHERE system_books_requested.userid='$session_userid'");
 
 	while($row = $stmt1->fetch_assoc()) {
 
