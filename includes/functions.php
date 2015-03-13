54482757d9e8fec5b56f8d3ac0e86e3752df75d1
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-13 18:53:05 +0100

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -1629,8 +1629,8 @@ function UpdateTask() {
 	$task_duedate = filter_input(INPUT_POST, 'task_duedate1', FILTER_SANITIZE_STRING);
 	$task_category = filter_input(INPUT_POST, 'task_category1', FILTER_SANITIZE_STRING);
 
-    $task_startdate = date($task_startdate, "Y-m-d H:i:s");
-    $task_duedate = date($task_duedate, "Y-m-d H:i:s");
+    $task_startdate = $task_startdate->format('Y-m-d H:i:s');
+    $task_duedate = $task_duedate->format('Y-m-d H:i:s');
 	$task_category = strtolower($task_category);
 
 	if ($task_category == 'university') { $task_class = 'event-important'; }
