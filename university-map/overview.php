a72350a13e0c7270e8a0ab971d9ab60a482fc431
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-13 12:25:34 +0100

    Changed structure

--- university-map/overview.php
+++ university-map/overview.php
@@ -34,8 +34,8 @@ include '../includes/session.php';
 
     <form class="form-custom">
 
-    <div id="marker-toggle">
-    <div style="background-color: #4480E8; ">
+    <div class="marker-toggle">
+    <div id="building-checkbox">
     <label class="checkbox-inline">
         <input type="checkbox" checked="checked" onclick="toggleGroup('building')"><span style="color: #FFFFFF; font-weight: 600;"> Buildings</span>
     </label>
