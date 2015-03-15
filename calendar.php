85ebcd9b867cbb81a71a3143ada8076705f936a0
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-15 12:15:10 +0100

    Changed structure

--- calendar.php
+++ calendar.php
@@ -520,8 +520,6 @@ include 'includes/session.php';
 
     $("#load-due-tasks").click(function() {
         $('.table-custom').dataTable().fnDestroy();
-        $("#due-tasks-load").load("../../includes/calendar/due-tasks.php");
-        initDataTables();
     });
 
     //Ladda
