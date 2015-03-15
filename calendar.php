70380b030843cd2a471a69293549b2509a5e7c34
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-15 12:36:01 +0100

    Changed structure

--- calendar.php
+++ calendar.php
@@ -72,7 +72,7 @@ include 'includes/session.php';
     <div class="panel-heading" role="tab" id="headingOne">
   	<h4 class="panel-title">
 	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Due tasks</a>
-    <a id="load-due-tasks" class="pull-right"><i class="fa fa-refresh"></i></a>
+    <a id="load-due-tasks" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Load</a>
   	</h4>
     </div>
     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
@@ -80,7 +80,7 @@ include 'includes/session.php';
 
 	<!-- Due tasks -->
 	<section id="no-more-tables">
-	<table class="table table-condensed table-custom table-due-tasks">
+	<table class="table table-condensed table-custom">
 
 	<thead>
 	<tr>
@@ -95,7 +95,115 @@ include 'includes/session.php';
 	</thead>
 
 	<tbody id="due-tasks-load">
+	<?php
 
+	$stmt1 = $mysqli->query("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate, task_category FROM user_tasks WHERE userid = '$session_userid' AND task_status = 'active'");
+
+	while($row = $stmt1->fetch_assoc()) {
+
+	$taskid = $row["taskid"];
+	$task_name = $row["task_name"];
+	$task_notes = $row["task_notes"];
+    $task_url = $row["task_url"];
+	$task_startdate = $row["task_startdate"];
+	$task_duedate = $row["task_duedate"];
+	$task_category = $row["task_category"];
+	$task_category = ucfirst($row["task_category"]);
+
+	echo '<tr id="task-'.$taskid.'">
+
+			<td data-title="Name">'.$task_name.'</td>
+			<td data-title="Notes">'.($task_notes === '' ? "-" : "$task_notes").'</td>
+			<td data-title="URL">'.($task_url === '' ? "-" : "<a class=\"btn btn-primary btn-md\" target=\"_blank\" href=\"//$task_url\">Link</a>").'</td>
+			<td data-title="Start date">'.$task_startdate.'</td>
+			<td data-title="Due date">'.$task_duedate.'</td>
+			<td data-title="Category">'.$task_category.'</td>
+			<td data-title="Action">
+
+			<div class="btn-group btn-action">
+            <a class="btn btn-primary" href="#complete-'.$taskid.'" data-toggle="modal">Complete</a>
+            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <span class="fa fa-caret-down"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+            <li><a href="../calendar/update-task?id='.$taskid.'" data-toggle="modal" data-toggle="modal">Update</a></li>
+            <li><a href="#deactivate-'.$taskid.'" data-toggle="modal" data-toggle="modal">Archive</a></li>
+            </ul>
+            </div>
+            </td>
+
+			</tr>
+
+            <div id="complete-'.$taskid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+    		<div class="modal-dialog">
+    		<div class="modal-content">
+
+			<div class="modal-header">
+			<div class="form-logo text-center">
+			<i class="fa fa-trash"></i>
+			</div>
+			</div>
+
+			<div class="modal-body">
+			<p id="complete-question" class="text-center feedback-sad">Are you sure you want to complete '.$task_name.'?</p>
+			<p id="complete-confirmation" class="text-center feedback-happy" style="display: none;">'.$task_name.' has been completed successfully.</p>
+			</div>
+
+			<div class="modal-footer">
+			<div id="complete-hide">
+			<div class="pull-left">
+			<a id="complete-'.$taskid.'" class="btn btn-danger btn-lg complete-button ladda-button" data-style="slide-up">Yes</a>
+			</div>
+			<div class="text-right">
+			<button type="button" class="btn btn-success btn-lg ladda-button" data-style="slide-up" data-dismiss="modal">No</button>
+			</div>
+			</div>
+			<div class="text-center">
+			<a id="complete-success-button" class="btn btn-primary btn-lg ladda-button" style="display: none;" data-style="slide-up">Continue</a>
+			</div>
+			</div>
+
+			</div><!-- /modal -->
+			</div><!-- /modal-dialog -->
+			</div><!-- /modal-content -->
+
+			<div id="deactivate-'.$taskid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+    		<div class="modal-dialog">
+    		<div class="modal-content">
+
+			<div class="modal-header">
+			<div class="form-logo text-center">
+			<i class="fa fa-trash"></i>
+			</div>
+			</div>
+
+			<div class="modal-body">
+			<p id="deactivate-question" class="text-center feedback-sad">Are you sure you want to archive '.$task_name.'?</p>
+			<p id="deactivate-confirmation" class="text-center feedback-happy" style="display: none;">'.$task_name.' has been archived successfully.</p>
+			</div>
+
+			<div class="modal-footer">
+			<div id="deactivate-hide">
+			<div class="pull-left">
+			<a id="deactivate-'.$taskid.'" class="btn btn-danger btn-lg deactivate-button ladda-button" data-style="slide-up">Yes</a>
+			</div>
+			<div class="text-right">
+			<button type="button" class="btn btn-success btn-lg ladda-button" data-style="slide-up" data-dismiss="modal">No</button>
+			</div>
+			</div>
+			<div class="text-center">
+			<a id="deactivate-success-button" class="btn btn-primary btn-lg ladda-button" style="display: none;" data-style="slide-up">Continue</a>
+			</div>
+			</div>
+
+			</div><!-- /modal -->
+			</div><!-- /modal-dialog -->
+			</div><!-- /modal-content -->';
+	}
+
+	$stmt1->close();
+	?>
 	</tbody>
 
 	</table>
@@ -130,7 +238,7 @@ include 'includes/session.php';
 	</tr>
 	</thead>
 
-	<tbody>
+	<tbody id="completed-tasks-load">
 	<?php
 
 	$stmt2 = $mysqli->query("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate, task_category, DATE_FORMAT(updated_on,'%d %b %y %H:%i') as updated_on FROM user_tasks where userid = '$session_userid' AND task_status = 'completed'");
@@ -402,16 +510,10 @@ include 'includes/session.php';
 
 	<script>
     $(document).ready(function () {
-
         $("#calendar-toggle").hide();
         $(".task-tile").addClass("tile-selected");
         $(".task-tile p").addClass("tile-text-selected");
         $(".task-tile i").addClass("tile-text-selected");
-    });
-
-    $("#load-due-tasks").click(function() {
-        $("#due-tasks-load").load("../../includes/calendar/due-tasks.php");
-        initDataTables();
     });
 
     //Ladda
@@ -456,18 +558,16 @@ include 'includes/session.php';
 	});
 	}(jQuery));
 
-    function initDataTables() {
     //DataTables
-    $('.table-due-tasks').dataTable({
+    $('.table-custom').dataTable({
         "iDisplayLength": 10,
-        "paging": true,
-        "ordering": true,
-        "info": false,
-        "language": {
-            "emptyTable": "There are no tasks to display at the moment."
-        }
-    });
-    }
+		"paging": true,
+		"ordering": true,
+		"info": false,
+		"language": {
+			"emptyTable": "There are no tasks to display at the moment."
+		}
+	});
 
     //Responsiveness
 	$(window).resize(function(){
