7f7b213da4a07774d4d738859b6b2c0efcbd4661
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-16 13:07:55 +0100

    Changed structure

--- timetable.php
+++ timetable.php
@@ -630,8 +630,18 @@ include 'includes/session.php';
 	<script>
     $( document ).ready(function() {
         var d = new Date();
-        var day = d.getDate();
-        alert(day);
+        var weekday = new Array(7);
+        weekday[0]=  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        var n = weekday[d.getDay()];
+
+        alert(n);
     });
 
     //Ladda
