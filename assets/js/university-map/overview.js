0c618e6d1f95d5fd867aad05d5da8ceb06cf718f
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-18 14:40:49 +0000

    Changed structure

--- assets/js/university-map/overview.js
+++ assets/js/university-map/overview.js
@@ -57,6 +57,48 @@
         // Setup click-event listener: simply set the map to London
         google.maps.event.addDomListener(currentLocationUI, 'click', function () {
 
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = new google.maps.LatLng(position.coords.latitude,
+                        position.coords.longitude);
+
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: pos,
+                        title: 'You are here.'
+                    });
+
+                    var infowindow = new google.maps.InfoWindow({
+                        map: map,
+                        position: pos,
+                        content: 'You are here.'
+                    });
+
+                    map.setCenter(pos);
+                }, function() {
+                    handleNoGeolocation(true);
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleNoGeolocation(false);
+            }
+
+        function handleNoGeolocation(errorFlag) {
+            if (errorFlag) {
+                var content = 'Error: The Geolocation service failed.';
+            } else {
+                var content = 'Error: Your browser doesn\'t support geolocation.';
+            }
+
+            var options = {
+                map: map,
+                position: new google.maps.LatLng(60, 105),
+                content: content
+            };
+
+            var infowindow = new google.maps.InfoWindow(options);
+            map.setCenter(options.position);
+        }
         });
     }
 
