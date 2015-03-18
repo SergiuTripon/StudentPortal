8c27e5081936553c590487fee41662833fce2d1b
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-18 16:28:19 +0000

    Changed structure

--- assets/js/university-map/overview.js
+++ assets/js/university-map/overview.js
@@ -74,6 +74,10 @@
 
                     infowindow.open(map,marker);
 
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map,marker);
+                    });
+
                     map.setCenter(pos);
                 }, function() {
                     handleNoGeolocation(true);
