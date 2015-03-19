45bc773ddfce0feb36597f89a45de4f0b4c80445
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-19 22:11:35 +0000

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -1285,7 +1285,7 @@ function AllocateExam() {
     $userToAllocate = filter_input(INPUT_POST, 'userToAllocate', FILTER_SANITIZE_NUMBER_INT);
     $examToAllocate = filter_input(INPUT_POST, 'examToAllocate', FILTER_SANITIZE_NUMBER_INT);
 
-    $stmt1 = $mysqli->prepare("DELETE FROM user_exam WHERE userid=? AND examid=?");
+    $stmt1 = $mysqli->prepare("INSERT INTO user_exam (userid, examid) VALUES (?, ?)");
     $stmt1->bind_param('ii', $userToAllocate, $examToAllocate);
     $stmt1->execute();
     $stmt1->close();
