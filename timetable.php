231f01caf11728ca5772ef4915bab6a53482b0d8
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-19 19:05:11 +0000

    Changed structure

--- timetable.php
+++ timetable.php
@@ -2189,8 +2189,6 @@ include 'includes/session.php';
     var clickedID = this.id.split('-');
     var lectureToReactivate = clickedID[1];
 
-    alert(lectureToReactivate);
-
 	jQuery.ajax({
 	type: "POST",
 	url: "https://student-portal.co.uk/includes/processes.php",
