3d08ae04843dec559f38ca6b808f9cd351b179da
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-19 23:03:57 +0000

    Changed structure

--- test.php
+++ test.php
@@ -5,12 +5,21 @@ global $mysqli;
 
 $feedbackToApprove = '2';
 
-$stmt2 = $mysqli->prepare("SELECT feedback_from, module_staff FROM user_feedback_sent WHERE feedbackid = ?");
+$stmt2 = $mysqli->prepare("SELECT feedback_from, module_staff FROM user_feedback_sent WHERE feedbackid = ? ORDER BY feedbackid DESC");
 $stmt2->bind_param('i', $feedbackToApprove);
 $stmt2->execute();
 $stmt2->store_result();
 $stmt2->bind_result($feedback_from, $module_staff);
 $stmt2->fetch();
 
+$stmt2 = $mysqli->prepare("SELECT feedback_from, module_staff FROM user_feedback_sent WHERE feedbackid = ? ORDER BY feedbackid ASC");
+$stmt2->bind_param('i', $feedbackToApprove);
+$stmt2->execute();
+$stmt2->store_result();
+$stmt2->bind_result($feedback_from1, $module_staff1);
+$stmt2->fetch();
+
 echo $feedback_from;
 echo $module_staff;
+echo $feedback_from1;
+echo $module_staff1;
