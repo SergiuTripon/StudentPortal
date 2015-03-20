6d6f52c7779d777cc519a327e2b43e72e31e669a
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-20 18:05:59 +0000

    Changed structure

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Student Portal</title>
+
+    <?php include 'assets/css-paths/common-css-paths.php'; ?>
+
+    <style>
+    #signin a {
+        color: #FFFFFF;
+        background-color: #992320;
+    }
+    #signin a:focus, #signin a:hover {
+        color: #FFFFFF;
+        background-color: #992320;
+    }
+    </style>
+
+</head>
+
+<body>
+
+</body>
+</html>
