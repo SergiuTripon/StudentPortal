a52bdea5f5971125f2ae2e8c945dbeea13baaa52
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-20 14:06:17 +0000

    Changed structure

--- feedback.php
+++ feedback.php
@@ -440,7 +440,7 @@ include 'includes/session.php';
 
 			<div class="modal-header">
             <div class="close"><i class="fa fa fa-check-square-o"></i></div>
-            <h4 class="modal-title" id="modal-custom-label">'.$firstname.' '.$firstname.'</h4>
+            <h4 class="modal-title" id="modal-custom-label">'.$firstname.' '.$surname.'</h4>
 			</div>
 
 			<div class="modal-body">
