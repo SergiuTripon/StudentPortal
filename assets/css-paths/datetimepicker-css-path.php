c8f19cdb2a2f5a052e5e2cf357869d97f88952f6
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-24 20:01:35 +0000

    Changed structure

--- assets/css-paths/datetimepicker-css-path.php
+++ assets/css-paths/datetimepicker-css-path.php
@@ -1,6 +1,3 @@
     <!-- Date Time Picker CSS -->
     <link href="https://student-portal.co.uk/assets/css/date-time-picker/bootstrap-datepicker.min.css" rel="stylesheet">
     <link href="https://student-portal.co.uk/assets/css/date-time-picker/bootstrap-timepicker.min.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery-ui.min.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery.ui.theme.font-awesome.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery-ui-timepicker-addon.min.css" rel="stylesheet">
--- assets/js-paths/datetimepicker-js-path.php
+++ assets/js-paths/datetimepicker-js-path.php
@@ -1,6 +1,3 @@
     <!-- Date Time Picker JS -->
     <script src="https://student-portal.co.uk/assets/js/date-time-picker/bootstrap-datepicker.min.js"></script>
     <script src="https://student-portal.co.uk/assets/js/date-time-picker/bootstrap-timepicker.min.js"></script>
-    <script src="https://student-portal.co.uk/assets/js/date-time-picker/jquery-ui.min.js"></script>
-    <script src="https://student-portal.co.uk/assets/js/date-time-picker/jquery-ui-sliderAccess.js"></script>
-    <script src="https://student-portal.co.uk/assets/js/date-time-picker/jquery-ui-timepicker-addon.min.js"></script>
