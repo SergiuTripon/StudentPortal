d788bc469eedd206f88ff7a7c3e3ae760753141c
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-24 21:51:44 +0000

    Changed structure

--- assets/css-paths/datetimepicker-css-path.php
+++ assets/css-paths/datetimepicker-css-path.php
@@ -1,3 +1,5 @@
-    <!-- Date Time Picker CSS -->
+    <link href="https://student-portal.co.uk/assets/css/date-time-picker/timepicki.css" rel="stylesheet">
+
+    <!-- Date Time Picker CSS
     <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery-ui.min.css" rel="stylesheet">
-    <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery-ui-timepicker-addon.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/date-time-picker/jquery-ui-timepicker-addon.min.css" rel="stylesheet">-->
