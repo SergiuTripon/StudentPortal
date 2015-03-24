8f26cb04da4c0caea0fb52a3a41e0fa4fc2c11e3
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-24 22:39:54 +0000

    Changed structure

--- calendar/create-task.php
+++ calendar/create-task.php
@@ -10,6 +10,7 @@ include '../includes/session.php';
 	<?php include '../assets/meta-tags.php'; ?>
 
 	<?php include '../assets/css-paths/common-css-paths.php'; ?>
+    <?php include '../assets/css-paths/select2-css-path.php'; ?>
 	<?php include '../assets/css-paths/datetimepicker-css-path.php'; ?>
 
 	<title>Student Portal | Create task</title>
@@ -133,8 +134,8 @@ include '../includes/session.php';
 	<?php endif; ?>
 
 	<?php include '../assets/js-paths/common-js-paths.php'; ?>
-	<?php include '../assets/js-paths/datetimepicker-js-path.php'; ?>
     <?php include '../assets/js-paths/select2-js-path.php'; ?>
+	<?php include '../assets/js-paths/datetimepicker-js-path.php'; ?>
 
 	<script>
     $(document).ready(function () {
