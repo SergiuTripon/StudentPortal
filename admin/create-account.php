e5f7027604b8ee23b279be7861ed5e9be60333c8
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-25 22:52:27 +0000

    Changed structure

--- admin/create-account.php
+++ admin/create-account.php
@@ -7,8 +7,9 @@ include '../includes/session.php';
 
 <head>
 
-	<?php include 'assets/meta-tags.php'; ?>
+	<?php include '../assets/meta-tags.php'; ?>
 
+    <?php include '../assets/css-paths/select2-css-path.php'; ?>
 	<?php include '../assets/css-paths/common-css-paths.php'; ?>
 	<?php include '../assets/css-paths/datetimepicker-css-path.php'; ?>
 
@@ -438,6 +439,7 @@ include '../includes/session.php';
 	<?php endif; ?>
 
 	<?php include '../assets/js-paths/common-js-paths.php'; ?>
+    <?php include '../assets/js-paths/select2-js-path.php'; ?>
 	<?php include '../assets/js-paths/datetimepicker-js-path.php'; ?>
 
 	<script>
