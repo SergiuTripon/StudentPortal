750af06fe2184930da34eaa30d67b5e6df44b96e
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-25 22:15:40 +0000

    Changed structure

--- includes/calendar/source/tasks_json.php
+++ includes/calendar/source/tasks_json.php
@@ -5,7 +5,7 @@ header("Cache-Control: no-cache, must-revalidate");
 
 $sql = 'SELECT t.taskid, t.task_name, t.task_class, t.task_startdate, t.task_duedate
 FROM user_task t
-WHERE t.userid = "'.$session_userid.'" AND t.task_status = "active"
+WHERE t.userid = "'.$session_userid.'" AND t.task_status = "active" AND t.task_duedate > NOW()
 
 UNION ALL
 
