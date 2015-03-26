15d194c8b46fed0b7845ceee46d2790108578608
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-26 22:24:00 +0000

    Changed structure

--- admin/create-account.php
+++ admin/create-account.php
@@ -675,6 +675,8 @@ include '../includes/session.php';
         fee_amount = $("#fee_amount").val();
 	}
 
+    alert(fee_amount);
+
 	var email = $("#email").val();
 	if(email === '') {
         $("label[for='email']").empty().append("Please enter an email address.");
