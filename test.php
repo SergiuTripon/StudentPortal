dba6c90e922ecdaba6ff163c4bde7c360983ecfb
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-27 15:03:32 +0000

    Changed structure

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$date  = '01/01/1993';
+$date  = '01/01/1993 14:49';
 $date = date("Y-m-d",strtotime("01/01/1993"));
 //$date = preg_replace('#(\d{2})/(\d{2})/(\d{4})\s(.*)#', '$3-$2-$1', $date);
 //$date = preg_replace('#(\d{2})/(\d{2})/(\d{4})\s(.*)#', '$3-$2-$1 $4', $date);
