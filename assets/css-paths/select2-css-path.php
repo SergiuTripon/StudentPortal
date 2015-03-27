68f8df51716ac8cbf7684673e4f823e291327ed5
triponsergiu@hotmail.co.uk <triponsergiu@hotmail.co.uk>
2015-03-27 18:21:16 +0000

    Changed structure

--- assets/css-paths/select2-css-path.php
+++ assets/css-paths/select2-css-path.php
@@ -1,2 +1,3 @@
-    <!-- bootstrap select -->
+    <!-- select2 -->
     <link href="https://student-portal.co.uk/assets/css/select2/select2.min.css" rel="stylesheet">
+    <link href="https://student-portal.co.uk/assets/css/select2/select2-fontawesome.css" rel="stylesheet">
