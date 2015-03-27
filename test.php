a7447cbf9eb0da688f8f7c03b004ea3f5cd232e7
SergiuTripon <triponsergiu@hotmail.co.uk>
2015-03-27 19:41:10 +0000

    Changing structure

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$date  = '01/07/1993';
-$date = DateTime::createFromFormat('d/m/Y', $date);
-$date = $date->format('Y-m-d');
+$date  = '27/03/2015 19:39';
+$date = DateTime::createFromFormat('d/m/Y H:i', $date);
+$date = $date->format('Y-m-d H:i');
 echo $date;
