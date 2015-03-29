68595b277a45ee5eb4bf524ffcbd72377dd75b65
triponsergiu <mexug0Aq>
2015-03-29 20:54:29 +0100

    Modified the contact email style

--- library.php
+++ library.php
@@ -94,7 +94,7 @@ include 'includes/session.php';
 	$book_status = $row["book_status"];
 	$book_status = ucfirst($book_status);
 
-    $stmt2 = $mysqli->prepare("SELECT r.bookid FROM system_book_reserved r LEFT JOIN system_book_loaned l ON r.bookid=l.bookid WHERE r.bookid=? AND ((r.isCollected='0' AND r.reservation_status='active') OR (l.isReturned = '0' AND l.loan_status='active') OR (l.isRequested = '0'))");
+    $stmt2 = $mysqli->prepare("SELECT r.bookid FROM system_book_reserved r LEFT JOIN system_book_loaned l ON r.bookid=l.bookid WHERE r.bookid=? AND ((r.isCollected='0' AND r.reservation_status='active') OR (l.isReturned = '0' AND l.loan_status='ongoing') OR (l.isRequested = '0'))");
     $stmt2->bind_param('i', $bookid);
     $stmt2->execute();
     $stmt2->store_result();
