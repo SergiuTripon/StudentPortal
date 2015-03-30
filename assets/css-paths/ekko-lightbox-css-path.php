f96a963de6431ee481f195c3c0d8d22eae00e595
triponsergiu <mexug0Aq>
2015-03-30 23:08:33 +0100

    Changed structure

---
+++ assets/css-paths/ekko-lightbox-css-path.php
@@ -0,0 +1,2 @@
+    <!-- ekko lightbox -->
+    <link href="https://student-portal.co.uk/assets/css/ekko-lightbox/ekko-lightbox.min.css" rel="stylesheet">
---
+++ assets/js-paths/ekko-lightbox-js-path.php
@@ -0,0 +1,2 @@
+    <!-- ekko lightbox -->
+    <script src="https://student-portal.co.uk/assets/js/ekko-lightbox/ekko-lightbox.min.js"></script>
