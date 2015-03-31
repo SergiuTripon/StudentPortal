89298247087109cfa21d16318340660fcebfccc3
triponsergiu <mexug0Aq>
2015-03-31 23:07:20 +0100

    Changed structure

--- calendar.php
+++ calendar.php
@@ -717,7 +717,7 @@ include 'includes/session.php';
             $('#complete-hide').hide();
             $('#complete-success-button').show();
             $("#complete-success-button").click(function () {
-                $('#complete'-taskToComplete).modal('toggle');
+                $('#complete-'+taskToComplete).modal('toggle');
             });
 	},
 	error:function (xhr, ajaxOptions, thrownError){
