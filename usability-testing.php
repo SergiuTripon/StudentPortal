7583d71d2e0b626d95b90fe6d3a5996ac6329592
triponsergiu <mexug0Aq>
2015-03-31 19:10:16 +0100

    Changed structure

--- usability-testing.php
+++ usability-testing.php
@@ -26,101 +26,110 @@ include 'includes/session.php';
 
     <div id="usability-testing-showcase" class="container">
 
-    <h1 class="text-center">Features</h1>
+    <h1 class="text-center">Usability testing</h1>
     <hr class="hr-small">
 
-	<p class="text-left">Click and discover</p>
+	<p class="text-left">Click to watch the videos</p>
 
     <div class="row">
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-    <div id="timetable">
+    <a href="https://www.youtube.com/watch?v=AJoGdJwocQ8" data-toggle="lightbox" data-type="youtube" data-title="Timetable (0:00)">
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
 	<div class="tile large-tile">
     <i class="fa fa-clock-o"></i>
 	<p>Timetable</p>
-    </div>
 	</div>
 	</div>
+    </a>
 
-    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div id="exams">
+    <a href="https://www.youtube.com/watch?v=AJoGdJwocQ8" data-toggle="lightbox" data-type="youtube" data-title="Exams (1:50)">
+    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
 	<div class="tile">
 	<i class="fa fa-pencil"></i>
 	<p>Exams</p>
-    </div>
 	</div>
 	</div>
+    </a>
 
+    <a href="https://www.youtube.com/watch?v=AJoGdJwocQ8" data-toggle="lightbox" data-type="youtube" data-title="Results (2:50)">
     <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-	<div id="library">
 	<div class="tile">
-    <i class="fa fa-book"></i>
-	<p>Library</p>
+	<i class="fa fa-pencil"></i>
+	<p>Results</p>
 	</div>
 	</div>
-	</div>
+    </a>
 
+    <a href="https://www.youtube.com/embed/vSVTuCNUWXM" data-toggle="lightbox" data-type="youtube" data-title="Transport">
     <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div id="transport">
 	<div class="tile">
     <i class="fa fa-bus"></i>
 	<p>Transport</p>
     </div>
 	</div>
-	</div>
+	</a>
 
+    <a href="https://www.youtube.com/embed/Ntq0qCIr7Qo" data-toggle="lightbox" data-type="youtube" data-title="Library">
+    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
+	<div class="tile">
+    <i class="fa fa-book"></i>
+	<p>Library</p>
+	</div>
+	</div>
+    </a>
+
+    <a href="https://www.youtube.com/embed/pjyUITZScrU" data-toggle="lightbox" data-type="youtube" data-title="Calendar">
     <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div id="calendar1">
 	<div class="tile">
 	<i class="fa fa-calendar"></i>
 	<p>Calendar</p>
 	</div>
 	</div>
-	</div>
+	</a>
 
-    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-    <div id="events">
-	<div class="tile">
-	<i class="fa fa-ticket"></i>
-	<p>Events</p>
-	</div>
-	</div>
-	</div>
-
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div id="university-map">
+    <a href="https://www.youtube.com/embed/jWVqzzC1d3o" data-toggle="lightbox" data-type="youtube" data-title="University map">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
 	<div class="tile">
     <i class="fa fa-map-marker"></i>
 	<p>University Map</p>
 	</div>
 	</div>
-	</div>
+    </a>
 
+    <a href="https://www.youtube.com/embed/vnF_P613aPs" data-toggle="lightbox" data-type="youtube" data-title="Events">
     <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
-    <div id="feedback">
+	<div class="tile">
+	<i class="fa fa-ticket"></i>
+	<p>Events</p>
+	</div>
+	</div>
+    </a>
+
+    <a href="https://www.youtube.com/embed/_TVZ9sqyvho" data-toggle="lightbox" data-type="youtube" data-title="Feedback">
+    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
 	<div class="tile">
     <i class="fa fa-check-square-o"></i>
 	<p>Feedback</p>
 	</div>
 	</div>
-	</div>
+    </a>
 
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-    <div id="messenger">
+    <a href="https://www.youtube.com/embed/e2u27XFd36M" data-toggle="lightbox" data-type="youtube" data-title="Messenger">
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
 	<div class="tile">
     <i class="fa fa-comments"></i>
 	<p>Messenger</p>
 	</div>
 	</div>
-	</div>
+    </a>
 
-    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-	<div id="account">
+    <a href="https://www.youtube.com/watch?v=sH_HzsFCb1Y" data-toggle="lightbox" data-type="youtube" data-title="Account">
+    <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
 	<div class="tile">
     <i class="fa fa-user"></i>
 	<p>Account</p>
 	</div>
 	</div>
-	</div>
+    </a>
 	</div><!-- /.row -->
 
     </div><!-- /.container -->
