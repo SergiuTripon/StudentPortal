0774e38d8762622920113836335852fe2dbb32d8
triponsergiu <mexug0Aq>
2015-04-03 00:16:53 +0100

    Changed structure

--- calendar.php
+++ calendar.php
@@ -824,13 +824,7 @@ global $session_userid;
 
             $(".table-due-tasks").dataTable().fnDestroy();
             $('#content-due-tasks').empty();
-            $('#content-due-tasks').append(html.due_tasks);
-            $(".table-due-tasks").dataTable(settings);
 
-            $(".table-completed-tasks").dataTable().fnDestroy();
-            $('#content-completed-tasks').empty();
-            $('#content-completed-tasks').append(html.completed_tasks);
-            $(".table-completed-tasks").dataTable(settings);
 
             $('#complete-success-'+taskToComplete).modal('show');
 	},
