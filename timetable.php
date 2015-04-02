bf775959db7c1a9bc5cacaa6cd11195f7f6ed59a
triponsergiu <mexug0Aq>
2015-04-02 01:49:18 +0100

    Changed structure

--- timetable.php
+++ timetable.php
@@ -1016,162 +1016,14 @@ include 'includes/session.php';
     </div><!-- /panel-collapse -->
 	</div><!-- /panel-default -->
 
-	<div class="panel panel-default">
-
-    <div class="panel-heading" role="tab" id="headingOne">
-  	<h4 class="panel-title">
-	<a id="panel-monday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Monday</a>
-    </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-  	<div class="panel-body">
-
-	<!-- Monday -->
-	<section id="no-more-tables">
-	<table class="table table-condensed table-custom">
-
-	<thead>
-	<tr>
-	<th>Name</th>
-	<th>From</th>
-    <th>To</th>
-    <th>Location</th>
-	</tr>
-	</thead>
-
-	<tbody>
-	<?php
-	$stmt1 = $mysqli->query("SELECT l.lectureid, l.lecture_name, l.lecture_notes, m.module_url, l.lecture_day, DATE_FORMAT(l.lecture_from_time,'%H:%i') as lecture_from_time, DATE_FORMAT(l.lecture_to_time,'%H:%i') as lecture_to_time, l.lecture_location, l.lecture_capacity FROM system_lecture l LEFT JOIN user_lecture u ON l.lectureid = u.lectureid LEFT JOIN system_module m ON l.moduleid = m.moduleid WHERE l.lecture_status = 'active' AND l.lecture_lecturer = '$session_userid' AND l.lecture_day = 'Monday'");
-
-	while($row = $stmt1->fetch_assoc()) {
-
-    $lectureid = $row["lectureid"];
-    $lecture_name = $row["lecture_name"];
-    $lecture_notes= $row["lecture_notes"];
-    $module_url = $row["module_url"];
-    $lecture_day = $row["lecture_day"];
-	$lecture_from_time = $row["lecture_from_time"];
-	$lecture_to_time = $row["lecture_to_time"];
-	$lecture_location = $row["lecture_location"];
-    $lecture_capacity = $row["lecture_capacity"];
-
-	echo '<tr>
-
-			<td data-title="Name"><a href="#view-lecture-'.$lectureid.'" data-toggle="modal">'.$lecture_name.'</a></td>
-			<td data-title="From">'.$lecture_from_time.'</td>
-			<td data-title="To">'.$lecture_to_time.'</td>
-			<td data-title="Location">'.$lecture_location.'</td>
-			</tr>
-
-			<div id="view-lecture-'.$lectureid.'" class="modal fade modal-custom" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-            <div class="close"><i class="fa fa-clock-o"></i></div>
-            <h4 class="modal-title" id="modal-custom-label">'.$lecture_name.'</h4>
-			</div>
-
-			<div class="modal-body">
-			<p><b>Description:</b> '.(empty($lecture_notes) ? "No description" : "$lecture_notes").'</p>
-			<p><b>Day:</b> '.$lecture_day.'</p>
-			<p><b>From:</b> '.$lecture_from_time.'</p>
-			<p><b>To:</b> '.$lecture_to_time.'</p>
-			<p><b>Location:</b> '.$lecture_location.'</p>
-			<p><b>Capacity:</b> '.$lecture_capacity.'</p>
-			</div>
-
-			<div class="modal-footer">
-            <div class="view-action pull-left">
-            <a href="'.$module_url.'" class="btn btn-primary btn-sm ladda-button" data-style="slide-up">Moodle</a>
-            <a href="../feedback/" class="btn btn-primary btn-sm ladda-button" data-style="slide-up">Feedback</a>
-			</div>
-			<div class="view-close pull-right">
-			<a class="btn btn-danger btn-sm ladda-button" data-style="slide-up" data-dismiss="modal">Close</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->';
-	}
-
-	$stmt1->close();
-
-    $stmt2 = $mysqli->query("SELECT t.tutorialid, t.tutorial_name, t.tutorial_notes, m.module_url, t.tutorial_day, DATE_FORMAT(t.tutorial_from_time,'%H:%i') as tutorial_from_time, DATE_FORMAT(t.tutorial_to_time,'%H:%i') as tutorial_to_time, t.tutorial_location, t.tutorial_capacity FROM system_tutorial t LEFT JOIN user_tutorial u ON t.tutorialid = u.tutorialid LEFT JOIN system_module m ON t.moduleid = m.moduleid WHERE t.tutorial_status = 'active' AND t.tutorial_assistant = '$session_userid' AND t.tutorial_day = 'Monday'");
-
-	while($row = $stmt2->fetch_assoc()) {
-
-    $tutorialid = $row["tutorialid"];
-    $tutorial_name = $row["tutorial_name"];
-    $tutorial_notes = $row["tutorial_notes"];
-    $module_url = $row["module_url"];
-    $tutorial_day = $row["tutorial_day"];
-    $tutorial_from_time = $row["tutorial_from_time"];
-    $tutorial_to_time = $row["tutorial_to_time"];
-    $tutorial_location = $row["tutorial_location"];
-    $tutorial_capacity = $row["tutorial_capacity"];
-
-	echo '<tr>
-
-			<td data-title="Name"><a href="#view-tutorial-'.$tutorialid.'" data-toggle="modal">'.$tutorial_name.'</a></td>
-			<td data-title="From">'.$tutorial_from_time.'</td>
-			<td data-title="To">'.$tutorial_to_time.'</td>
-			<td data-title="Location">'.$tutorial_location.'</td>
-			</tr>
-
-			<div id="view-tutorial-'.$tutorialid.'" class="modal fade modal-custom" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-            <div class="close"><i class="fa fa-clock-o"></i></div>
-            <h4 class="modal-title" id="modal-custom-label">'.$tutorial_name.'</h4>
-			</div>
-
-			<div class="modal-body">
-			<p><b>Description:</b> '.(empty($tutorial_notes) ? "No description" : "$tutorial_notes").'</p>
-			<p><b>Day:</b> '.$tutorial_day.'</p>
-			<p><b>From:</b> '.$tutorial_from_time.'</p>
-			<p><b>To:</b> '.$tutorial_to_time.'</p>
-			<p><b>Location:</b> '.$tutorial_location.'</p>
-			<p><b>Capacity:</b> '.$tutorial_capacity.'</p>
-			</div>
-
-			<div class="modal-footer">
-            <div class="view-action pull-left">
-            <a href="'.$module_url.'" class="btn btn-primary btn-sm ladda-button" data-style="slide-up">Moodle</a>
-            <a href="../feedback/" class="btn btn-primary btn-sm ladda-button" data-style="slide-up">Feedback</a>
-			</div>
-			<div class="view-close pull-right">
-			<a class="btn btn-danger btn-sm ladda-button" data-style="slide-up" data-dismiss="modal">Close</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->';
-	}
-
-	$stmt2->close();
-	?>
-	</tbody>
-
-	</table>
-	</section>
-
-  	</div><!-- /panel-body -->
-    </div><!-- /panel-collapse -->
-	</div><!-- /panel-default -->
-
     <div class="panel panel-default">
 
-    <div class="panel-heading" role="tab" id="headingOne">
+    <div class="panel-heading" role="tab" id="headingTwo">
   	<h4 class="panel-title">
-	<a id="panel-tuesday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Tuesday</a>
+	<a id="panel-tuesday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> Tuesday</a>
     </h4>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
   	<div class="panel-body">
 
 	<!-- Tuesday -->
@@ -1314,12 +1166,12 @@ include 'includes/session.php';
 
     <div class="panel panel-default">
 
-    <div class="panel-heading" role="tab" id="headingOne">
+    <div class="panel-heading" role="tab" id="headingThree">
   	<h4 class="panel-title">
-	<a id="panel-wednesday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Wednesday</a>
+	<a id="panel-wednesday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree"> Wednesday</a>
     </h4>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
   	<div class="panel-body">
 
 	<!-- Wednesday -->
@@ -1462,12 +1314,12 @@ include 'includes/session.php';
 
     <div class="panel panel-default">
 
-    <div class="panel-heading" role="tab" id="headingOne">
+    <div class="panel-heading" role="tab" id="headingFour">
   	<h4 class="panel-title">
-	<a id="panel-thursday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Thursday</a>
+	<a id="panel-thursday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour"> Thursday</a>
     </h4>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
   	<div class="panel-body">
 
 	<!-- Thursday -->
@@ -1610,12 +1462,12 @@ include 'includes/session.php';
 
     <div class="panel panel-default">
 
-    <div class="panel-heading" role="tab" id="headingOne">
+    <div class="panel-heading" role="tab" id="headingFive">
   	<h4 class="panel-title">
-	<a id="panel-friday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Friday</a>
+	<a id="panel-friday" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive"> Friday</a>
     </h4>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
   	<div class="panel-body">
 
 	<!-- Friday -->
