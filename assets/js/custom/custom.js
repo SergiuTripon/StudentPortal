3071ccb3f6b3479abbdd7afc284766dfac0dfdec
triponsergiu <mexug0Aq>
2015-04-02 14:39:53 +0100

    Changed structure

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -2,11 +2,6 @@
 		$(".preloader").fadeOut("slow");
 	});
 
-    $('.btn').on('click', function () {
-        var $btn = $(this);
-        $btn.button('loading');
-    });
-
 	// Disables the background of a cell that contains Victoria in the Station Status table
 	$(".table-stationstatus td").filter(function() { return $.trim($(this).text()) === "Victoria"; }).
 	closest('tr').addClass( "no-background" );
