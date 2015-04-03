92c6881c2e4a4aede4241e7f7b9a8b95204a4899
triponsergiu <mexug0Aq>
2015-04-03 11:48:45 +0100

    Changed structure

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ $task_status = 'active';
 $stmt1 = $mysqli->prepare("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate FROM user_task WHERE userid=? AND task_status=?");
 $stmt1->bind_param('is', $session_userid, $task_status);
 $stmt1->execute();
-$stmt1->bind_result($taskid, $task_name, $task_url, $task_startdate, $task_duedate);
+$stmt1->bind_result($taskid, $task_name, $task_notes, $task_url, $task_startdate, $task_duedate);
 $stmt1->store_result();
 
 if ($stmt1->num_rows > 0) {
