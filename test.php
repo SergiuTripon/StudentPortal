30ae63cd95d1b91b09d12167c036cee33232c1f9
triponsergiu <mexug0Aq>
2015-04-04 00:25:11 +0100

    Changed structure

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $isUpdate = '0';
 calendarUpdate1($isUpdate);
 
 function calendarUpdate1($isUpdate) {
-    if ($isUpdate = 1) {
+    if ($isUpdate === 1) {
         echo 'Parameter is 1';
     } else {
         echo 'Parameter is 0';
