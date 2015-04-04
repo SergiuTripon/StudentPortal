3d4f7b0bece82824e12a6512d1a614402b1e03dc
triponsergiu <mexug0Aq>
2015-04-04 02:08:10 +0100

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -2530,7 +2530,7 @@ function calendarUpdate($isUpdate = 0) {
         <td data-title="Action">
 
         <div class="btn-group btn-action">
-        <a id="complete-button" class="btn btn-primary" href="#complete-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Complete</a>
+        <a id="complete-'.$taskid.'" class="btn btn-primary btn-complete">Complete</a>
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
         <span class="fa fa-caret-down"></span>
         <span class="sr-only">Toggle Dropdown</span>
@@ -2572,29 +2572,6 @@ function calendarUpdate($isUpdate = 0) {
         </div><!--/modal -->
         </div><!--/modal-dialog-->
         </div><!--/modal-content-->
-
-        <div id="complete-'.$taskid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-
-        <div class="modal-header">
-        <div class="close" data-dismiss="modal"><i class="fa fa-times"></i></div>
-        <h4 class="modal-title" id="modal-custom-label">Complete task?</h4>
-        </div>
-
-        <div class="modal-body">
-        <p class="confirmation-default text-left">Are you sure you want to complete "'.$task_name.'"?</p></div>
-
-        <div class="modal-footer">
-        <div class="text-right">
-        <a class="btn btn-confirmation-cancel btn-lg" data-dismiss="modal">Cancel</a>
-        <a id="complete-'.$taskid.'" class="btn btn-confirmation-confirm btn-lg btn-complete">Confirm</a>
-        </div>
-        </div>
-
-        </div><!-- /modal -->
-        </div><!-- /modal-dialog -->
-        </div><!-- /modal-content -->
 
         <div id="deactivate-'.$taskid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
         <div class="modal-dialog">
