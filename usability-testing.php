495adbb2c6c6311cde12ec9865d9a503ef3e44bf
triponsergiu <mexug0Aq>
2015-04-04 22:15:25 +0100

    Changed structure

--- usability-testing.php
+++ usability-testing.php
@@ -162,7 +162,7 @@ if (isset($_GET["video"])) {
     <div class="row">
     <div class="row">
     <a href="https://www.youtube.com/watch?v=AJoGdJwocQ8" data-toggle="lightbox" data-type="youtube" data-title="Timetable (0:00)" id="video-timetable">
-    <div class="col-xs-6 col-sm-3 col-md-4 col-lg-4">
+    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
 	<div class="tile">
     <i class="fa fa-clock-o"></i>
 	<p>Timetable</p>
