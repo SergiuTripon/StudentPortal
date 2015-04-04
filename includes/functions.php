a82884aab23da7161a0e6dd44438da4449180ed8
triponsergiu <mexug0Aq>
2015-04-04 02:15:23 +0100

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -2541,6 +2541,7 @@ function calendarUpdate($isUpdate = 0) {
         <li><a href="#delete-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Delete</a></li>
         </ul>
         </div>
+
         <div id="view-'.$taskid.'" class="modal fade modal-custom" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
         <div class="modal-dialog">
         <div class="modal-content">
@@ -2621,7 +2622,19 @@ function calendarUpdate($isUpdate = 0) {
         <td data-title="Start">'.$task_startdate.'</td>
         <td data-title="Due">'.$task_duedate.'</td>
         <td data-title="Completed on">'.$task_duedate.'</td>
-        <td data-title="Action"><a class="btn btn-primary btn-md" href="#delete-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Delete</a>
+        <td data-title="Action">
+
+        <div class="btn-group btn-action">
+        <a id="reactivate-'.$taskid.'" class="btn btn-primary btn-reactivate">Restore</a>
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+        <span class="fa fa-caret-down"></span>
+        <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+        <li><a href="#delete-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Delete</a></li>
+        </ul>
+        </div>
+
         <div id="view-'.$taskid.'" class="modal fade modal-custom" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
         <div class="modal-dialog">
         <div class="modal-content">
@@ -2704,8 +2717,9 @@ function calendarUpdate($isUpdate = 0) {
         <td data-title="Due date">'.$task_duedate.'</td>
         <td data-title="Archived on">'.$updated_on.'</td>
         <td data-title="Action">
+
         <div class="btn-group btn-action">
-        <a class="btn btn-primary" href="#reactivate-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Restore</a>
+        <a id="reactivate-'.$taskid.'" class="btn btn-primary btn-reactivate">Restore</a>
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
         <span class="fa fa-caret-down"></span>
         <span class="sr-only">Toggle Dropdown</span>
@@ -2714,6 +2728,7 @@ function calendarUpdate($isUpdate = 0) {
         <li><a href="#delete-'.$taskid.'" data-toggle="modal" data-dismiss="modal">Delete</a></li>
         </ul>
         </div>
+
         <div id="view-'.$taskid.'" class="modal fade modal-custom" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
         <div class="modal-dialog">
         <div class="modal-content">
@@ -2738,30 +2753,6 @@ function calendarUpdate($isUpdate = 0) {
         </div>
         <div class="view-close pull-right">
         <a class="btn btn-danger btn-md" data-dismiss="modal">Close</a>
-        </div>
-        </div>
-
-        </div><!-- /modal -->
-        </div><!-- /modal-dialog -->
-        </div><!-- /modal-content -->
-
-        <div id="reactivate-'.$taskid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-
-        <div class="modal-header">
-        <div class="close" data-dismiss="modal"><i class="fa fa-times"></i></div>
-        <h4 class="modal-title" id="modal-custom-label">Restore task?</h4>
-        </div>
-
-        <div class="modal-body">
-        <p class="confirmation-default text-center">Are you sure you want to restore "'.$task_name.'"?</p>
-        </div>
-
-        <div class="modal-footer">
-        <div class="text-right">
-        <a class="btn btn-confirmation-cancel btn-lg" data-dismiss="modal">Cancel</a>
-        <a id="reactivate-'.$taskid.'" class="btn btn-confirmation-confirm btn-lg btn-reactivate">Confirm</a>
         </div>
         </div>
 
