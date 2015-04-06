1cae9c53b243985ad4ddd87ff09471fb19e90cd5
triponsergiu <mexug0Aq>
2015-04-07 00:29:36 +0100

    Changed structure

--- test.php
+++ test.php
@@ -3,3 +3,6 @@ include 'includes/session.php';
 include 'includes/functions.php';
 
 AdminTimetableUpdate($isIpdate = 1, $userid = '3');
+
+echo $array['active_result'];
+echo $array['inactive_result'];
