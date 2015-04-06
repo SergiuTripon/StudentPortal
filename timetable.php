b08bce3d27fa8e7cfb6a49813801dc050622e990
triponsergiu <mexug0Aq>
2015-04-06 20:17:58 +0100

    Changed structure

--- timetable.php
+++ timetable.php
@@ -2210,6 +2210,26 @@ global $inactive_tutorial;
         $('#content-inactive-module').html(html.inactive_module);
         $(".table-inactive-module").dataTable(admin_settings);
 
+        $(".table-inactive-tutorial").dataTable().fnDestroy();
+        $('#content-inactive-tutorial').empty();
+        $('#content-inactive-tutorial').html(html.inactive_tutorial);
+        $(".table-inactive-tutorial").dataTable(admin_settings);
+
+        $(".table-active-tutorial").dataTable().fnDestroy();
+        $('#content-active-tutorial').empty();
+        $('#content-active-tutorial').html(html.active_tutorial);
+        $(".table-active-tutorial").dataTable(admin_settings);
+
+        $(".table-inactive-lecture").dataTable().fnDestroy();
+        $('#content-inactive-lecture').empty();
+        $('#content-inactive-lecture').html(html.inactive_lecture);
+        $(".table-inactive-lecture").dataTable(admin_settings);
+
+        $(".table-active-lecture").dataTable().fnDestroy();
+        $('#content-active-lecture').empty();
+        $('#content-active-lecture').html(html.active_lecture);
+        $(".table-active-lecture").dataTable(admin_settings);
+
         $(".table-active-module").dataTable().fnDestroy();
         $('#content-active-module').empty();
         $('#content-active-module').html(html.active_module);
