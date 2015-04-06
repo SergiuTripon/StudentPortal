d26199b073f58d0653ce005ac858d04f0ecb9d3d
triponsergiu <mexug0Aq>
2015-04-06 21:43:38 +0100

    Changed structure

--- admin/select-module.php
+++ admin/select-module.php
@@ -2,10 +2,14 @@
 include '../includes/session.php';
 
 global $userToCreateResults;
+global $active_result;
+global $inactive_result;
+
 
 if (isset($_GET['id'])) {
-
     $userToCreateResults = $_GET['id'];
+
+    AdminTimetableUpdate();
 
 } else {
     header('Location: ../../results/');
@@ -118,106 +122,7 @@ if (isset($_GET['id'])) {
 
 	<tbody>
     <?php
-
-	$stmt1 = $mysqli->query("SELECT user_result.resultid, system_module.module_name, user_result.result_coursework_mark, user_result.result_exam_mark, user_result.result_overall_mark FROM user_result LEFT JOIN system_module ON user_result.moduleid=system_module.moduleid WHERE user_result.userid = '$userToCreateResults' AND user_result.result_status='active'");
-
-	while($row = $stmt1->fetch_assoc()) {
-
-	$resultid = $row["resultid"];
-    $module_name = $row["module_name"];
-    $result_coursework_mark = $row["result_coursework_mark"];
-    $result_exam_mark = $row["result_exam_mark"];
-    $result_overall_mark = $row["result_overall_mark"];
-
-	echo '<tr id="result-'.$resultid.'">
-
-			<td data-title="Name">'.$module_name.'</td>
-			<td data-title="Coursework mark">'.$result_coursework_mark.'</td>
-			<td data-title="Exam mark">'.$result_exam_mark.'</td>
-			<td data-title="Overall mark">'.$result_overall_mark.'</td>
-			<td data-title="Action">
-
-			<div class="btn-group btn-action">
-            <a class="btn btn-primary" href="../update-result/?id='.$resultid.'">Update</a>
-            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            <span class="fa fa-caret-down"></span>
-            <span class="sr-only">Toggle Dropdown</span>
-            </button>
-            <ul class="dropdown-menu" role="menu">
-            <li><a href="#deactivate-'.$resultid.'" data-toggle="modal" data-toggle="modal" data-toggle="modal">Deactivate</a></li>
-            <li><a href="#delete-'.$resultid.'" data-toggle="modal" data-toggle="modal" data-toggle="modal">Delete</a></li>
-            </ul>
-            </div>
-            </td>
-			</tr>
-
-			<div class="modal modal-custom fade" id="deactivate-'.$resultid.'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-			<div class="form-logo text-center">
-			<i class="fa fa-minus-square-o"></i>
-			</div>
-			</div>
-
-			<div class="modal-body">
-			<p id="deactivate-question" class="text-center feedback-sad">Are you sure you want to deactivate this result for '.$module_name.'?</p>
-            <p id="deactivate-confirmation" style="display: none;" class="text-center feedback-happy">The result for '.$module_name.' has been deactivated successfully.</p>
-			</div>
-
-			<div class="modal-footer">
-			<div id="deactivate-hide">
-			<div class="pull-left">
-			<a id="deactivate-'.$resultid.'" class="btn btn-danger btn-lg deactivate-button" >Yes</a>
-			</div>
-			<div class="text-right">
-			<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">No</button>
-			</div>
-			</div>
-			<div class="text-center">
-			<a id="deactivate-success-button" class="btn btn-primary btn-lg" style="display: none;" >Continue</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->
-
-            <div class="modal modal-custom fade" id="delete-'.$resultid.'" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-			<div class="form-logo text-center">
-			<i class="fa fa-trash"></i>
-			</div>
-			</div>
-
-			<div class="modal-body">
-			<p id="delete-question" class="text-center feedback-sad">Are you sure you want to delete this result for '.$module_name.'?</p>
-            <p id="delete-confirmation" style="display: none;" class="text-center feedback-happy">The result for '.$module_name.' has been deleted successfully.</p>
-			</div>
-
-			<div class="modal-footer">
-			<div id="delete-hide">
-			<div class="pull-left">
-			<a id="delete-'.$resultid.'" class="btn btn-danger btn-lg delete-button" >Yes</a>
-			</div>
-			<div class="text-right">
-			<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">No</button>
-			</div>
-			</div>
-			<div class="text-center">
-			<a id="delete-success-button" class="btn btn-primary btn-lg" style="display: none;" >Continue</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->';
-    }
-	$stmt1->close();
+    echo $active_result;
 	?>
 	</tbody>
 
