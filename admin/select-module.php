a7f1a1bc24d35949d0e8337b242ea49423ff49c9
triponsergiu <mexug0Aq>
2015-04-07 00:22:13 +0100

    Changed structure

--- admin/select-module.php
+++ admin/select-module.php
@@ -237,7 +237,7 @@ if (isset($_GET['id'])) {
 
     var clickedID = this.id.split('-');
     var resultToDeactivate = clickedID[1];
-    var userToCreateResult = $('userid').html();
+    var userToCreateResult = $('userid').val();
 
     alert(userToCreateResult);
 
