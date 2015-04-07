07fc8c7b7aeb50b806ba764031363a6e859f4587
triponsergiu <mexug0Aq>
2015-04-07 16:55:08 +0100

    Changed structure

--- library.php
+++ library.php
@@ -1138,7 +1138,10 @@ AdminLibraryUpdate();
 	<!-- Sign Out (Inactive) JS -->
     <script src="https://student-portal.co.uk/assets/js/custom/sign-out-inactive.js"></script>
 
-    	<script>
+    <?php include 'assets/js-paths/common-js-paths.php'; ?>
+    <?php include 'assets/js-paths/datatables-js-path.php'; ?>
+
+    <script>
 	//DataTables
 
     var settings = {
