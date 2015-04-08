68028bc7ce19b9dc394a29538b8f197e026a9d55
triponsergiu <mexug0Aq>
2015-04-08 21:27:29 +0100

    Changed structure

--- calendar.php
+++ calendar.php
@@ -286,7 +286,7 @@ global $archived_task;
     <div class="modal-footer">
     <div class="text-right">
     <a class="btn btn-danger btn-lg" data-dismiss="modal">Cancel</a>
-    <a id="FormSubmit" class="btn btn-primary btn-lg btn-perform">Create task</a>
+    <a id="FormSubmit" class="btn btn-primary btn-lg btn-load">Create task</a>
     </div>
     </div>
 
