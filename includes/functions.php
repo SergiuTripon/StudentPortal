33705df5081e16977e84a4a3de01136c1a3448b4
triponsergiu <mexug0Aq>
2015-04-08 23:19:59 +0100

    Changed structure

--- includes/functions.php
+++ includes/functions.php
@@ -3753,7 +3753,7 @@ function calendarUpdate($isUpdate = 0) {
 
     $task_status = 'active';
 
-    $stmt1 = $mysqli->prepare("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d %b %y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d %b %y %H:%i') as task_duedate FROM user_task WHERE userid=? AND task_status=?");
+    $stmt1 = $mysqli->prepare("SELECT taskid, task_name, task_notes, task_url, DATE_FORMAT(task_startdate,'%d/%m/%Y %H:%i') as task_startdate, DATE_FORMAT(task_duedate,'%d/%m/%Y %H:%i') as task_duedate FROM user_task WHERE userid=? AND task_status=?");
     $stmt1->bind_param('is', $session_userid, $task_status);
     $stmt1->execute();
     $stmt1->bind_result($taskid, $task_name, $task_notes, $task_url, $task_startdate, $task_duedate);
