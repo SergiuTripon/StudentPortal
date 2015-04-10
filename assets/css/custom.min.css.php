7c1fb3b1298d6230c9fbb7b124cae0a8c579ed48
triponsergiu <mexug0Aq>
2015-04-10 15:16:14 +0100

    Changed structure

--- assets/css/custom.min.css.php
+++ assets/css/custom.min.css.php
@@ -14,14 +14,14 @@ function compress($minify)
 }
 
 /* css files for combining */
-include('bootstrap/bootstrap.min.css');
-include('calendar/calendar.min.css');
-include('custom/custom.css');
-include('data-tables/dataTables.bootstrap.css');
-include('data-tables/dataTables.fontAwesome.css');
-include('date-time-picker/bootstrap-datetimepicker.min.css');
-include('ekko-lightbox/ekko-lightbox.min.css');
-include('font-awesome/font-awesome.min.css');
-include('select2/select2.min.css');
+include('bootstrap.min.css');
+include('calendar.min.css');
+include('custom.css');
+include('dataTables.bootstrap.css');
+include('dataTables.fontAwesome.css');
+include('bootstrap-datetimepicker.min.css');
+include('ekko-lightbox.min.css');
+include('font-awesome.min.css');
+include('select2.min.css');
 
 ob_end_flush();
