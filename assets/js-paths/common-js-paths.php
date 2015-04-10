673a5f5463eea937bcea734f9e46147bad5098c6
triponsergiu <mexug0Aq>
2015-04-10 20:20:49 +0100

    Changed structure

--- assets/js-paths/common-js-paths.php
+++ assets/js-paths/common-js-paths.php
@@ -1,15 +1,2 @@
-    <!-- js library -->
-    <script src="https://student-portal.co.uk/assets/js/jquery/jquery-latest.min.js"></script>
-
-    <!-- bootstrap -->
-    <script src="https://student-portal.co.uk/assets/js/bootstrap/bootstrap.min.js"></script>
-
-    <!-- ladda -->
-    <script src="https://student-portal.co.uk/assets/js/ladda/spin.min.js"></script>
-    <script src="https://student-portal.co.uk/assets/js/ladda/ladda.min.js"></script>
-
-    <!-- custom -->
-    <script src="https://student-portal.co.uk/assets/js/custom/custom.js"></script>
-
-    <!-- common-->
-    <script src="https://student-portal.co.uk/assets/js/common/ie10-viewport-bug-workaround.js"></script>
+    <!-- everything -->
+    <script src="https://student-portal.co.uk/assets/js/custom.min.js.php"></script>
