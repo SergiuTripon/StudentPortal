25a4172c935691c806abf56239c564278df05242
triponsergiu <mexug0Aq>
2015-04-11 12:29:46 +0100

    Changed structure

--- assets/css-paths/common-css-paths.php
+++ assets/css-paths/common-css-paths.php
@@ -1,5 +1,5 @@
     <!-- favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico">
+    <link type="image/x-icon" href="https://student-portal.co.uk/assets/img/favicon/favicon.ico" rel="shortcut icon" property="shortcut icon">
 
     <!-- open-sans-font -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" property="stylesheet">
