beee8b0e8cabbcb73d48f34d77e3c0db25c1dde6
triponsergiu <mexug0Aq>
2015-04-12 17:18:49 +0100

    Changed structure

--- university-map/location-overview.php
+++ university-map/location-overview.php
@@ -20,7 +20,7 @@ include '../includes/session.php';
 <body>
 <div class="preloader"></div>
 
-    <?php if (isset($_SESSION['signedIn']) && $_SESSION['signedIn'] == true) : ?>
+    <?php if (isset($_SESSION['account_type']) && ($_SESSION['account_type'] == 'student' || $_SESSION['account_type'] == 'academic staff')) : ?>
 
     <?php include '../includes/menus/portal_menu.php'; ?>
 
