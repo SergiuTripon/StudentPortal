4e1b3e44b07da8f0c623736dae50cf0a4dac8fc9
triponsergiu <mexug0Aq>
2015-04-12 17:28:57 +0100

    Changed structure

--- admin/update-result.php
+++ admin/update-result.php
@@ -35,8 +35,8 @@ if (isset($_GET['id'])) {
 	<div class="preloader"></div>
 
 	<?php if (isset($_SESSION['signedIn']) && $_SESSION['signedIn'] == true) : ?>
-	
-    <?php if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'administrator') : ?>
+
+    <?php if (isset($_SESSION['account_type']) && ($_SESSION['account_type'] == 'academic staff' || $_SESSION['account_type'] == 'administrator')) : ?>
 
 	<?php include '../includes/menus/portal_menu.php'; ?>
 
