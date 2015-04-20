262d10027f2a608996cf7f01c554bb10bae9e125
triponsergiu <mexug0Aq>
2015-04-20 14:59:29 +0100

    Changed structure

--- admin/allocate-exam.php
+++ admin/allocate-exam.php
@@ -76,45 +76,12 @@ if (isset($_GET['id'])) {
     $firstname = $row["firstname"];
     $surname = $row["surname"];
 
-	echo '<tr id="user-'.$userid.'">
+	echo '<tr>
 
 			<td data-title="Full name">'.$firstname.' '.$surname.'</td>
 			<td data-title="Student number">'.$studentno.'</td>
-			<td data-title="Action"><a class="btn btn-primary btn-md" href="#allocate-'.$userid.'" data-toggle="modal" >Allocate</span></a></td>
-			</tr>
-
-			<div id="allocate-'.$userid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-			<div class="form-logo text-center">
-			<i class="fa fa-user-plus"></i>
-			</div>
-			</div>
-
-			<div class="modal-body">
-			<p id="allocate-question" class="text-center feedback-sad">Are you sure you want to allocate '.$firstname.' '.$surname.' to this module?</p>
-            <p id="allocate-confirmation" style="display: none;" class="text-center feedback-happy">'.$firstname.' '.$surname.' has been allocated to this module successfully.</p>
-			</div>
-
-			<div class="modal-footer">
-			<div id="allocate-hide">
-			<div class="pull-left">
-			<a id="allocate-'.$userid.'" class="btn btn-danger btn-lg allocate-button" >Yes</a>
-			</div>
-			<div class="text-right">
-			<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">No</button>
-			</div>
-			</div>
-			<div class="text-center">
-			<a id="allocate-success-button" class="btn btn-primary btn-lg" style="display: none;" >Continue</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->';
+			<td data-title="Action"><a id="#allocate-'.$userid.'" class="btn btn-primary btn-md btn-allocate-exam">Allocate</a></td>
+			</tr>';
     }
 	$stmt1->close();
 	?>
@@ -161,45 +128,12 @@ if (isset($_GET['id'])) {
     $firstname = $row["firstname"];
     $surname = $row["surname"];
 
-	echo '<tr id="user-'.$userid.'">
+	echo '<tr>
 
 			<td data-title="First name">'.$firstname.' '.$surname.'</td>
 			<td data-title="Student number">'.$studentno.'</td>
-			<td data-title="Action"><a class="btn btn-primary btn-md" href="#deallocate-'.$userid.'" data-toggle="modal" >Deallocate</span></a></td>
-			</tr>
-
-			<div id="deallocate-'.$userid.'" class="modal fade modal-custom" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
-    		<div class="modal-dialog">
-    		<div class="modal-content">
-
-			<div class="modal-header">
-			<div class="form-logo text-center">
-			<i class="fa fa-user-times"></i>
-			</div>
-			</div>
-
-			<div class="modal-body">
-			<p id="deallocate-question" class="text-center feedback-sad">Are you sure you want to deallocate '.$firstname.' '.$surname.' to this module?</p>
-            <p id="deallocate-confirmation" style="display: none;" class="text-center feedback-happy">'.$firstname.' '.$surname.' has been deallocated from this module successfully.</p>
-			</div>
-
-			<div class="modal-footer">
-			<div id="deallocate-hide">
-			<div class="pull-left">
-			<a id="deallocate-'.$userid.'" class="btn btn-danger btn-lg deallocate-button" >Yes</a>
-			</div>
-			<div class="text-right">
-			<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">No</button>
-			</div>
-			</div>
-			<div class="text-center">
-			<a id="deallocate-success-button" class="btn btn-primary btn-lg" style="display: none;" >Continue</a>
-			</div>
-			</div>
-
-			</div><!-- /modal -->
-			</div><!-- /modal-dialog -->
-			</div><!-- /modal-content -->';
+			<td data-title="Action"><a id="#deallocate-'.$userid.'" class="btn btn-primary btn-md btn-deallocate-exam btn-load">Deallocate</a></td>
+			</tr>';
     }
 	$stmt2->close();
 	?>
@@ -254,23 +188,21 @@ if (isset($_GET['id'])) {
 	<?php include '../assets/js-paths/common-js-paths.php'; ?>
 
 	<script>
-
-
-
-
     //DataTables
-    $('.table-custom').dataTable({
+    var settings = {
         "iDisplayLength": 10,
-		"paging": true,
-		"ordering": true,
-		"info": false,
-		"language": {
-			"emptyTable": "There are no records to display."
-		}
-	});
+        "paging": true,
+        "ordering": true,
+        "info": false,
+        "language": {
+            "emptyTable": "There are no records to display."
+        }
+    };
+
+    $('.table-custom').dataTable(settings);
 
     //Allocate module
-	$("body").on("click", ".allocate-button", function(e) {
+	$("body").on("click", ".btn-allocate-exam", function(e) {
     e.preventDefault();
 
     var clickedID = this.id.split('-');
@@ -283,16 +215,7 @@ if (isset($_GET['id'])) {
 	dataType:"text",
 	data:'userToAllocate='+ userToAllocate + '&examToAllocate='+ examToAllocate,
 	success:function(){
-        $('#user-'+userToAllocate).hide();
-        $('.form-logo i').removeClass('fa-user-plus');
-        $('.form-logo i').addClass('fa-check-square-o');
-        $('#allocate-question').hide();
-        $('#allocate-confirmation').show();
-        $('#allocate-hide').hide();
-        $('#allocate-success-button').show();
-        $("#allocate-success-button").click(function () {
             location.reload();
-        });
     },
 	error:function (xhr, ajaxOptions, thrownError){
 		$("#error").show();
@@ -304,7 +227,7 @@ if (isset($_GET['id'])) {
     });
 
     //Deallocate module
-    $("body").on("click", ".deallocate-button", function(e) {
+    $("body").on("click", ".btn-deallocate-exam", function(e) {
     e.preventDefault();
 
     var clickedID = this.id.split('-');
@@ -317,16 +240,7 @@ if (isset($_GET['id'])) {
 	dataType:"text",
 	data:'userToDeallocate='+ userToDeallocate + '&examToDeallocate='+ examToDeallocate,
 	success:function(){
-        $('#user-'+userToDeallocate).hide();
-        $('.form-logo i').removeClass('fa-user-times');
-        $('.form-logo i').addClass('fa-check-square-o');
-        $('#deallocate-question').hide();
-        $('#deallocate-confirmation').show();
-        $('#deallocate-hide').hide();
-        $('#deallocate-success-button').show();
-        $("#deallocate-success-button").click(function () {
             location.reload();
-        });
     },
 	error:function (xhr, ajaxOptions, thrownError){
 		$("#error").show();
