c05b640a617c82a94fd38eccf5c5f692f595581b
triponsergiu <mexug0Aq>
2015-04-20 15:14:26 +0100

    Changed structure

--- admin/allocate-tutorial.php
+++ admin/allocate-tutorial.php
@@ -237,7 +237,7 @@ if (isset($_GET['id'])) {
 	data:'userToDeallocate='+ userToDeallocate + '&tutorialToDeallocate='+ tutorialToDeallocate,
 	success:function(html){
         if (html) {
-
+            alert(html);
         } else {
             location.reload();
         }
