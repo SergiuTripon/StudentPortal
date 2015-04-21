e49c648ed9dfc39f7da81dcb58f21c137d61942e
triponsergiu <mexug0Aq>
2015-04-21 15:51:49 +0100

    Changed structure

--- features.php
+++ features.php
@@ -161,9 +161,71 @@ include 'includes/session.php';
 	<!-- End of Feature Modal -->
 
     <?php include 'includes/footers/footer.php'; ?>
+    <?php include 'assets/js-paths/common-js-paths.php'; ?>
 
+	<script>
 
-
+    $("#timetable").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Timetable");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-table\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">No one likes 9am lectures, especially when hungover from the night before. An easily accessible timetable will help you organise yourself better as you roll out of bed in the morning‏.</p>");
+    });
+    $("#exams").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Exams");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-pencil\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">The word that every student hates. But this feature will make your life a little less stressful by helping you get to your exams on time and get the best results. Revision's all on you though.</p>");
+    });
+    $("#library").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Library");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-book\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">Feeling like a couch potato and don't want to go to the library? No sweat, just log on, have a browse and reserve what you fancy, or renew something that you already have on loan‏.</p>");
+    });
+    $("#transport").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Transport");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-subway\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">So you get to the tube station and your line is delayed. We've all been there. Now you can check transport updates from our web app without the hassle of going on the TfL website‏.</p>");
+    });
+    $("#calendar1").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Calendar");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-calendar\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">Your own personal secretary who sits in your back pocket. They'll nag you about what you need to do and when.</p>");
+    });
+    $("#events").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Events");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-beer\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">Let's be honest. We all say at the beginning of the year that we'll go to events but we never do. Here's your chance to get involved and have some fun. It's in your hands‏.</p>");
+    });
+    $("#university-map").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("University Map");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-map-marker\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">That moment of panic when you have to go to a new classroom that you've never heard of. Admit it, it's not just freshers who get lost. But fear not, with the web app you'll have your own portable TomTom&#8482;‏.</p>");
+    });
+    $("#feedback").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Feedback");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-check-square-o\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">Everyone dreads their lecturer handing them the paper-based Student Survey. Let's not kill anymore trees. Just keep calm and log on to provide your feedback. Your lecturer will even be able to contact you in person to discuss any concerns.</p>");
+    });
+    $("#messenger").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Messenger");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-comments\"></i>");
+        $('.modal-body').empty().append("<p class=\"feedback-custom text-justify\">So your lecturers office is on the fourth floor and there's no lift. Why bother climbing the stairs or opening your email when you can just send them a quick message? Simples.</p>");
+    });
+    $("#account").click(function() {
+        $('.modal-custom').modal('show');
+        $('.modal-custom #modal-custom-label').empty().append("Account");
+        $('.modal-custom .close').empty().append("<i class=\"fa fa-user\"></i>");
+        $('.modal-custom .modal-body').empty().append("<p class=\"feedback-custom text-justify\">There are some things in life you can't control, but you can have full control of your account right here. See what it feels like to be the boss.</p>");
+    });
+    </script>
 
 	<?php else : ?>
 
@@ -295,12 +357,7 @@ include 'includes/session.php';
 	<!-- End of Feature Modal -->
 
     <?php include 'includes/footers/footer.php'; ?>
-
-	<?php endif; ?>
-
     <?php include 'assets/js-paths/common-js-paths.php'; ?>
-
-
 
 	<script>
     $(document).ready(function() {
@@ -368,6 +425,8 @@ include 'includes/session.php';
 
     });
     </script>
+
+	<?php endif; ?>
 
 </body>
 </html>
