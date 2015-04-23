50a5c718493aa6e758dadbb503a998815a72b6c3
triponsergiu <mexug0Aq>
2015-04-23 22:25:44 +0100

    Changed structure

--- account.php
+++ account.php
@@ -415,7 +415,7 @@ global $mysqli;
 			</div><!-- /modal-dialog -->
 			</div><!-- /modal-content -->
 
-			<div id="delete-'.$userid.'" class="modal fade modal-custom modal-info" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
+			<div id="delete-'.$userid.'" class="modal fade modal-custom modal-warning" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modal-custom-label" aria-hidden="true">
     		<div class="modal-dialog">
     		<div class="modal-content">
 
