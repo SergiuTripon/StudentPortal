7749a283027616c8c528dceeb04692531ad8098b
triponsergiu <mexug0Aq>
2015-04-23 12:07:31 +0100

    Changed structure

--- admin/allocate-lecture.php
+++ admin/allocate-lecture.php
@@ -164,81 +164,11 @@ if (isset($_GET['id'])) {
     </div><!-- /container -->
 
 	<?php include '../includes/footers/footer.php'; ?>
-
-    <?php else : ?>
-
-	<?php include '../includes/menus/portal_menu.php'; ?>
-
-    <div class="container">
-
-    <form class="form-horizontal form-custom">
-
-	<div class="form-logo text-center">
-    <i class="fa fa-graduation-cap"></i>
-    </div>
-
-    <hr>
-	<p class="feedback-danger text-center">You need to have an admin account to access this area.</p>
-    <hr>
-
-    <div class="text-center">
-    <a class="btn btn-primary btn-lg btn-load" href="/home/">Home</a>
-    </div>
-
-    </form>
-
-	</div>
-
-	<?php include '../includes/footers/footer.php'; ?>
     <?php include '../assets/js-paths/common-js-paths.php'; ?>
-
-    <?php endif; ?>
-
-	<?php else : ?>
-
-	<?php include '../includes/menus/menu.php'; ?>
-
-    <div class="container">
-
-	<form class="form-horizontal form-custom">
-
-    <div class="form-logo text-center">
-    <i class="fa fa-graduation-cap"></i>
-    </div>
-
-    <hr>
-
-    <p class="feedback-danger text-center">Looks like you're not signed in yet. Please Sign in before accessing this area.</p>
-
-    <hr>
-
-    <div class="text-center">
-	<a class="btn btn-primary btn-lg" href="/">Sign in</a>
-    </div>
-
-    </form>
-
-	</div>
-
-	<?php include '../includes/footers/footer.php'; ?>
-
-	<?php endif; ?>
-
-	<?php include '../assets/js-paths/common-js-paths.php'; ?>
 
 	<script>
 
     //DataTables
-    var settings= {
-        "iDisplayLength": 10,
-        "paging": true,
-        "ordering": true,
-        "info": false,
-        "language": {
-            "emptyTable": "There are no users to display."
-        }
-    };
-
     $('.table-custom').dataTable(settings);
 
     //Allocate module
@@ -288,6 +218,66 @@ if (isset($_GET['id'])) {
 	});
     });
 	</script>
+
+    <?php else : ?>
+
+	<?php include '../includes/menus/portal_menu.php'; ?>
+
+    <div class="container">
+
+    <form class="form-horizontal form-custom">
+
+	<div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+	<p class="feedback-danger text-center">You need to have an admin account to access this area.</p>
+    <hr>
+
+    <div class="text-center">
+    <a class="btn btn-primary btn-lg btn-load" href="/home/">Home</a>
+    </div>
+
+    </form>
+
+	</div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+    <?php include '../assets/js-paths/common-js-paths.php'; ?>
+
+    <?php endif; ?>
+
+	<?php else : ?>
+
+	<?php include '../includes/menus/menu.php'; ?>
+
+    <div class="container">
+
+	<form class="form-horizontal form-custom">
+
+    <div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+
+    <p class="feedback-danger text-center">Looks like you're not signed in yet. Please Sign in before accessing this area.</p>
+
+    <hr>
+
+    <div class="text-center">
+	<a class="btn btn-primary btn-lg btn-load" href="/">Sign in</a>
+    </div>
+
+    </form>
+
+	</div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+    <?php include '../assets/js-paths/common-js-paths.php'; ?>
+
+    <?php endif; ?>
 
 </body>
 </html>
