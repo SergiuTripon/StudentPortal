3ac9f12d7544571557b5639cb8eb74d19a9b136a
triponsergiu <mexug0Aq>
2015-04-23 11:02:57 +0100

    Changed structure

--- admin/create-module.php
+++ admin/create-module.php
@@ -81,67 +81,6 @@ include '../includes/session.php';
 	
 	<?php include '../includes/footers/footer.php'; ?>
 
-
-
-
-    <?php else : ?>
-
-	<?php include '../includes/menus/portal_menu.php'; ?>
-
-    <div class="container">
-
-    <form class="form-horizontal form-custom">
-
-	<div class="form-logo text-center">
-    <i class="fa fa-graduation-cap"></i>
-    </div>
-
-    <hr>
-	<p class="feedback-danger text-center">You need to have an admin account to access this area.</p>
-    <hr>
-
-    <div class="text-center">
-    <a class="btn btn-primary btn-lg" href="/home/">Home</span></a>
-    </div>
-
-    </form>
-    
-	</div>
-
-	<?php include '../includes/footers/footer.php'; ?>
-
-
-
-
-    <?php endif; ?>
-	<?php else : ?>
-
-	<?php include '../includes/menus/menu.php'; ?>
-
-    <div class="container">
-	
-    <form class="form-horizontal form-custom">
-
-	<div class="form-logo text-center">
-    <i class="fa fa-graduation-cap"></i>
-    </div>
-
-    <hr>
-    <p class="feedback-danger text-center">Looks like you're not signed in yet. Please Sign in before accessing this area.</p>
-    <hr>
-
-    <div class="text-center">
-    <a class="btn btn-primary btn-lg" href="/">Sign in</span></a>
-	</div>
-	
-    </form>
-
-    </div>
-
-	<?php include '../includes/footers/footer.php'; ?>
-
-	<?php endif; ?>
-
     <?php include '../assets/js-paths/common-js-paths.php'; ?>
 
 	<script>
@@ -200,6 +139,63 @@ include '../includes/session.php';
 	return true;
 	});
 	</script>
+
+    <?php else : ?>
+
+	<?php include '../includes/menus/portal_menu.php'; ?>
+
+    <div class="container">
+
+    <form class="form-horizontal form-custom">
+
+	<div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+	<p class="feedback-danger text-center">You need to have an admin account to access this area.</p>
+    <hr>
+
+    <div class="text-center">
+    <a class="btn btn-primary btn-lg btn-load" href="/home/">Home</span></a>
+    </div>
+
+    </form>
+    
+	</div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+    <?php include '../assets/js-paths/common-js-paths.php'; ?>
+
+    <?php endif; ?>
+	<?php else : ?>
+
+	<?php include '../includes/menus/menu.php'; ?>
+
+    <div class="container">
+	
+    <form class="form-horizontal form-custom">
+
+	<div class="form-logo text-center">
+    <i class="fa fa-graduation-cap"></i>
+    </div>
+
+    <hr>
+    <p class="feedback-danger text-center">Looks like you're not signed in yet. Please Sign in before accessing this area.</p>
+    <hr>
+
+    <div class="text-center">
+    <a class="btn btn-primary btn-lg btn-load" href="/">Sign in</span></a>
+	</div>
+	
+    </form>
+
+    </div>
+
+	<?php include '../includes/footers/footer.php'; ?>
+    <?php include '../assets/js-paths/common-js-paths.php'; ?>
+
+	<?php endif; ?>
 
 </body>
 </html>
