85fb9e4a4518ead72f917b4b3ad5413b8dfd833e
triponsergiu <mexug0Aq>
2015-04-24 22:18:46 +0100

    Changed structure

--- admin/allocate-lecture.php
+++ admin/allocate-lecture.php
@@ -202,6 +202,9 @@ if (isset($_GET['id'])) {
     var userToDeallocate = clickedID[1];
     var lectureToDeallocate = $("#lectureid").html();
 
+    alert(userToDeallocate);
+    alert(lectureToDeallocate);
+
 	jQuery.ajax({
 	type: "POST",
 	url: "https://student-portal.co.uk/includes/processes.php",
