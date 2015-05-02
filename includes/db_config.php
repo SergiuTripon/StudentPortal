aa77167932f4c728778578a74c9d480c2a61e511
triponsergiu <mexug0Aq>
2015-05-02 14:20:32 +0100

    Changed structure

--- includes/db_config.php
+++ includes/db_config.php
@@ -2,18 +2,17 @@
 
 //Database information to be used for connection (MySQLi)
 define("HOST", "localhost");
-define("USER", "username");
-define("PASSWORD", "password");
-define("DATABASE", "database name");
+define("USER", "sergiutripon");
+define("PASSWORD", "MJvmJRG4XGdV8Wv"); 
+define("DATABASE", "student_portal");
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
 
 //Database information to be used for connection (PDO)
-$dsn = 'mysql:host=localhost;dbname=database name';
-$username = 'username';
-$password = 'password';
+$dsn = 'mysql:host=localhost;dbname=student_portal';
+$username = 'sergiutripon';
+$password = 'MJvmJRG4XGdV8Wv';
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 );
-
 
